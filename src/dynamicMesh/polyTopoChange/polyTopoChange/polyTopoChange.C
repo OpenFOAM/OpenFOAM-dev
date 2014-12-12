d26c772cf59eca6ae1f20a1b2a03dc4fdebe2b97
Henry <Henry>
2014-12-12 08:55:32 +0000

    Keep copy rather than reference to returned object. Resolves bug report http://www.openfoam.org/mantisbt/view.php?id=1454

--- src/dynamicMesh/polyTopoChange/polyTopoChange/polyTopoChange.C
+++ src/dynamicMesh/polyTopoChange/polyTopoChange/polyTopoChange.C
@@ -2,7 +2,7 @@
   =========                 |
   \\      /  F ield         | OpenFOAM: The Open Source CFD Toolbox
    \\    /   O peration     |
-    \\  /    A nd           | Copyright (C) 2011-2013 OpenFOAM Foundation
+    \\  /    A nd           | Copyright (C) 2011-2014 OpenFOAM Foundation
      \\/     M anipulation  |
 -------------------------------------------------------------------------------
 License
@@ -681,7 +681,7 @@ Foam::label Foam::polyTopoChange::getCellOrder
                 cellInOrder++;
 
                 // find if the neighbours have been visited
-                const labelList& neighbours = cellCellAddressing[currentCell];
+                const labelUList neighbours = cellCellAddressing[currentCell];
 
                 // Add in increasing order of connectivity
 
