9550a248cf4a74627d174aaebbd62a8148e98008
Henry <Henry>
2014-12-28 17:00:11 +0000

    fanFvPatchField: Correct the type specialization Resolves bug report http://www.openfoam.org/mantisbt/view.php?id=1415

--- src/finiteVolume/fields/fvPatchFields/derived/fan/fanFvPatchField.C
+++ src/finiteVolume/fields/fvPatchFields/derived/fan/fanFvPatchField.C
@@ -2,7 +2,7 @@
   =========                 |
   \\      /  F ield         | OpenFOAM: The Open Source CFD Toolbox
    \\    /   O peration     |
-    \\  /    A nd           | Copyright (C) 2011-2012 OpenFOAM Foundation
+    \\  /    A nd           | Copyright (C) 2011-2014 OpenFOAM Foundation
      \\/     M anipulation  |
 -------------------------------------------------------------------------------
 License
@@ -108,8 +108,8 @@ void Foam::fanFvPatchField<Type>::updateCoeffs()
 
     calcFanJump();
 
-    // call fixedJump variant - uniformJump will overwrite the jump value
-    fixedJumpFvPatchField<scalar>::updateCoeffs();
+    // Call fixedJump variant - uniformJump will overwrite the jump value
+    fixedJumpFvPatchField<Type>::updateCoeffs();
 }
 
 
