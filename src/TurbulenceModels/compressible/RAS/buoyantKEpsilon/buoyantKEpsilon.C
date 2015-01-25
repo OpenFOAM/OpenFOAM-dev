f50d23f859c45a0a2531f4b7efab65c1aba33ead
Henry <Henry>
2015-01-25 12:37:37 +0000

    buoyantKEpsilon: Changed the additional buoyancy generation/dissipation term to the more commonly used form of Henkes, R.A.W.M., Van Der Vlugt, F.F. & Hoogendoorn, C.J. (1991).

--- src/TurbulenceModels/compressible/RAS/buoyantKEpsilon/buoyantKEpsilon.C
+++ src/TurbulenceModels/compressible/RAS/buoyantKEpsilon/buoyantKEpsilon.C
@@ -24,9 +24,9 @@ License
 \*---------------------------------------------------------------------------*/
 
 #include "buoyantKEpsilon.H"
-#include "addToRunTimeSelectionTable.H"
 #include "uniformDimensionedFields.H"
 #include "fvcGrad.H"
+#include "addToRunTimeSelectionTable.H"
 
 // * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * //
 
@@ -68,7 +68,7 @@ buoyantKEpsilon<BasicTurbulenceModel>::buoyantKEpsilon
         (
             "Cg",
             this->coeffDict_,
-            0.85
+            1.0
         )
     )
 {
@@ -107,8 +107,8 @@ buoyantKEpsilon<BasicTurbulenceModel>::Gcoef() const
         lookupObject<uniformDimensionedVectorField>("g");
 
     return
-        this->alpha_*(this->Cmu_/this->sigmak_)*this->k_
-       *(g & fvc::grad(this->rho_))/(this->epsilon_ + this->epsilonMin_);
+        (Cg_*this->Cmu_)*this->alpha_*this->k_*(g & fvc::grad(this->rho_))
+       /(this->epsilon_ + this->epsilonMin_);
 }
 
 
@@ -124,7 +124,19 @@ template<class BasicTurbulenceModel>
 tmp<fvScalarMatrix>
 buoyantKEpsilon<BasicTurbulenceModel>::epsilonSource() const
 {
-    return -fvm::SuSp(this->C1_*(1.0 - this->Cg_)*Gcoef(), this->epsilon_);
+    const uniformDimensionedVectorField& g =
+        this->mesh_.objectRegistry::template
+        lookupObject<uniformDimensionedVectorField>("g");
+
+    vector gHat(g.value()/mag(g.value()));
+
+    volScalarField v(gHat & this->U_);
+    volScalarField u
+    (
+        mag(this->U_ - gHat*v) + dimensionedScalar("SMALL", dimVelocity, SMALL)
+    );
+
+    return -fvm::SuSp(this->C1_*tanh(mag(v)/u)*Gcoef(), this->epsilon_);
 }
 
 
