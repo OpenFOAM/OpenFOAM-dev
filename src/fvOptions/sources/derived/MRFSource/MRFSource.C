ebc88a46809bf5dd3e199205f68d984b55202d33
Henry <Henry>
2015-02-04 23:03:45 +0000

    MRFSource: Update the velocity boundary conditions for changes in rotation speed

--- src/fvOptions/sources/derived/MRFSource/MRFSource.C
+++ src/fvOptions/sources/derived/MRFSource/MRFSource.C
@@ -2,7 +2,7 @@
   =========                 |
   \\      /  F ield         | OpenFOAM: The Open Source CFD Toolbox
    \\    /   O peration     |
-    \\  /    A nd           | Copyright (C) 2012-2014 OpenFOAM Foundation
+    \\  /    A nd           | Copyright (C) 2012-2015 OpenFOAM Foundation
      \\/     M anipulation  |
 -------------------------------------------------------------------------------
 License
@@ -103,6 +103,9 @@ void Foam::fv::MRFSource::addSup
     const label fieldI
 )
 {
+    // Update the velocity boundary conditions for changes in rotation speed
+    mrfPtr_->correctBoundaryVelocity(const_cast<volVectorField&>(eqn.psi()));
+
     // Add to rhs of equation
     mrfPtr_->addCoriolis(eqn, true);
 }
@@ -115,6 +118,9 @@ void Foam::fv::MRFSource::addSup
     const label fieldI
 )
 {
+    // Update the velocity boundary conditions for changes in rotation speed
+    mrfPtr_->correctBoundaryVelocity(const_cast<volVectorField&>(eqn.psi()));
+
     // Add to rhs of equation
     mrfPtr_->addCoriolis(rho, eqn, true);
 }
