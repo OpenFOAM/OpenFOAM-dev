38415f88391ded8b94afbbe557c0ba50bb816398
Henry <Henry>
2015-05-18 22:34:39 +0100

    Upgraded scotch to 6.0.3 (6.0.4 failed to compile)

--- etc/config/scotch.sh
+++ etc/config/scotch.sh
@@ -2,7 +2,7 @@
 # =========                 |
 # \\      /  F ield         | OpenFOAM: The Open Source CFD Toolbox
 #  \\    /   O peration     |
-#   \\  /    A nd           | Copyright (C) 2011-2014 OpenFOAM Foundation
+#   \\  /    A nd           | Copyright (C) 2011-2015 OpenFOAM Foundation
 #    \\/     M anipulation  |
 #------------------------------------------------------------------------------
 # License
@@ -33,7 +33,7 @@
 #     during the wmake process
 #------------------------------------------------------------------------------
 
-export SCOTCH_VERSION=scotch_6.0.0
+export SCOTCH_VERSION=scotch_6.0.3
 export SCOTCH_ARCH_PATH=$WM_THIRD_PARTY_DIR/platforms/$WM_ARCH$WM_COMPILER$WM_LABEL_OPTION/$SCOTCH_VERSION
 
 # -----------------------------------------------------------------------------
