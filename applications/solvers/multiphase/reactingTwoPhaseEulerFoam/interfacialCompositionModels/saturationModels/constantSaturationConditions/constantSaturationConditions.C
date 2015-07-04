5b9303962bf8c998a8a43240996efb88747997fb
Henry Weller <http://cfd.direct>
2015-07-04 14:48:19 +0100

    saturationModels: Added a model for constant pSat and Tsat

---
+++ applications/solvers/multiphase/reactingTwoPhaseEulerFoam/interfacialCompositionModels/saturationModels/constantSaturationConditions/constantSaturationConditions.C
@@ -0,0 +1,170 @@
+/*---------------------------------------------------------------------------*\
+  =========                 |
+  \\      /  F ield         | OpenFOAM: The Open Source CFD Toolbox
+   \\    /   O peration     |
+    \\  /    A nd           | Copyright (C) 2015 OpenFOAM Foundation
+     \\/     M anipulation  |
+-------------------------------------------------------------------------------
+License
+    This file is part of OpenFOAM.
+
+    OpenFOAM is free software: you can redistribute it and/or modify it
+    under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    OpenFOAM is distributed in the hope that it will be useful, but WITHOUT
+    ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+    FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License
+    for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with OpenFOAM.  If not, see <http://www.gnu.org/licenses/>.
+
+\*---------------------------------------------------------------------------*/
+
+#include "constantSaturationConditions.H"
+#include "addToRunTimeSelectionTable.H"
+
+// * * * * * * * * * * * * * * Static Data Members * * * * * * * * * * * * * //
+
+namespace Foam
+{
+namespace saturationModels
+{
+    defineTypeNameAndDebug(constantSaturationConditions, 0);
+    addToRunTimeSelectionTable
+    (
+        saturationModel,
+        constantSaturationConditions,
+        dictionary
+    );
+}
+}
+
+
+// * * * * * * * * * * * * * * * * Constructors  * * * * * * * * * * * * * * //
+
+Foam::saturationModels::constantSaturationConditions::
+constantSaturationConditions(const dictionary& dict)
+:
+    saturationModel(),
+    pSat_("pSat", dimPressure, dict.lookup("pSat")),
+    Tsat_("Tsat", dimTemperature, dict.lookup("Tsat"))
+{}
+
+
+// * * * * * * * * * * * * * * * * Destructor  * * * * * * * * * * * * * * * //
+
+Foam::saturationModels::constantSaturationConditions::
+~constantSaturationConditions()
+{}
+
+
+// * * * * * * * * * * * * * * * Member Functions  * * * * * * * * * * * * * //
+
+Foam::tmp<Foam::volScalarField>
+Foam::saturationModels::constantSaturationConditions::pSat
+(
+    const volScalarField& T
+) const
+{
+    return tmp<volScalarField>
+    (
+        new volScalarField
+        (
+            IOobject
+            (
+                "pSat",
+                T.mesh().time().timeName(),
+                T.mesh(),
+                IOobject::NO_READ,
+                IOobject::NO_WRITE,
+                false
+            ),
+            T.mesh(),
+            pSat_
+        )
+    );
+}
+
+
+Foam::tmp<Foam::volScalarField>
+Foam::saturationModels::constantSaturationConditions::pSatPrime
+(
+    const volScalarField& T
+) const
+{
+    return tmp<volScalarField>
+    (
+        new volScalarField
+        (
+            IOobject
+            (
+                "pSatPrime",
+                T.mesh().time().timeName(),
+                T.mesh(),
+                IOobject::NO_READ,
+                IOobject::NO_WRITE,
+                false
+            ),
+            T.mesh(),
+            dimensionedScalar("zero", dimPressure/dimTemperature, 0)
+        )
+    );
+}
+
+
+Foam::tmp<Foam::volScalarField>
+Foam::saturationModels::constantSaturationConditions::lnPSat
+(
+    const volScalarField& T
+) const
+{
+    return tmp<volScalarField>
+    (
+        new volScalarField
+        (
+            IOobject
+            (
+                "lnPSat",
+                T.mesh().time().timeName(),
+                T.mesh(),
+                IOobject::NO_READ,
+                IOobject::NO_WRITE,
+                false
+            ),
+            T.mesh(),
+            dimensionedScalar("lnPSat", dimless, log(pSat_.value()))
+        )
+    );
+}
+
+
+Foam::tmp<Foam::volScalarField>
+Foam::saturationModels::constantSaturationConditions::Tsat
+(
+    const volScalarField& p
+) const
+{
+    return tmp<volScalarField>
+    (
+        new volScalarField
+        (
+            IOobject
+            (
+                "Tsat",
+                p.mesh().time().timeName(),
+                p.mesh(),
+                IOobject::NO_READ,
+                IOobject::NO_WRITE,
+                false
+            ),
+            p.mesh(),
+            Tsat_
+        )
+    );
+}
+
+
+// ************************************************************************* //
