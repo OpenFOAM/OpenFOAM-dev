3b46408d38cc41583c7cce8408861ccfcd827a38
Henry Weller <http://cfd.direct>
2015-10-29 22:42:42 +0000

    reactingEulerFoam: Add polynomial saturation model Provided by Juho Peltola

---
+++ applications/solvers/multiphase/reactingEulerFoam/interfacialCompositionModels/saturationModels/polynomial/polynomial.C
@@ -0,0 +1,136 @@
+/*---------------------------------------------------------------------------*\
+  =========                 |
+  \\      /  F ield         | OpenFOAM: The Open Source CFD Toolbox
+   \\    /   O peration     |
+    \\  /    A nd           | Copyright (C) 2015 OpenFOAM Foundation
+     \\/     M anipulation  |
+-------------------------------------------------------------------------------
+License
+    This file is part of OpenFOAM.
+
+    OpenFOAM is free software: you can redistribute it and/or modify it
+    under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    OpenFOAM is distributed in the hope that it will be useful, but WITHOUT
+    ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+    FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License
+    for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with OpenFOAM.  If not, see <http://www.gnu.org/licenses/>.
+
+\*---------------------------------------------------------------------------*/
+
+#include "polynomial.H"
+#include "addToRunTimeSelectionTable.H"
+
+// * * * * * * * * * * * * * * Static Data Members * * * * * * * * * * * * * //
+
+namespace Foam
+{
+namespace saturationModels
+{
+    defineTypeNameAndDebug(polynomial, 0);
+    addToRunTimeSelectionTable(saturationModel, polynomial, dictionary);
+}
+}
+
+
+// * * * * * * * * * * * * * * * * Constructors  * * * * * * * * * * * * * * //
+
+Foam::saturationModels::polynomial::polynomial(const dictionary& dict)
+:
+    saturationModel(),
+    C_(dict.lookup("C<8>"))
+{}
+
+
+// * * * * * * * * * * * * * * * * Destructor  * * * * * * * * * * * * * * * //
+
+Foam::saturationModels::polynomial::~polynomial()
+{}
+
+
+// * * * * * * * * * * * * * * * Member Functions  * * * * * * * * * * * * * //
+
+Foam::tmp<Foam::volScalarField>
+Foam::saturationModels::polynomial::pSat
+(
+    const volScalarField& T
+) const
+{
+    NotImplemented;
+    return volScalarField::null();
+}
+
+
+Foam::tmp<Foam::volScalarField>
+Foam::saturationModels::polynomial::pSatPrime
+(
+    const volScalarField& T
+) const
+{
+    NotImplemented;
+    return volScalarField::null();
+}
+
+
+Foam::tmp<Foam::volScalarField>
+Foam::saturationModels::polynomial::lnPSat
+(
+    const volScalarField& T
+) const
+{
+    NotImplemented;
+    return volScalarField::null();
+}
+
+
+Foam::tmp<Foam::volScalarField>
+Foam::saturationModels::polynomial::Tsat
+(
+    const volScalarField& p
+) const
+{
+    tmp<volScalarField> tTsat
+    (
+        new volScalarField
+        (
+            IOobject
+            (
+                "Tsat",
+                p.mesh().time().timeName(),
+                p.mesh(),
+                IOobject::NO_READ,
+                IOobject::NO_WRITE
+            ),
+            p.mesh(),
+            dimensionedScalar("zero", dimTemperature, 0)
+        )
+    );
+
+    volScalarField& Tsat = tTsat();
+
+    forAll(Tsat,celli)
+    {
+        Tsat[celli] = C_.value(p[celli]);
+    }
+
+    forAll(Tsat.boundaryField(), patchi)
+    {
+        scalarField& Tsatp = Tsat.boundaryField()[patchi];
+        const scalarField& pp = p.boundaryField()[patchi];
+
+        forAll(Tsatp, facei)
+        {
+            Tsatp[facei] = C_.value(pp[facei]);
+        }
+    }
+
+    return tTsat;
+}
+
+
+// ************************************************************************* //
