08a1b11df126a1589be59875507911e5e76d497f
Henry Weller <http://cfd.direct>
2015-12-03 15:37:05 +0000

    TurbulenceModels/phaseCompressible/RAS/LaheyKEpsilon: Added support for fvOptions

--- src/TurbulenceModels/phaseCompressible/RAS/LaheyKEpsilon/LaheyKEpsilon.C
+++ src/TurbulenceModels/phaseCompressible/RAS/LaheyKEpsilon/LaheyKEpsilon.C
@@ -24,9 +24,8 @@ License
 \*---------------------------------------------------------------------------*/
 
 #include "LaheyKEpsilon.H"
-#include "addToRunTimeSelectionTable.H"
+#include "fvOptions.H"
 #include "twoPhaseSystem.H"
-#include "dragModel.H"
 
 // * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * //
 
@@ -176,6 +175,9 @@ void LaheyKEpsilon<BasicTurbulenceModel>::correctNut()
        *(mag(this->U_ - gasTurbulence.U()));
 
     this->nut_.correctBoundaryConditions();
+    fv::options::New(this->mesh_).correct(this->nut_);
+
+    BasicTurbulenceModel::correctNut();
 }
 
 
