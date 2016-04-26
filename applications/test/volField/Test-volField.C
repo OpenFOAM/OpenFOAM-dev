f0d25904e48f59cbf40d9e726c804c32e56800bf
Henry Weller <http://cfd.direct>
2016-04-26 20:45:53 +0100

    GeometricField, volFields: Added experimental member function ".v()" and perfix operator "~" both of which return the dimensionedInternalField for volFields only.
    These will be useful in FV equation source term expressions which need
    not evaluate boundary conditions.

--- applications/test/volField/Test-volField.C
+++ applications/test/volField/Test-volField.C
@@ -2,7 +2,7 @@
   =========                 |
   \\      /  F ield         | OpenFOAM: The Open Source CFD Toolbox
    \\    /   O peration     |
-    \\  /    A nd           | Copyright (C) 2011-2015 OpenFOAM Foundation
+    \\  /    A nd           | Copyright (C) 2011-2016 OpenFOAM Foundation
      \\/     M anipulation  |
 -------------------------------------------------------------------------------
 License
@@ -51,6 +51,10 @@ int main(int argc, char *argv[])
         ),
         mesh
     );
+
+    Info<< nl
+        << "p.v().size(): "
+        << p.v().size() << endl;
 
     Info<< "Reading field U\n" << endl;
     volVectorField U
