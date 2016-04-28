aa09dd3f7b073d4e94b1c70136b48ea5d42edc87
Henry Weller <http://cfd.direct>
2016-04-28 20:40:32 +0100

    functionObjects/field/histogram: New functionObject to write the volume-weighted histogram of a volScalarField
    e.g.
        pressureHistogram
        {
            type            histogram;
    
            functionObjectLibs ("libfieldFunctionObjects.so");
    
            field           p;
            nBins           100;
            min             -5;
            max             5;
            setFormat       gnuplot;
        }

---
+++ src/postProcessing/functionObjects/field/histogram/histogram.C
@@ -0,0 +1,221 @@
+/*---------------------------------------------------------------------------*\
+  =========                 |
+  \\      /  F ield         | OpenFOAM: The Open Source CFD Toolbox
+   \\    /   O peration     |
+    \\  /    A nd           | Copyright (C) 2016 OpenFOAM Foundation
+     \\/     M anipulation  |
+-------------------------------------------------------------------------------
+License
+    This file is part of OpenFOAM.
+
+    OpenFOAM is free software: you can redistribute it and/or modify it
+    under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    OpenFOAM is distributed in the hope that it will be useful, but WITHOUT
+    ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+    FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License
+    for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with OpenFOAM.  If not, see <http://www.gnu.org/licenses/>.
+
+\*---------------------------------------------------------------------------*/
+
+#include "histogram.H"
+#include "volFields.H"
+
+// * * * * * * * * * * * * * * Static Data Members * * * * * * * * * * * * * //
+
+namespace Foam
+{
+namespace functionObjects
+{
+    defineTypeNameAndDebug(histogram, 0);
+}
+}
+
+
+// * * * * * * * * * * * * * Private Member Functions  * * * * * * * * * * * //
+
+void Foam::functionObjects::histogram::writeGraph
+(
+    const coordSet& coords,
+    const word& fieldName,
+    const scalarField& values
+) const
+{
+    const wordList fieldNames(1, fieldName);
+
+    fileName outputPath = baseTimeDir();
+    mkDir(outputPath);
+    OFstream graphFile
+    (
+        outputPath/formatterPtr_().getFileName(coords, fieldNames)
+    );
+
+    Info<< "Writing histogram of " << fieldName
+        << " to " << graphFile.name() << endl;
+
+    List<const scalarField*> yPtrs(1);
+    yPtrs[0] = &values;
+    formatterPtr_().write(coords, fieldNames, yPtrs, graphFile);
+}
+
+
+// * * * * * * * * * * * * * * * * Constructors  * * * * * * * * * * * * * * //
+
+Foam::functionObjects::histogram::histogram
+(
+    const word& name,
+    const objectRegistry& obr,
+    const dictionary& dict,
+    const bool loadFromFiles
+)
+:
+    functionObjectFile(obr, name, typeName),
+    name_(name),
+    obr_(obr),
+    active_(true)
+{
+    // Check if the available mesh is an fvMesh, otherwise deactivate
+    if (isA<fvMesh>(obr_))
+    {
+        read(dict);
+    }
+    else
+    {
+        active_ = false;
+        WarningInFunction
+            << "No fvMesh available, deactivating " << name_ << nl
+            << endl;
+    }
+}
+
+
+// * * * * * * * * * * * * * * * * Destructor  * * * * * * * * * * * * * * * //
+
+Foam::functionObjects::histogram::~histogram()
+{}
+
+
+// * * * * * * * * * * * * * * * Member Functions  * * * * * * * * * * * * * //
+
+void Foam::functionObjects::histogram::read(const dictionary& dict)
+{
+    if (active_)
+    {
+        dict.lookup("field") >> fieldName_;
+        dict.lookup("max") >> max_;
+        min_ = 0.0;
+        dict.readIfPresent("min", min_);
+        dict.lookup("nBins") >> nBins_;
+
+        word format(dict.lookup("setFormat"));
+        formatterPtr_ = writer<scalar>::New(format);
+    }
+}
+
+
+void Foam::functionObjects::histogram::execute()
+{}
+
+
+void Foam::functionObjects::histogram::end()
+{}
+
+
+void Foam::functionObjects::histogram::timeSet()
+{}
+
+
+void Foam::functionObjects::histogram::write()
+{
+    if (active_)
+    {
+        Info<< type() << " " << name_ << " output:" << nl;
+
+        const fvMesh& mesh = refCast<const fvMesh>(obr_);
+
+        autoPtr<volScalarField> fieldPtr;
+        if (obr_.foundObject<volScalarField>(fieldName_))
+        {
+            Info<< "    Looking up field " << fieldName_ << endl;
+        }
+        else
+        {
+            Info<< "    Reading field " << fieldName_ << endl;
+            fieldPtr.reset
+            (
+                new volScalarField
+                (
+                    IOobject
+                    (
+                        fieldName_,
+                        mesh.time().timeName(),
+                        mesh,
+                        IOobject::MUST_READ,
+                        IOobject::NO_WRITE
+                    ),
+                    mesh
+                )
+            );
+        }
+
+        const volScalarField& field =
+        (
+             fieldPtr.valid()
+           ? fieldPtr()
+           : obr_.lookupObject<volScalarField>(fieldName_)
+        );
+
+        // Calculate the mid-points of bins for the graph axis
+        pointField xBin(nBins_);
+        const scalar delta = (max_- min_)/nBins_;
+
+        scalar x = min_ + 0.5*delta;
+        forAll(xBin, i)
+        {
+            xBin[i] = point(x, 0, 0);
+            x += delta;
+        }
+
+        scalarField volFrac(nBins_, 0);
+        const scalarField& V = mesh.V();
+
+        forAll(field, celli)
+        {
+            const label bini = (field[celli] - min_)/delta;
+            if (bini >= 0 && bini < nBins_)
+            {
+                volFrac[bini] += V[celli];
+            }
+        }
+
+        Pstream::listCombineGather(volFrac, plusEqOp<scalar>());
+
+        if (Pstream::master())
+        {
+            const scalar sumVol = sum(volFrac);
+
+            if (sumVol > SMALL)
+            {
+                volFrac /= sumVol;
+
+                const coordSet coords
+                (
+                    "Volume_Fraction",
+                    "x",
+                    xBin,
+                    mag(xBin)
+                );
+
+                writeGraph(coords, field.name(), volFrac);
+            }
+        }
+    }
+}
+
+
+// ************************************************************************* //
