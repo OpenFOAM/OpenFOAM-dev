71a448d66fde5b240054ecf380533c6bc747d156
Henry Weller <http://cfd.direct>
2016-05-07 09:32:00 +0100

    functionObjects: Renamed option "adjustableTime" -> "adjustableRunTime" for consistency with Time

--- src/OpenFOAM/db/functionObjects/outputFilterOutputControl/outputFilterOutputControl.C
+++ src/OpenFOAM/db/functionObjects/outputFilterOutputControl/outputFilterOutputControl.C
@@ -2,7 +2,7 @@
   =========                 |
   \\      /  F ield         | OpenFOAM: The Open Source CFD Toolbox
    \\    /   O peration     |
-    \\  /    A nd           | Copyright (C) 2011-2015 OpenFOAM Foundation
+    \\  /    A nd           | Copyright (C) 2011-2016 OpenFOAM Foundation
      \\/     M anipulation  |
 -------------------------------------------------------------------------------
 License
@@ -36,7 +36,7 @@ namespace Foam
     {
         "timeStep",
         "outputTime",
-        "adjustableTime",
+        "adjustableRunTime",
         "runTime",
         "clockTime",
         "cpuTime",
