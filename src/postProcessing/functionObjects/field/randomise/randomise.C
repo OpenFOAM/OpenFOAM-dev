f4ecb2ff23aa1a7155b66d73ad97719f01686109
Henry Weller <http://cfd.direct>
2016-05-26 15:32:10 +0100

    functionObjects::randomise: New functionObject to create a field with a random perturbation

---
+++ src/postProcessing/functionObjects/field/randomise/randomise.C
@@ -0,0 +1,90 @@
+/*---------------------------------------------------------------------------*\
+  =========                 |
+  \\      /  F ield         | OpenFOAM: The Open Source CFD Toolbox
+   \\    /   O peration     |
+    \\  /    A nd           | Copyright (C) 2016 OpenFOAM Foundation
+     \\/     M anipulation  |
+-------------------------------------------------------------------------------
+License
+    This file is part of OpenFOAM.
+
+    OpenFOAM is free software: you can redistribute it and/or modify it
+    under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    OpenFOAM is distributed in the hope that it will be useful, but WITHOUT
+    ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+    FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License
+    for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with OpenFOAM.  If not, see <http://www.gnu.org/licenses/>.
+
+\*---------------------------------------------------------------------------*/
+
+#include "randomise.H"
+#include "addToRunTimeSelectionTable.H"
+
+// * * * * * * * * * * * * * * Static Data Members * * * * * * * * * * * * * //
+
+namespace Foam
+{
+namespace functionObjects
+{
+    defineTypeNameAndDebug(randomise, 0);
+    addToRunTimeSelectionTable(functionObject, randomise, dictionary);
+}
+}
+
+
+// * * * * * * * * * * * * * Private Member Functions  * * * * * * * * * * * //
+
+bool Foam::functionObjects::randomise::calc()
+{
+    bool processed = false;
+
+    processed = processed || calcRandomised<scalar>();
+    processed = processed || calcRandomised<vector>();
+    processed = processed || calcRandomised<sphericalTensor>();
+    processed = processed || calcRandomised<symmTensor>();
+    processed = processed || calcRandomised<tensor>();
+
+    return processed;
+}
+
+
+// * * * * * * * * * * * * * * * * Constructors  * * * * * * * * * * * * * * //
+
+Foam::functionObjects::randomise::randomise
+(
+    const word& name,
+    const Time& runTime,
+    const dictionary& dict
+)
+:
+    fieldExpression(name, runTime, dict)
+{
+    read(dict);
+}
+
+
+// * * * * * * * * * * * * * * * * Destructor  * * * * * * * * * * * * * * * //
+
+Foam::functionObjects::randomise::~randomise()
+{}
+
+
+// * * * * * * * * * * * * * * * Member Functions  * * * * * * * * * * * * * //
+
+bool Foam::functionObjects::randomise::read(const dictionary& dict)
+{
+    fieldExpression::read(dict);
+
+    dict.lookup("magPerturbation") >> magPerturbation_;
+
+    return true;
+}
+
+
+// ************************************************************************* //
---
+++ src/postProcessing/functionObjects/field/randomise/randomiseTemplates.C
@@ -0,0 +1,65 @@
+/*---------------------------------------------------------------------------*\
+  =========                 |
+  \\      /  F ield         | OpenFOAM: The Open Source CFD Toolbox
+   \\    /   O peration     |
+    \\  /    A nd           | Copyright (C) 2016 OpenFOAM Foundation
+     \\/     M anipulation  |
+-------------------------------------------------------------------------------
+License
+    This file is part of OpenFOAM.
+
+    OpenFOAM is free software: you can redistribute it and/or modify it
+    under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    OpenFOAM is distributed in the hope that it will be useful, but WITHOUT
+    ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+    FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License
+    for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with OpenFOAM.  If not, see <http://www.gnu.org/licenses/>.
+
+\*---------------------------------------------------------------------------*/
+
+#include "volFields.H"
+#include "Random.H"
+
+// * * * * * * * * * * * * * * * Member Functions  * * * * * * * * * * * * * //
+
+template<class Type>
+bool Foam::functionObjects::randomise::calcRandomised()
+{
+    typedef GeometricField<Type, fvPatchField, volMesh> VolFieldType;
+
+    if (foundObject<VolFieldType>(fieldName_))
+    {
+        const VolFieldType& field = lookupObject<VolFieldType>(fieldName_);
+
+        resultName_ = fieldName_ + "Random";
+
+        tmp<VolFieldType> rfieldt(new VolFieldType(field));
+        VolFieldType& rfield = rfieldt.ref();
+
+        Random rand(1234567);
+
+        forAll(field, celli)
+        {
+            Type rndPert;
+            rand.randomise(rndPert);
+            rndPert = 2.0*rndPert - pTraits<Type>::one;
+            rndPert /= mag(rndPert);
+            rfield[celli] += magPerturbation_*rndPert;
+        }
+
+        return store(resultName_, rfieldt);
+    }
+    else
+    {
+        return false;
+    }
+}
+
+
+// ************************************************************************* //
