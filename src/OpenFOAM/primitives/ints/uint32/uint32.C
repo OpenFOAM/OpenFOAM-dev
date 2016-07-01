97a889dbd8398323bb8c66c7743c75c5428ea726
Henry Weller <http://cfd.direct>
2016-07-01 14:52:54 +0100

    src/OpenFOAM/primitives/ints: Further tweaks Resolved additional bug-report http://bugs.openfoam.org/view.php?id=2137

--- src/OpenFOAM/primitives/ints/uint32/uint32.C
+++ src/OpenFOAM/primitives/ints/uint32/uint32.C
@@ -31,7 +31,7 @@ const uint32_t Foam::pTraits<uint32_t>::zero = 0;
 const uint32_t Foam::pTraits<uint32_t>::one = 1;
 const uint32_t Foam::pTraits<uint32_t>::min = 0;
 const uint32_t Foam::pTraits<uint32_t>::max = UINT32_MAX;
-const uint32_t Foam::pTraits<uint32_t>::rootMin = pTraits<uint32_t>::min;
+const uint32_t Foam::pTraits<uint32_t>::rootMin = 0;
 const uint32_t Foam::pTraits<uint32_t>::rootMax = pTraits<uint32_t>::max;
 
 const char* const Foam::pTraits<uint32_t>::componentNames[] = { "" };
--- src/OpenFOAM/primitives/ints/uint64/uint64.C
+++ src/OpenFOAM/primitives/ints/uint64/uint64.C
@@ -31,7 +31,7 @@ const uint64_t Foam::pTraits<uint64_t>::zero = 0;
 const uint64_t Foam::pTraits<uint64_t>::one = 1;
 const uint64_t Foam::pTraits<uint64_t>::min = 0;
 const uint64_t Foam::pTraits<uint64_t>::max = UINT64_MAX;
-const uint64_t Foam::pTraits<uint64_t>::rootMin = pTraits<uint64_t>::min;
+const uint64_t Foam::pTraits<uint64_t>::rootMin = 0;
 const uint64_t Foam::pTraits<uint64_t>::rootMax = pTraits<uint64_t>::max;
 
 const char* const Foam::pTraits<uint64_t>::componentNames[] = { "" };
