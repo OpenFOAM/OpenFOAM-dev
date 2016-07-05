f989f58d854ab334586aaca9ac4963c471caa370
Henry Weller <http://cfd.direct>
2016-07-05 16:10:07 +0100

    reactingEulerFoam dragModels: New models Beetstra, Tenneti
    Contributed by Alberto Passalacqua, Iowa State University
    
    Foam::dragModels::Beetstra
        Drag model of Beetstra et al. for monodisperse gas-particle flows obtained
        with direct numerical simulations with the Lattice-Boltzmann method and
        accounting for the effect of particle ensembles.
    
        Reference:
        \verbatim
            Beetstra, R., van der Hoef, M. A., & Kuipers, J. a. M. (2007).
            Drag force of intermediate Reynolds number flow past mono- and
            bidisperse arrays of spheres.
            AIChE Journal, 53(2), 489–501.
        \endverbatim
    
    Foam::dragModels::Tenneti
        Drag model of Tenneti et al. for monodisperse gas-particle flows obtained
        with particle-resolved direct numerical simulations and accounting for the
        effect of particle ensembles.
    
        Reference:
        \verbatim
            Tenneti, S., Garg, R., & Subramaniam, S. (2011).
            Drag law for monodisperse gas–solid systems using particle-resolved
            direct numerical simulation of flow past fixed assemblies of spheres.
            International Journal of Multiphase Flow, 37(9), 1072–1092.
        \verbatim

---
+++ applications/solvers/multiphase/reactingEulerFoam/interfacialModels/dragModels/Beetstra/Beetstra.C
@@ -0,0 +1,101 @@
+/*---------------------------------------------------------------------------*\
+  =========                 |
+  \\      /  F ield         | OpenFOAM: The Open Source CFD Toolbox
+   \\    /   O peration     |
+    \\  /    A nd           | Copyright (C) 2016 OpenFOAM Foundation
+     \\/     M anipulation  |
+-------------------------------------------------------------------------------
+License
+    This file is part of OpenFOAM.
+
+    OpenFOAM is free software: you can redistribute it and/or modify it
+    under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    OpenFOAM is distributed in the hope that it will be useful, but WITHOUT
+    ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+    FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License
+    for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with OpenFOAM.  If not, see <http://www.gnu.org/licenses/>.
+
+\*---------------------------------------------------------------------------*/
+
+#include "Beetstra.H"
+#include "phasePair.H"
+#include "addToRunTimeSelectionTable.H"
+
+// * * * * * * * * * * * * * * Static Data Members * * * * * * * * * * * * * //
+
+namespace Foam
+{
+namespace dragModels
+{
+    defineTypeNameAndDebug(Beetstra, 0);
+    addToRunTimeSelectionTable(dragModel, Beetstra, dictionary);
+}
+}
+
+
+// * * * * * * * * * * * * * * * * Constructors  * * * * * * * * * * * * * * //
+
+Foam::dragModels::Beetstra::Beetstra
+(
+    const dictionary& dict,
+    const phasePair& pair,
+    const bool registerObject
+)
+:
+    dragModel(dict, pair, registerObject),
+    residualRe_("residualRe", dimless, dict.lookup("residualRe"))
+{}
+
+
+// * * * * * * * * * * * * * * * * Destructor  * * * * * * * * * * * * * * * //
+
+Foam::dragModels::Beetstra::~Beetstra()
+{}
+
+
+// * * * * * * * * * * * * * * * Member Functions  * * * * * * * * * * * * * //
+
+Foam::tmp<Foam::volScalarField> Foam::dragModels::Beetstra::CdRe() const
+{
+    volScalarField alpha1
+    (
+        max(pair_.dispersed(), pair_.continuous().residualAlpha())
+    );
+
+    volScalarField alpha2
+    (
+        max(scalar(1) - pair_.dispersed(), pair_.continuous().residualAlpha())
+    );
+
+    volScalarField Re(pair_.Re());
+    volScalarField ReLim
+    (
+        "ReLim",
+        max(Re, residualRe_)
+    );
+
+    volScalarField F0
+    (
+        "F0",
+        10.0*alpha1/sqr(alpha2) + sqr(alpha2)*(1.0 + 1.5*sqrt(alpha1))
+    );
+
+    volScalarField F1
+    (
+        "F1",
+        0.413*Re/(24.0*sqr(alpha2))*(1.0/alpha2
+        + 3.0*alpha1*alpha2 + 8.4*pow(ReLim, -0.343))
+        /(1.0 + pow(10.0, 3*alpha1)*pow(ReLim, -(1.0 + 4.0*alpha1)/2.0))
+    );
+
+    return 24.0*alpha2*(F0 + F1);
+}
+
+
+// ************************************************************************* //
---
+++ applications/solvers/multiphase/reactingEulerFoam/interfacialModels/dragModels/Tenneti/Tenneti.C
@@ -0,0 +1,106 @@
+/*---------------------------------------------------------------------------*\
+  =========                 |
+  \\      /  F ield         | OpenFOAM: The Open Source CFD Toolbox
+   \\    /   O peration     |
+    \\  /    A nd           | Copyright (C) 2016 OpenFOAM Foundation
+     \\/     M anipulation  |
+-------------------------------------------------------------------------------
+License
+    This file is part of OpenFOAM.
+
+    OpenFOAM is free software: you can redistribute it and/or modify it
+    under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    OpenFOAM is distributed in the hope that it will be useful, but WITHOUT
+    ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+    FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License
+    for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with OpenFOAM.  If not, see <http://www.gnu.org/licenses/>.
+
+\*---------------------------------------------------------------------------*/
+
+#include "Tenneti.H"
+#include "phasePair.H"
+#include "SchillerNaumann.H"
+#include "addToRunTimeSelectionTable.H"
+
+// * * * * * * * * * * * * * * Static Data Members * * * * * * * * * * * * * //
+
+namespace Foam
+{
+namespace dragModels
+{
+    defineTypeNameAndDebug(Tenneti, 0);
+    addToRunTimeSelectionTable(dragModel, Tenneti, dictionary);
+}
+}
+
+
+// * * * * * * * * * * * * * * * * Constructors  * * * * * * * * * * * * * * //
+
+Foam::dragModels::Tenneti::Tenneti
+(
+    const dictionary& dict,
+    const phasePair& pair,
+    const bool registerObject
+)
+:
+    dragModel(dict, pair, registerObject),
+    SchillerNaumann_
+    (
+        new SchillerNaumann
+        (
+            dict,
+            pair,
+            false
+        )
+    ),
+    residualRe_("residualRe", dimless, dict.lookup("residualRe"))
+{}
+
+
+// * * * * * * * * * * * * * * * * Destructor  * * * * * * * * * * * * * * * //
+
+Foam::dragModels::Tenneti::~Tenneti()
+{}
+
+
+// * * * * * * * * * * * * * * * Member Functions  * * * * * * * * * * * * * //
+
+Foam::tmp<Foam::volScalarField> Foam::dragModels::Tenneti::CdRe() const
+{
+    volScalarField alpha1
+    (
+        max(pair_.dispersed(), pair_.continuous().residualAlpha())
+    );
+
+    volScalarField alpha2
+    (
+        max(scalar(1) - pair_.dispersed(), pair_.continuous().residualAlpha())
+    );
+
+    volScalarField F0
+    (
+        5.81*alpha1/pow3(alpha2) + 0.48*pow(alpha1, 1.0/3.0)/pow4(alpha2)
+    );
+
+    volScalarField F1
+    (
+        pow(alpha1, 3)*max(pair_.Re(), residualRe_)
+       *(0.95 + 0.61*pow3(alpha1)/sqr(alpha2))
+    );
+
+    // Tenneti et al. correlation includes the mean pressure drag.
+    // This was removed here by multiplying F by alpha2 for consistency with
+    // the formulation used in OpenFOAM
+    return
+        SchillerNaumann_->CdRe()/(alpha2*max(pair_.Re(), residualRe_)) +
+        24.0*sqr(alpha2)*(F0 + F1);
+}
+
+
+// ************************************************************************* //
