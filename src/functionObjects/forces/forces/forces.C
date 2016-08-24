73eee8e54247e339a540ea31d803282e0aa91c99
Henry Weller <http://cfd.direct>
2016-08-24 11:24:27 +0100

    functionObjects::forces: Corrected file names when using the 'binData' option Resolves bug-report http://bugs.openfoam.org/view.php?id=2214

--- src/functionObjects/forces/forces/forces.C
+++ src/functionObjects/forces/forces/forces.C
@@ -53,6 +53,7 @@ Foam::wordList Foam::functionObjects::forces::createFileNames
     DynamicList<word> names(1);
 
     const word forceType(dict.lookup("type"));
+    names.append(forceType);
 
     if (dict.found("binData"))
     {
@@ -63,8 +64,6 @@ Foam::wordList Foam::functionObjects::forces::createFileNames
             names.append(forceType + "_bins");
         }
     }
-
-    names.append(forceType);
 
     return names;
 }
