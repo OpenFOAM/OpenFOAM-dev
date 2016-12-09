14973e39a6ccd02ba153a385098d525bd892d017
Henry Weller <http://cfd.direct>
2016-12-09 16:36:56 +0000

    gaussConvectionScheme: Removed temporary warnUnboundedGauss debug switch
    which provided warning about backward-compatibility issue with setting div
    schemes for steady-state.  It caused confusion by generating incorrect warning
    messages for compressible cases for which the 'bounded' should NOT be applied to
    the 'div(phid,p)'.

--- src/finiteVolume/finiteVolume/convectionSchemes/gaussConvectionScheme/gaussConvectionSchemes.C
+++ src/finiteVolume/finiteVolume/convectionSchemes/gaussConvectionScheme/gaussConvectionSchemes.C
@@ -2,7 +2,7 @@
   =========                 |
   \\      /  F ield         | OpenFOAM: The Open Source CFD Toolbox
    \\    /   O peration     |
-    \\  /    A nd           | Copyright (C) 2011-2015 OpenFOAM Foundation
+    \\  /    A nd           | Copyright (C) 2011-2016 OpenFOAM Foundation
      \\/     M anipulation  |
 -------------------------------------------------------------------------------
 License
@@ -27,11 +27,6 @@ License
 #include "fvMesh.H"
 
 // * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * //
-
-int Foam::fv::warnUnboundedGauss
-(
-    Foam::debug::debugSwitch("warnUnboundedGauss", true)
-);
 
 makeFvConvectionScheme(gaussConvectionScheme)
 
