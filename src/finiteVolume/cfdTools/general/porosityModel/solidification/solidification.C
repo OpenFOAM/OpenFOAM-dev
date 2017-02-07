6d272d6c8c2e70c27f89590078d10d3d8656892e
Henry Weller <http://cfd.direct>
2017-02-07 19:02:30 +0000

    porosityModels::solidification: New porosity model to simulate solidification
    Description
        Simple solidification porosity model
    
        This is a simple approximation to solidification where the solid phase
        is represented as a porous blockage with the drag-coefficient evaluated from
    
            \f[
                S = - \rho D(T) U
            \f]
    
        where
        \vartable
            D(T) | User-defined drag-coefficient as function of temperature
        \endvartable
    
        Note that the latent heat of solidification is not included and the
        temperature is unchanged by the modelled change of phase.
    
        Example of the solidification model specification:
        \verbatim
            type            solidification;
    
            solidificationCoeffs
            {
                // Solidify between 330K and 330.5K
                D table
                (
                    (330.0     10000) // Solid below 330K
                    (330.5     0)     // Liquid above 330.5K
                );
    
                // Solidification porosity is isotropic
                // use the global coordinate system
                coordinateSystem
                {
                    type    cartesian;
                    origin  (0 0 0);
                    coordinateRotation
                    {
                        type    axesRotation;
                        e1      (1 0 0);
                        e2      (0 1 0);
                    }
                }
            }
        \endverbatim

---
+++ src/finiteVolume/cfdTools/general/porosityModel/solidification/solidification.C
@@ -0,0 +1,163 @@
+/*---------------------------------------------------------------------------*\
+  =========                 |
+  \\      /  F ield         | OpenFOAM: The Open Source CFD Toolbox
+   \\    /   O peration     |
+    \\  /    A nd           | Copyright (C) 2017 OpenFOAM Foundation
+     \\/     M anipulation  |
+-------------------------------------------------------------------------------
+License
+    This file is part of OpenFOAM.
+
+    OpenFOAM is free software: you can redistribute it and/or modify it
+    under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    OpenFOAM is distributed in the hope that it will be useful, but WITHOUT
+    ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+    FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License
+    for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with OpenFOAM.  If not, see <http://www.gnu.org/licenses/>.
+
+\*---------------------------------------------------------------------------*/
+
+#include "addToRunTimeSelectionTable.H"
+#include "solidification.H"
+#include "geometricOneField.H"
+#include "fvMatrices.H"
+
+// * * * * * * * * * * * * * * Static Data Members * * * * * * * * * * * * * //
+
+namespace Foam
+{
+    namespace porosityModels
+    {
+        defineTypeNameAndDebug(solidification, 0);
+        addToRunTimeSelectionTable(porosityModel, solidification, mesh);
+    }
+}
+
+
+// * * * * * * * * * * * * * * * * Constructors  * * * * * * * * * * * * * * //
+
+Foam::porosityModels::solidification::solidification
+(
+    const word& name,
+    const word& modelType,
+    const fvMesh& mesh,
+    const dictionary& dict,
+    const word& cellZoneName
+)
+:
+    porosityModel(name, modelType, mesh, dict, cellZoneName),
+    TName_(coeffs_.lookupOrDefault<word>("T", "T")),
+    rhoName_(coeffs_.lookupOrDefault<word>("rho", "rho")),
+    D_(Function1<scalar>::New("D", coeffs_))
+{}
+
+
+// * * * * * * * * * * * * * * * * Destructor  * * * * * * * * * * * * * * * //
+
+Foam::porosityModels::solidification::~solidification()
+{}
+
+
+// * * * * * * * * * * * * * * * Member Functions  * * * * * * * * * * * * * //
+
+void Foam::porosityModels::solidification::calcTransformModelData()
+{}
+
+
+void Foam::porosityModels::solidification::calcForce
+(
+    const volVectorField& U,
+    const volScalarField& rho,
+    const volScalarField& mu,
+    vectorField& force
+) const
+{
+    scalarField Udiag(U.size(), 0.0);
+    const scalarField& V = mesh_.V();
+
+    apply(Udiag, V, rho, U);
+
+    force = Udiag*U;
+}
+
+
+void Foam::porosityModels::solidification::correct
+(
+    fvVectorMatrix& UEqn
+) const
+{
+    const volVectorField& U = UEqn.psi();
+    const scalarField& V = mesh_.V();
+    scalarField& Udiag = UEqn.diag();
+
+    if (UEqn.dimensions() == dimForce)
+    {
+        const volScalarField& rho = mesh_.lookupObject<volScalarField>
+        (
+            IOobject::groupName(rhoName_, U.group())
+        );
+
+        apply(Udiag, V, rho, U);
+    }
+    else
+    {
+        apply(Udiag, V, geometricOneField(), U);
+    }
+}
+
+
+void Foam::porosityModels::solidification::correct
+(
+    fvVectorMatrix& UEqn,
+    const volScalarField& rho,
+    const volScalarField& mu
+) const
+{
+    const volVectorField& U = UEqn.psi();
+    const scalarField& V = mesh_.V();
+    scalarField& Udiag = UEqn.diag();
+
+    apply(Udiag, V, rho, U);
+}
+
+
+void Foam::porosityModels::solidification::correct
+(
+    const fvVectorMatrix& UEqn,
+    volTensorField& AU
+) const
+{
+    const volVectorField& U = UEqn.psi();
+
+    if (UEqn.dimensions() == dimForce)
+    {
+        const volScalarField& rho = mesh_.lookupObject<volScalarField>
+        (
+            IOobject::groupName(rhoName_, U.group())
+        );
+
+        apply(AU, rho, U);
+    }
+    else
+    {
+        apply(AU, geometricOneField(), U);
+    }
+}
+
+
+bool Foam::porosityModels::solidification::writeData(Ostream& os) const
+{
+    os  << indent << name_ << endl;
+    dict_.write(os);
+
+    return true;
+}
+
+
+// ************************************************************************* //
---
+++ src/finiteVolume/cfdTools/general/porosityModel/solidification/solidificationTemplates.C
@@ -0,0 +1,83 @@
+/*---------------------------------------------------------------------------*\
+  =========                 |
+  \\      /  F ield         | OpenFOAM: The Open Source CFD Toolbox
+   \\    /   O peration     |
+    \\  /    A nd           | Copyright (C) 2017 OpenFOAM Foundation
+     \\/     M anipulation  |
+-------------------------------------------------------------------------------
+License
+    This file is part of OpenFOAM.
+
+    OpenFOAM is free software: you can redistribute it and/or modify it
+    under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    OpenFOAM is distributed in the hope that it will be useful, but WITHOUT
+    ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+    FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License
+    for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with OpenFOAM.  If not, see <http://www.gnu.org/licenses/>.
+
+\*---------------------------------------------------------------------------*/
+
+#include "volFields.H"
+
+// * * * * * * * * * * * * * Private Member Functions  * * * * * * * * * * * //
+
+template<class RhoFieldType>
+void Foam::porosityModels::solidification::apply
+(
+    scalarField& Udiag,
+    const scalarField& V,
+    const RhoFieldType& rho,
+    const volVectorField& U
+) const
+{
+    const volScalarField& T = mesh_.lookupObject<volScalarField>
+    (
+        IOobject::groupName(TName_, U.group())
+    );
+
+    forAll(cellZoneIDs_, zoneI)
+    {
+        const labelList& cells = mesh_.cellZones()[cellZoneIDs_[zoneI]];
+
+        forAll(cells, i)
+        {
+            const label celli = cells[i];
+            Udiag[celli] += V[celli]*rho[celli]*D_->value(T[celli]);
+        }
+    }
+}
+
+
+template<class RhoFieldType>
+void Foam::porosityModels::solidification::apply
+(
+    tensorField& AU,
+    const RhoFieldType& rho,
+    const volVectorField& U
+) const
+{
+    const volScalarField& T = mesh_.lookupObject<volScalarField>
+    (
+        IOobject::groupName(TName_, U.group())
+    );
+
+    forAll(cellZoneIDs_, zoneI)
+    {
+        const labelList& cells = mesh_.cellZones()[cellZoneIDs_[zoneI]];
+
+        forAll(cells, i)
+        {
+            const label celli = cells[i];
+            AU[celli] += tensor::I*rho[celli]*D_->value(T[celli]);
+        }
+    }
+}
+
+
+// ************************************************************************* //
