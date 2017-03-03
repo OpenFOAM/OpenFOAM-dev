577b089a340a5d2295db1bbadfea9f538b6f353a
Henry Weller <http://cfd.direct>
2017-03-03 22:04:41 +0000

    GAMGSolverScale: minor update

--- src/OpenFOAM/matrices/lduMatrix/solvers/GAMG/GAMGSolverScale.C
+++ src/OpenFOAM/matrices/lduMatrix/solvers/GAMG/GAMGSolverScale.C
@@ -2,7 +2,7 @@
   =========                 |
   \\      /  F ield         | OpenFOAM: The Open Source CFD Toolbox
    \\    /   O peration     |
-    \\  /    A nd           | Copyright (C) 2011-2013 OpenFOAM Foundation
+    \\  /    A nd           | Copyright (C) 2011-2017 OpenFOAM Foundation
      \\/     M anipulation  |
 -------------------------------------------------------------------------------
 License
@@ -60,7 +60,7 @@ void Foam::GAMGSolver::scale
     vector2D scalingVector(scalingFactorNum, scalingFactorDenom);
     A.mesh().reduce(scalingVector, sumOp<vector2D>());
 
-    scalar sf = scalingVector.x()/stabilise(scalingVector.y(), VSMALL);
+    const scalar sf = scalingVector.x()/stabilise(scalingVector.y(), VSMALL);
 
     if (debug >= 2)
     {
