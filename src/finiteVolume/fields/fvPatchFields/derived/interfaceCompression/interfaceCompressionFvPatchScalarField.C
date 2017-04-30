8514ff55d08a91429ec2b2afae4ec11bb04bafb7
Henry Weller <http://cfd.direct>
2017-04-30 19:38:47 +0100

    VoF solvers: New interfaceCompressionFvPatchScalarField BC and additional shear compression
    Provides the additional compression necessary to ensure interface integrity
    adjacent to a boundary at a low angle of incidence to the interface.  This is
    particularly important when simulating planing hulls.

---
+++ src/finiteVolume/fields/fvPatchFields/derived/interfaceCompression/interfaceCompressionFvPatchScalarField.C
@@ -0,0 +1,127 @@
+/*---------------------------------------------------------------------------*\
+  =========                 |
+  \\      /  F ield         | OpenFOAM: The Open Source CFD Toolbox
+   \\    /   O peration     |
+    \\  /    A nd           | Copyright (C) 2017 OpenFOAM Foundation
+     \\/     M anipulation  |
+-------------------------------------------------------------------------------
+License
+    This file is part of OpenFOAM.
+
+    OpenFOAM is free software: you can redistribute it and/or modify it
+    under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    OpenFOAM is distributed in the hope that it will be useful, but WITHOUT
+    ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+    FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License
+    for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with OpenFOAM.  If not, see <http://www.gnu.org/licenses/>.
+
+\*---------------------------------------------------------------------------*/
+
+#include "interfaceCompressionFvPatchScalarField.H"
+#include "addToRunTimeSelectionTable.H"
+#include "fvPatchFieldMapper.H"
+
+// * * * * * * * * * * * * * * * * Constructors  * * * * * * * * * * * * * * //
+
+Foam::interfaceCompressionFvPatchScalarField::
+interfaceCompressionFvPatchScalarField
+(
+    const fvPatch& p,
+    const DimensionedField<scalar, volMesh>& iF
+)
+:
+    fixedValueFvPatchScalarField(p, iF)
+{}
+
+
+Foam::interfaceCompressionFvPatchScalarField::
+interfaceCompressionFvPatchScalarField
+(
+    const fvPatch& p,
+    const DimensionedField<scalar, volMesh>& iF,
+    const dictionary& dict
+)
+:
+    fixedValueFvPatchScalarField(p, iF)
+{
+    evaluate();
+}
+
+
+Foam::interfaceCompressionFvPatchScalarField::
+interfaceCompressionFvPatchScalarField
+(
+    const interfaceCompressionFvPatchScalarField& ptf,
+    const fvPatch& p,
+    const DimensionedField<scalar, volMesh>& iF,
+    const fvPatchFieldMapper& mapper
+)
+:
+    fixedValueFvPatchScalarField(ptf, p, iF, mapper)
+{}
+
+
+Foam::interfaceCompressionFvPatchScalarField::
+interfaceCompressionFvPatchScalarField
+(
+    const interfaceCompressionFvPatchScalarField& ptf
+)
+:
+    fixedValueFvPatchScalarField(ptf)
+{}
+
+
+Foam::interfaceCompressionFvPatchScalarField::
+interfaceCompressionFvPatchScalarField
+(
+    const interfaceCompressionFvPatchScalarField& ptf,
+    const DimensionedField<scalar, volMesh>& iF
+)
+:
+    fixedValueFvPatchScalarField(ptf, iF)
+{}
+
+
+// * * * * * * * * * * * * * * * Member Functions  * * * * * * * * * * * * * //
+
+void Foam::interfaceCompressionFvPatchScalarField::updateCoeffs()
+{
+    if (updated())
+    {
+        return;
+    }
+
+    operator==(pos(this->patchInternalField() - 0.5));
+
+    fixedValueFvPatchScalarField::updateCoeffs();
+}
+
+
+void Foam::interfaceCompressionFvPatchScalarField::write
+(
+    Ostream& os
+) const
+{
+    fvPatchScalarField::write(os);
+    writeEntry("value", os);
+}
+
+
+// * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * //
+
+namespace Foam
+{
+    makePatchTypeField
+    (
+        fvPatchScalarField,
+        interfaceCompressionFvPatchScalarField
+    );
+}
+
+// ************************************************************************* //
