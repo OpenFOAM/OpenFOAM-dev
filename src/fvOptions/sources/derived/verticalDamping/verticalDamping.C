813a67433a7cdeec908dc192b08982e11b14a0ed
Will Bainbridge <http://cfd.direct>
2017-05-26 12:17:58 +0100

    fvOptions: Added vertical damping
    This fvOption applies an explicit damping force to components of the
    vector field in the direction of gravity. Its intended purpose is to
    damp the vertical motions of an interface in the region approaching an
    outlet so that no reflections are generated. The level of damping is
    specified by a coefficient, lambda, given in units of 1/s.
    
    It can be enabled for a cellZone named "nearOutlet", by adding the
    following entry to constant/fvOptions:
    
        verticalDamping1
        {
            type            verticalDamping;
    
            selectionMode   cellZone;
            cellZone        nearOutlet;
    
            lambda          [0 0 -1 0 0 0 0] 1;
    
            timeStart       0;
            duration        1e6;
        }
    
    This work was supported by Jan Kaufmann and Jan Oberhagemann at DNV GL.

---
+++ src/fvOptions/sources/derived/verticalDamping/verticalDamping.C
@@ -0,0 +1,153 @@
+/*---------------------------------------------------------------------------*\
+  =========                 |
+  \\      /  F ield         | OpenFOAM: The Open Source CFD Toolbox
+   \\    /   O peration     |
+    \\  /    A nd           | Copyright (C) 2017 OpenFOAM Foundation
+     \\/     M anipulation  |
+-------------------------------------------------------------------------------
+License
+    This file is part of OpenFOAM.
+
+    OpenFOAM is free software: you can redistribute it and/or modify it
+    under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    OpenFOAM is distributed in the hope that it will be useful, but WITHOUT
+    ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+    FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License
+    for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with OpenFOAM.  If not, see <http://www.gnu.org/licenses/>.
+
+\*---------------------------------------------------------------------------*/
+
+#include "verticalDamping.H"
+#include "fvMesh.H"
+#include "fvMatrix.H"
+#include "geometricOneField.H"
+#include "meshTools.H"
+#include "uniformDimensionedFields.H"
+#include "addToRunTimeSelectionTable.H"
+
+// * * * * * * * * * * * * * * Static Data Members * * * * * * * * * * * * * //
+
+namespace Foam
+{
+namespace fv
+{
+    defineTypeNameAndDebug(verticalDamping, 0);
+    addToRunTimeSelectionTable(option, verticalDamping, dictionary);
+}
+}
+
+
+// * * * * * * * * * * * * * Private Member Functions  * * * * * * * * * * * //
+
+void Foam::fv::verticalDamping::addSup
+(
+    const volVectorField& alphaRhoU,
+    fvMatrix<vector>& eqn,
+    const label fieldi
+)
+{
+    const uniformDimensionedVectorField& g =
+        mesh_.lookupObject<uniformDimensionedVectorField>("g");
+
+    const dimensionedSymmTensor lgg(lambda_*sqr(g)/magSqr(g));
+
+    const DimensionedField<scalar, volMesh>& V = mesh_.V();
+
+    // Check dimensions
+    eqn.dimensions()
+      - V.dimensions()*(lgg.dimensions() & alphaRhoU.dimensions());
+
+    forAll(cells_, i)
+    {
+        const label c = cells_[i];
+        vector f = V[c]*(lgg.value() & alphaRhoU[c]);
+        meshTools::constrainDirection(mesh_, mesh_.solutionD(), f);
+        eqn.source()[c] += f;
+    }
+}
+
+
+// * * * * * * * * * * * * * * * * Constructors  * * * * * * * * * * * * * * //
+
+Foam::fv::verticalDamping::verticalDamping
+(
+    const word& name,
+    const word& modelType,
+    const dictionary& dict,
+    const fvMesh& mesh
+)
+:
+    cellSetOption(name, modelType, dict, mesh),
+    lambda_("lambda", dimless/dimTime, coeffs_.lookup("lambda"))
+{
+    read(dict);
+}
+
+
+// * * * * * * * * * * * * * * * Member Functions  * * * * * * * * * * * * * //
+
+void Foam::fv::verticalDamping::addSup
+(
+    fvMatrix<vector>& eqn,
+    const label fieldi
+)
+{
+    addSup(eqn.psi(), eqn, fieldi);
+}
+
+
+void Foam::fv::verticalDamping::addSup
+(
+    const volScalarField& rho,
+    fvMatrix<vector>& eqn,
+    const label fieldi
+)
+{
+    addSup(rho*eqn.psi(), eqn, fieldi);
+}
+
+
+void Foam::fv::verticalDamping::addSup
+(
+    const volScalarField& alpha,
+    const volScalarField& rho,
+    fvMatrix<vector>& eqn,
+    const label fieldi
+)
+{
+    addSup(alpha*rho*eqn.psi(), eqn, fieldi);
+}
+
+
+bool Foam::fv::verticalDamping::read(const dictionary& dict)
+{
+    if (cellSetOption::read(dict))
+    {
+        lambda_ =
+            dimensionedScalar
+            (
+                lambda_.name(),
+                lambda_.dimensions(),
+                coeffs_.lookup(lambda_.name())
+            );
+
+        fieldNames_ = wordList(1, coeffs_.lookupOrDefault<word>("U", "U"));
+
+        applied_.setSize(1, false);
+
+        return true;
+    }
+    else
+    {
+        return false;
+    }
+}
+
+
+// ************************************************************************* //
