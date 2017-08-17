5bea9ad62fddaa28107f7dd6d96bdac9e57deaa5
Henry Weller <http://openfoam.org>
2017-08-17 14:53:55 +0100

    Time::purgeWrite: Corrected handling of collated processors directory
    Resolves bug-report https://bugs.openfoam.org/view.php?id=2667

--- src/OpenFOAM/db/Time/TimeIO.C
+++ src/OpenFOAM/db/Time/TimeIO.C
@@ -551,7 +551,13 @@ bool Foam::Time::writeObject
 
                 while (previousWriteTimes_.size() > purgeWrite_)
                 {
-                    rmDir(objectRegistry::path(previousWriteTimes_.pop()));
+                    fileHandler().rmDir
+                    (
+                        fileHandler().filePath
+                        (
+                            objectRegistry::path(previousWriteTimes_.pop())
+                        )
+                    );
                 }
             }
         }
