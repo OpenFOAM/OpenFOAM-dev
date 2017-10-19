3055587a5c3d07affef86ed5e905a7b185b7b417
Will Bainbridge <http://cfd.direct>
2017-10-19 10:00:36 +0100

    semiPermeableBaffle: Added two new boundary conditions and a tutorial
    Two boundary conditions for the modelling of semi-permeable baffles have
    been added. These baffles are permeable to a number of species within
    the flow, and are impermeable to others. The flux of a given species is
    calculated as a constant multipled by the drop in mass fraction across
    the baffle.
    
    The species mass-fraction condition requires the transfer constant and
    the name of the patch on the other side of the baffle:
    
    boundaryField
    {
        // ...
    
        membraneA
        {
            type            semiPermeableBaffleMassFraction;
            samplePatch     membranePipe;
            c               0.1;
            value           uniform 0;
        }
        membraneB
        {
            type            semiPermeableBaffleMassFraction;
            samplePatch     membraneSleeve;
            c               0.1;
            value           uniform 1;
        }
    }
    
    If the value of c is omitted, or set to zero, then the patch is
    considered impermeable to the species in question. The samplePatch entry
    can also be omitted in this case.
    
    The velocity condition does not require any special input:
    
    boundaryField
    {
        // ...
    
        membraneA
        {
            type            semiPermeableBaffleVelocity;
            value           uniform (0 0 0);
        }
        membraneB
        {
            type            semiPermeableBaffleVelocity;
            value           uniform (0 0 0);
        }
    }
    
    These two boundary conditions must be used in conjunction, and the
    mass-fraction condition must be applied to all species in the
    simulation. The calculation will fail with an error message if either is
    used in isolation.
    
    A tutorial, combustion/reactingFoam/RAS/membrane, has been added which
    demonstrates this transfer process.
    
    This work was done with support from Stefan Lipp, at BASF.

---
+++ src/semiPermeableBaffle/derivedFvPatchFields/semiPermeableBaffleMassFraction/semiPermeableBaffleMassFractionFvPatchScalarField.C
@@ -0,0 +1,190 @@
+/*---------------------------------------------------------------------------*\
+  =========                 |
+  \\      /  F ield         | OpenFOAM: The Open Source CFD Toolbox
+   \\    /   O peration     |
+    \\  /    A nd           | Copyright (C) 2017 OpenFOAM Foundation
+     \\/     M anipulation  |
+-------------------------------------------------------------------------------
+License
+    This file is part of OpenFOAM.
+
+    OpenFOAM is free software: you can redistribute it and/or modify it
+    under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    OpenFOAM is distributed in the hope that it will be useful, but WITHOUT
+    ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+    FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License
+    for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with OpenFOAM.  If not, see <http://www.gnu.org/licenses/>.
+
+\*---------------------------------------------------------------------------*/
+
+#include "semiPermeableBaffleMassFractionFvPatchScalarField.H"
+#include "addToRunTimeSelectionTable.H"
+#include "fvPatchFieldMapper.H"
+#include "volFields.H"
+#include "surfaceFields.H"
+#include "turbulenceModel.H"
+
+// * * * * * * * * * * * * * * * * Constructors  * * * * * * * * * * * * * * //
+
+Foam::semiPermeableBaffleMassFractionFvPatchScalarField::
+semiPermeableBaffleMassFractionFvPatchScalarField
+(
+    const fvPatch& p,
+    const DimensionedField<scalar, volMesh>& iF
+)
+:
+    mappedPatchBase(p.patch()),
+    mixedFvPatchScalarField(p, iF),
+    c_(0),
+    phiName_("phi")
+{
+    refValue() = Zero;
+    refGrad() = Zero;
+    valueFraction() = Zero;
+}
+
+
+Foam::semiPermeableBaffleMassFractionFvPatchScalarField::
+semiPermeableBaffleMassFractionFvPatchScalarField
+(
+    const fvPatch& p,
+    const DimensionedField<scalar, volMesh>& iF,
+    const dictionary& dict
+)
+:
+    mappedPatchBase(p.patch(), NEARESTPATCHFACE, dict),
+    mixedFvPatchScalarField(p, iF),
+    c_(dict.lookupOrDefault<scalar>("c", scalar(0))),
+    phiName_(dict.lookupOrDefault<word>("phi", "phi"))
+{
+    fvPatchScalarField::operator=(scalarField("value", dict, p.size()));
+
+    refValue() = Zero;
+    refGrad() = Zero;
+    valueFraction() = Zero;
+}
+
+
+Foam::semiPermeableBaffleMassFractionFvPatchScalarField::
+semiPermeableBaffleMassFractionFvPatchScalarField
+(
+    const semiPermeableBaffleMassFractionFvPatchScalarField& ptf,
+    const fvPatch& p,
+    const DimensionedField<scalar, volMesh>& iF,
+    const fvPatchFieldMapper& mapper
+)
+:
+    mappedPatchBase(p.patch(), ptf),
+    mixedFvPatchScalarField(ptf, p, iF, mapper),
+    c_(ptf.c_),
+    phiName_(ptf.phiName_)
+{}
+
+
+Foam::semiPermeableBaffleMassFractionFvPatchScalarField::
+semiPermeableBaffleMassFractionFvPatchScalarField
+(
+    const semiPermeableBaffleMassFractionFvPatchScalarField& ptf
+)
+:
+    mappedPatchBase(ptf.patch().patch(), ptf),
+    mixedFvPatchScalarField(ptf),
+    c_(ptf.c_),
+    phiName_(ptf.phiName_)
+{}
+
+
+Foam::semiPermeableBaffleMassFractionFvPatchScalarField::
+semiPermeableBaffleMassFractionFvPatchScalarField
+(
+    const semiPermeableBaffleMassFractionFvPatchScalarField& ptf,
+    const DimensionedField<scalar, volMesh>& iF
+)
+:
+    mappedPatchBase(ptf.patch().patch(), ptf),
+    mixedFvPatchScalarField(ptf, iF),
+    c_(ptf.c_),
+    phiName_(ptf.phiName_)
+{}
+
+
+// * * * * * * * * * * * * * * * Member Functions  * * * * * * * * * * * * * //
+
+Foam::tmp<Foam::scalarField>
+Foam::semiPermeableBaffleMassFractionFvPatchScalarField::phiY() const
+{
+    if (c_ == scalar(0))
+    {
+        return tmp<scalarField>(new scalarField(patch().size(), Zero));
+    }
+
+    const word& YName = internalField().name();
+
+    const label nbrPatchi = samplePolyPatch().index();
+    const fvPatch& nbrPatch = patch().boundaryMesh()[nbrPatchi];
+
+    const fvPatchScalarField& nbrYp =
+        nbrPatch.lookupPatchField<volScalarField, scalar>(YName);
+    scalarField nbrYc(nbrYp.patchInternalField());
+    mappedPatchBase::map().distribute(nbrYc);
+
+    return c_*patch().magSf()*(patchInternalField() - nbrYc);
+}
+
+
+void Foam::semiPermeableBaffleMassFractionFvPatchScalarField::updateCoeffs()
+{
+    if (updated())
+    {
+        return;
+    }
+
+    const scalarField& phip =
+        patch().lookupPatchField<surfaceScalarField, scalar>(phiName_);
+
+    const turbulenceModel& turbModel =
+        db().lookupObject<turbulenceModel>
+        (
+            turbulenceModel::propertiesName
+        );
+    const scalarField muEffp(turbModel.muEff(patch().index()));
+    const scalarField AMuEffp(patch().magSf()*muEffp);
+
+    valueFraction() = phip/(phip - patch().deltaCoeffs()*AMuEffp);
+    refGrad() = - phiY()/AMuEffp;
+
+    mixedFvPatchScalarField::updateCoeffs();
+}
+
+
+void Foam::semiPermeableBaffleMassFractionFvPatchScalarField::write
+(
+    Ostream& os
+) const
+{
+    fvPatchScalarField::write(os);
+    mappedPatchBase::write(os);
+    writeEntryIfDifferent<scalar>(os, "c", scalar(0), c_);
+    writeEntryIfDifferent<word>(os, "phi", "phi", phiName_);
+    writeEntry("value", os);
+}
+
+
+// * * * * * * * * * * * * * * Build Macro Function  * * * * * * * * * * * * //
+
+namespace Foam
+{
+    makePatchTypeField
+    (
+        fvPatchScalarField,
+        semiPermeableBaffleMassFractionFvPatchScalarField
+    );
+}
+
+// ************************************************************************* //
