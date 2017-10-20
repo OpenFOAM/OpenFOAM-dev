aaa8405bd1d006a9b35877fb232a6eebf5b01db5
Henry Weller <http://openfoam.org>
2017-10-20 11:33:38 +0100

    dictionary::functionEntry: Write new line at end of entry
    Resolves bug-report https://bugs.openfoam.org/view.php?id=2726

--- src/OpenFOAM/db/dictionary/functionEntries/functionEntry/functionEntry.C
+++ src/OpenFOAM/db/dictionary/functionEntries/functionEntry/functionEntry.C
@@ -2,7 +2,7 @@
   =========                 |
   \\      /  F ield         | OpenFOAM: The Open Source CFD Toolbox
    \\    /   O peration     |
-    \\  /    A nd           | Copyright (C) 2011-2016 OpenFOAM Foundation
+    \\  /    A nd           | Copyright (C) 2011-2017 OpenFOAM Foundation
      \\/     M anipulation  |
 -------------------------------------------------------------------------------
 License
@@ -172,6 +172,8 @@ void Foam::functionEntry::write(Ostream& os) const
     {
         os.write(s[i]);
     }
+
+    os << endl;
 }
 
 
