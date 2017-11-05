c2835a450b2990a8d2d496ad3507f81ba743ea08
Henry Weller <http://openfoam.org>
2017-11-05 10:58:07 +0000

    functionObjects::ddt: New functionObject which calculates and writes the Eulerian time derivative of a field
    Based on patch contributed by Tobias Holzmann
    Resolves feature-request https://bugs.openfoam.org/view.php?id=2525

---
+++ src/functionObjects/field/ddt/ddt.C
@@ -0,0 +1,87 @@
+/*---------------------------------------------------------------------------*\
+  =========                 |
+  \\      /  F ield         | OpenFOAM: The Open Source CFD Toolbox
+   \\    /   O peration     |
+    \\  /    A nd           | Copyright (C) 2017 OpenFOAM Foundation
+     \\/     M anipulation  |
+-------------------------------------------------------------------------------
+License
+    This file is part of OpenFOAM.
+
+    OpenFOAM is free software: you can redistribute it and/or modify it
+    under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    OpenFOAM is distributed in the hope that it will be useful, but WITHOUT
+    ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+    FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License
+    for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with OpenFOAM.  If not, see <http://www.gnu.org/licenses/>.
+
+\*---------------------------------------------------------------------------*/
+
+#include "ddt.H"
+#include "addToRunTimeSelectionTable.H"
+
+// * * * * * * * * * * * * * * Static Data Members * * * * * * * * * * * * * //
+
+namespace Foam
+{
+namespace functionObjects
+{
+    defineTypeNameAndDebug(ddt, 0);
+    addToRunTimeSelectionTable(functionObject, ddt, dictionary);
+}
+}
+
+
+// * * * * * * * * * * * * * Private Member Functions  * * * * * * * * * * * //
+
+bool Foam::functionObjects::ddt::calc()
+{
+    if (functionObject::postProcess)
+    {
+        WarningInFunction
+            << "ddt is not supported with the postProcess utility"
+            << endl;
+
+        return false;
+    }
+
+    bool processed = false;
+
+    processed = processed || calcDdt<scalar>();
+    processed = processed || calcDdt<vector>();
+    processed = processed || calcDdt<sphericalTensor>();
+    processed = processed || calcDdt<symmTensor>();
+    processed = processed || calcDdt<tensor>();
+
+    return processed;
+}
+
+
+// * * * * * * * * * * * * * * * * Constructors  * * * * * * * * * * * * * * //
+
+Foam::functionObjects::ddt::ddt
+(
+    const word& name,
+    const Time& runTime,
+    const dictionary& dict
+)
+:
+    fieldExpression(name, runTime, dict)
+{
+    read(dict);
+}
+
+
+// * * * * * * * * * * * * * * * * Destructor  * * * * * * * * * * * * * * * //
+
+Foam::functionObjects::ddt::~ddt()
+{}
+
+
+// ************************************************************************* //
