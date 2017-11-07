204c6ee449ddf6eb416f648ccbb4c18d4ac04002
Henry Weller <http://openfoam.org>
2017-11-07 13:53:46 +0000

    basicThermo, heSolidThermo: Added support for specifying the boundary conditions of alpha
    Patch contributed by Björn Pfeiffelmann
    Resolves patch request https://bugs.openfoam.org/view.php?id=2713

--- src/thermophysicalModels/solidThermo/solidThermo/heSolidThermo.C
+++ src/thermophysicalModels/solidThermo/solidThermo/heSolidThermo.C
@@ -2,7 +2,7 @@
   =========                 |
   \\      /  F ield         | OpenFOAM: The Open Source CFD Toolbox
    \\    /   O peration     |
-    \\  /    A nd           | Copyright (C) 2011-2016 OpenFOAM Foundation
+    \\  /    A nd           | Copyright (C) 2011-2017 OpenFOAM Foundation
      \\/     M anipulation  |
 -------------------------------------------------------------------------------
 License
@@ -134,6 +134,8 @@ void Foam::heSolidThermo<BasicSolidThermo, MixtureType>::calculate()
             }
         }
     }
+
+    this->alpha_.correctBoundaryConditions();
 }
 
 
