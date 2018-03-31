b17c0e3fa13e1107aaac1f3d92f74c5f729d463c
Henry Weller <http://openfoam.org>
2018-03-31 21:47:58 +0100

    PrghPressureFvPatchScalarField: New base class used to create p_rgh variants of pressure BCs
    The prghPressureFvPatchScalarField, prghTotalPressureFvPatchScalarField and
    prghUniformDensityHydrostaticPressure p_rgh boundary conditions are now derived
    from the corresponding pressure boundary conditions using the
    PrghPressureFvPatchScalarField template.

---
+++ src/finiteVolume/fields/fvPatchFields/derived/PrghPressure/PrghPressureFvPatchScalarField.C
@@ -0,0 +1,135 @@
+/*---------------------------------------------------------------------------*\
+  =========                 |
+  \\      /  F ield         | OpenFOAM: The Open Source CFD Toolbox
+   \\    /   O peration     |
+    \\  /    A nd           | Copyright (C) 2018 OpenFOAM Foundation
+     \\/     M anipulation  |
+-------------------------------------------------------------------------------
+License
+    This file is part of OpenFOAM.
+
+    OpenFOAM is free software: you can redistribute it and/or modify it
+    under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    OpenFOAM is distributed in the hope that it will be useful, but WITHOUT
+    ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+    FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License
+    for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with OpenFOAM.  If not, see <http://www.gnu.org/licenses/>.
+
+\*---------------------------------------------------------------------------*/
+
+#include "PrghPressureFvPatchScalarField.H"
+#include "addToRunTimeSelectionTable.H"
+#include "fvPatchFieldMapper.H"
+#include "volFields.H"
+#include "uniformDimensionedFields.H"
+
+// * * * * * * * * * * * * * * * * Constructors  * * * * * * * * * * * * * * //
+
+template<class PressureFvPatchScalarField>
+Foam::PrghPressureFvPatchScalarField<PressureFvPatchScalarField>::
+PrghPressureFvPatchScalarField
+(
+    const fvPatch& p,
+    const DimensionedField<scalar, volMesh>& iF
+)
+:
+    PressureFvPatchScalarField(p, iF)
+{}
+
+
+template<class PressureFvPatchScalarField>
+Foam::PrghPressureFvPatchScalarField<PressureFvPatchScalarField>::
+PrghPressureFvPatchScalarField
+(
+    const fvPatch& p,
+    const DimensionedField<scalar, volMesh>& iF,
+    const dictionary& dict
+)
+:
+    PressureFvPatchScalarField(p, iF, dict)
+{}
+
+
+template<class PressureFvPatchScalarField>
+Foam::PrghPressureFvPatchScalarField<PressureFvPatchScalarField>::
+PrghPressureFvPatchScalarField
+(
+    const PrghPressureFvPatchScalarField& ptf,
+    const fvPatch& p,
+    const DimensionedField<scalar, volMesh>& iF,
+    const fvPatchFieldMapper& mapper
+)
+:
+    PressureFvPatchScalarField(ptf, p, iF, mapper)
+{}
+
+
+template<class PressureFvPatchScalarField>
+Foam::PrghPressureFvPatchScalarField<PressureFvPatchScalarField>::
+PrghPressureFvPatchScalarField
+(
+    const PrghPressureFvPatchScalarField& ptf
+)
+:
+    PressureFvPatchScalarField(ptf)
+{}
+
+
+template<class PressureFvPatchScalarField>
+Foam::PrghPressureFvPatchScalarField<PressureFvPatchScalarField>::
+PrghPressureFvPatchScalarField
+(
+    const PrghPressureFvPatchScalarField& ptf,
+    const DimensionedField<scalar, volMesh>& iF
+)
+:
+    PressureFvPatchScalarField(ptf, iF)
+{}
+
+
+// * * * * * * * * * * * * * * * Member Functions  * * * * * * * * * * * * * //
+
+template<class PressureFvPatchScalarField>
+void Foam::PrghPressureFvPatchScalarField<PressureFvPatchScalarField>::
+updateCoeffs()
+{
+    if (PressureFvPatchScalarField::updated())
+    {
+        return;
+    }
+
+    PressureFvPatchScalarField::updateCoeffs();
+
+    const scalarField& rhop = this->patch().template
+        lookupPatchField<volScalarField, scalar>
+        (
+            "rho"
+        );
+
+    const uniformDimensionedVectorField& g =
+        this->db().template lookupObject<uniformDimensionedVectorField>("g");
+
+    const uniformDimensionedScalarField& hRef =
+        this->db().template lookupObject<uniformDimensionedScalarField>("hRef");
+
+    dimensionedScalar ghRef
+    (
+        mag(g.value()) > small
+      ? g & (cmptMag(g.value())/mag(g.value()))*hRef
+      : dimensionedScalar("ghRef", g.dimensions()*dimLength, 0)
+    );
+
+    this->operator==
+    (
+        *this - rhop*((g.value() & this->patch().Cf()) - ghRef.value())
+    );
+}
+
+
+// ************************************************************************* //
--- src/finiteVolume/fields/fvPatchFields/derived/pressure/pressureFvPatchScalarField.C
+++ src/finiteVolume/fields/fvPatchFields/derived/pressure/pressureFvPatchScalarField.C
@@ -23,7 +23,7 @@ License
 
 \*---------------------------------------------------------------------------*/
 
-#include "prghUniformDensityHydrostaticPressureFvPatchScalarField.H"
+#include "pressureFvPatchScalarField.H"
 #include "addToRunTimeSelectionTable.H"
 #include "fvPatchFieldMapper.H"
 #include "volFields.H"
@@ -31,22 +31,18 @@ License
 
 // * * * * * * * * * * * * * * * * Constructors  * * * * * * * * * * * * * * //
 
-Foam::prghUniformDensityHydrostaticPressureFvPatchScalarField::
-prghUniformDensityHydrostaticPressureFvPatchScalarField
+Foam::pressureFvPatchScalarField::pressureFvPatchScalarField
 (
     const fvPatch& p,
     const DimensionedField<scalar, volMesh>& iF
 )
 :
     fixedValueFvPatchScalarField(p, iF),
-    pRef_(0),
-    rhoRef_(0),
-    rhoName_("rho")
+    p_(p.size(), 0.0)
 {}
 
 
-Foam::prghUniformDensityHydrostaticPressureFvPatchScalarField::
-prghUniformDensityHydrostaticPressureFvPatchScalarField
+Foam::pressureFvPatchScalarField::pressureFvPatchScalarField
 (
     const fvPatch& p,
     const DimensionedField<scalar, volMesh>& iF,
@@ -54,9 +50,7 @@ prghUniformDensityHydrostaticPressureFvPatchScalarField
 )
 :
     fixedValueFvPatchScalarField(p, iF, dict, false),
-    pRef_(readScalar(dict.lookup("pRef"))),
-    rhoRef_(readScalar(dict.lookup("rhoRef"))),
-    rhoName_(dict.lookupOrDefault<word>("rho", "rho"))
+    p_("p", dict, p.size())
 {
     if (dict.found("value"))
     {
@@ -67,100 +61,89 @@ prghUniformDensityHydrostaticPressureFvPatchScalarField
     }
     else
     {
-        fvPatchField<scalar>::operator=(pRef_);
+        fvPatchField<scalar>::operator=(p_);
     }
 }
 
 
-Foam::prghUniformDensityHydrostaticPressureFvPatchScalarField::
-prghUniformDensityHydrostaticPressureFvPatchScalarField
+Foam::pressureFvPatchScalarField::pressureFvPatchScalarField
 (
-    const prghUniformDensityHydrostaticPressureFvPatchScalarField& ptf,
+    const pressureFvPatchScalarField& ptf,
     const fvPatch& p,
     const DimensionedField<scalar, volMesh>& iF,
     const fvPatchFieldMapper& mapper
 )
 :
     fixedValueFvPatchScalarField(ptf, p, iF, mapper),
-    pRef_(ptf.pRef_),
-    rhoRef_(ptf.rhoRef_),
-    rhoName_(ptf.rhoName_)
+    p_(ptf.p_, mapper)
 {}
 
 
-Foam::prghUniformDensityHydrostaticPressureFvPatchScalarField::
-prghUniformDensityHydrostaticPressureFvPatchScalarField
+Foam::pressureFvPatchScalarField::pressureFvPatchScalarField
 (
-    const prghUniformDensityHydrostaticPressureFvPatchScalarField& ptf
+    const pressureFvPatchScalarField& ptf
 )
 :
     fixedValueFvPatchScalarField(ptf),
-    pRef_(ptf.pRef_),
-    rhoRef_(ptf.rhoRef_),
-    rhoName_(ptf.rhoName_)
+    p_(ptf.p_)
 {}
 
 
-Foam::prghUniformDensityHydrostaticPressureFvPatchScalarField::
-prghUniformDensityHydrostaticPressureFvPatchScalarField
+Foam::pressureFvPatchScalarField::pressureFvPatchScalarField
 (
-    const prghUniformDensityHydrostaticPressureFvPatchScalarField& ptf,
+    const pressureFvPatchScalarField& ptf,
     const DimensionedField<scalar, volMesh>& iF
 )
 :
     fixedValueFvPatchScalarField(ptf, iF),
-    pRef_(ptf.pRef_),
-    rhoRef_(ptf.rhoRef_),
-    rhoName_(ptf.rhoName_)
+    p_(ptf.p_)
 {}
 
 
 // * * * * * * * * * * * * * * * Member Functions  * * * * * * * * * * * * * //
 
-void Foam::prghUniformDensityHydrostaticPressureFvPatchScalarField::
-updateCoeffs()
+void Foam::pressureFvPatchScalarField::autoMap
+(
+    const fvPatchFieldMapper& m
+)
+{
+    fixedValueFvPatchScalarField::autoMap(m);
+    p_.autoMap(m);
+}
+
+
+void Foam::pressureFvPatchScalarField::rmap
+(
+    const fvPatchScalarField& ptf,
+    const labelList& addr
+)
+{
+    fixedValueFvPatchScalarField::rmap(ptf, addr);
+
+    const pressureFvPatchScalarField& tiptf =
+        refCast<const pressureFvPatchScalarField>(ptf);
+
+    p_.rmap(tiptf.p_, addr);
+}
+
+
+void Foam::pressureFvPatchScalarField::updateCoeffs()
 {
     if (updated())
     {
         return;
     }
 
-    const scalarField& rhop = patch().lookupPatchField<volScalarField, scalar>
-    (
-        rhoName_
-    );
-
-    const uniformDimensionedVectorField& g =
-        db().lookupObject<uniformDimensionedVectorField>("g");
-
-    const uniformDimensionedScalarField& hRef =
-        db().lookupObject<uniformDimensionedScalarField>("hRef");
-
-    dimensionedScalar ghRef
-    (
-        mag(g.value()) > small
-      ? g & (cmptMag(g.value())/mag(g.value()))*hRef
-      : dimensionedScalar("ghRef", g.dimensions()*dimLength, 0)
-    );
-
-    operator==
-    (
-        pRef_ - (rhop - rhoRef_)*((g.value() & patch().Cf()) - ghRef.value())
-    );
+    operator==(p_);
 
     fixedValueFvPatchScalarField::updateCoeffs();
 }
 
 
-void Foam::prghUniformDensityHydrostaticPressureFvPatchScalarField::write
-(
-    Ostream& os
-) const
+void Foam::pressureFvPatchScalarField::write(Ostream& os) const
 {
     fvPatchScalarField::write(os);
-    os.writeKeyword("pRef") << pRef_ << token::END_STATEMENT << nl;
-    os.writeKeyword("rhoRef") << rhoRef_ << token::END_STATEMENT << nl;
-    writeEntryIfDifferent<word>(os, "rho", "rho", rhoName_);
+    p_.writeEntry("p", os);
     writeEntry("value", os);
 }
 
@@ -172,7 +155,7 @@ namespace Foam
     makePatchTypeField
     (
         fvPatchScalarField,
-        prghUniformDensityHydrostaticPressureFvPatchScalarField
+        pressureFvPatchScalarField
     );
 }
 
