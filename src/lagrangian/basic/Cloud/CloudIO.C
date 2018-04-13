a1cc703bf8434b80f406d51cb157a97e188baf0c
Will Bainbridge <http://cfd.direct>
2018-04-13 14:25:28 +0100

    Cloud: Removed unused data

--- src/lagrangian/basic/Cloud/CloudIO.C
+++ src/lagrangian/basic/Cloud/CloudIO.C
@@ -2,7 +2,7 @@
   =========                 |
   \\      /  F ield         | OpenFOAM: The Open Source CFD Toolbox
    \\    /   O peration     |
-    \\  /    A nd           | Copyright (C) 2011-2017 OpenFOAM Foundation
+    \\  /    A nd           | Copyright (C) 2011-2018 OpenFOAM Foundation
      \\/     M anipulation  |
 -------------------------------------------------------------------------------
 License
@@ -149,8 +149,7 @@ Foam::Cloud<ParticleType>::Cloud
 :
     cloud(pMesh, cloudName),
     polyMesh_(pMesh),
-    labels_(),
-    cellWallFacesPtr_()
+    globalPositionsPtr_()
 {
     checkPatches();
 
