OpenFOAM/OpenFOAM-dev

commit 89de53492334abe52356c4c15c4ce28c1ec30f26

particle: Improved robustness of locate method

src/lagrangian/basic/particle/particle.C
@@ -409,20 +409,55 @@ void Foam::particle::locate
             << exit(FatalError);
     }
 
-    // Put the particle at the cell centre and in a random tet
+    // Track from the centre of the cell to the desired position
+    const vector displacement = position - mesh_.cellCentres()[celli_];
+
+    // Loop all cell tets to find the one containing the position. Track
+    // through each tet from the cell centre. If a tet contains the position
+    // then the track will end with a single trackToTri.
+    const class cell& c = mesh_.cells()[celli_];
+    label minF = 1, minTetFacei = -1, minTetPti = -1;
+    forAll(c, cellTetFacei)
+    {
+        const class face& f = mesh_.faces()[c[cellTetFacei]];
+        for (label tetPti = 1; tetPti < f.size() - 1; ++ tetPti)
+        {
+            coordinates_ = barycentric(1, 0, 0, 0);
+            tetFacei_ = c[cellTetFacei];
+            tetPti_ = tetPti;
+            facei_ = -1;
+
+            label tetTriI = -1;
+            const scalar f = trackToTri(displacement, 0, tetTriI);
+
+            if (tetTriI == -1)
+            {
+                return;
+            }
+
+            if (f < minF)
+            {
+                minF = f;
+                minTetFacei = tetFacei_;
+                minTetPti = tetPti_;
+            }
+        }
+    }
+
+    // The particle must be (hopefully only slightly) outside the cell. Track
+    // into the tet which got the furthest.
     coordinates_ = barycentric(1, 0, 0, 0);
-    tetFacei_ = mesh_.cells()[celli_][0];
-    tetPti_ = 1;
+    tetFacei_ = minTetFacei;
+    tetPti_ = minTetPti;
     facei_ = -1;
 
-    // Track to the injection point
-    track(position - mesh_.cellCentres()[celli_], 0);
+    track(displacement, 0);
     if (!onFace())
     {
         return;
     }
 
-    // We hit a boundary ...
+    // If we are here then we hit a boundary
     if (boundaryFail)
     {
         FatalErrorInFunction << boundaryMsg << exit(FatalError);
@@ -440,13 +475,12 @@ void Foam::particle::locate
         }
 
         const vector n = *direction/mag(*direction);
-        const vector s = position - mesh_.cellCentres()[celli_];
-        const vector sN = (s & n)*n;
-        const vector sT = s - sN;
+        const vector sN = (displacement & n)*n;
+        const vector sT = displacement - sN;
 
         coordinates_ = barycentric(1, 0, 0, 0);
-        tetFacei_ = mesh_.cells()[celli_][0];
-        tetPti_ = 1;
+        tetFacei_ = minTetFacei;
+        tetPti_ = minTetPti;
         facei_ = -1;
 
         track(sT, 0);

commit 50a965f8866683a81d79cbc7811af7333baf9d10

particle: Fixed topology error in locate method
Fixes problem introduced by commit 89de5349

src/lagrangian/basic/particle/particle.C
@@ -390,24 +390,23 @@ void Foam::particle::locate
 (
     const vector& position,
     const vector* direction,
-    const label celli,
+    label celli,
     const bool boundaryFail,
     const string boundaryMsg
 )
 {
-    celli_ = celli;
-
     // Find the cell, if it has not been given
-    if (celli_ < 0)
+    if (celli < 0)
     {
-        celli_ = mesh_.cellTree().findInside(position);
+        celli = mesh_.cellTree().findInside(position);
     }
-    if (celli_ < 0)
+    if (celli < 0)
     {
         FatalErrorInFunction
             << "Cell not found for particle position " << position << "."
             << exit(FatalError);
     }
+    celli_ = celli;
 
     // Track from the centre of the cell to the desired position
     const vector displacement = position - mesh_.cellCentres()[celli_];
@@ -479,6 +478,7 @@ void Foam::particle::locate
         const vector sT = displacement - sN;
 
         coordinates_ = barycentric(1, 0, 0, 0);
+        celli_ = celli;
         tetFacei_ = minTetFacei;
         tetPti_ = minTetPti;
         facei_ = -1;

src/lagrangian/basic/particle/particle.H
@@ -275,7 +275,7 @@ private:
             (
                 const vector& position,
                 const vector* direction,
-                const label celli,
+                label celli,
                 const bool boundaryFail,
                 const string boundaryMsg
             );