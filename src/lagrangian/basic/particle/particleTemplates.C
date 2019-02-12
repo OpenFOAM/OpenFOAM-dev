OpenFOAM/OpenFOAM-dev

commit 81947c80859e6051aca73de4b766309fc7f0b60d

particle: Optimisation by inlining key methods
Resolves bug report https://bugs.openfoam.org/view.php?id=2871

src/lagrangian/basic/particle/particle.H
@@ -168,7 +168,7 @@ private:
         // Tetrahedra functions
 
             //- Get the vertices of the current tet
-            void stationaryTetGeometry
+            inline void stationaryTetGeometry
             (
                 vector& centre,
                 vector& base,
@@ -181,7 +181,7 @@ private:
             //  cartesian position in the global coordinate system. The
             //  conversion is x = A & y, where x is the cartesian position, y is
             //  the barycentric position and A is the transformation tensor.
-            barycentricTensor stationaryTetTransform() const;
+            inline barycentricTensor stationaryTetTransform() const;
 
             //- Get the reverse transform associated with the current tet. The
             //  conversion is detA*y = (x - centre) & T. The variables x, y and
@@ -200,7 +200,7 @@ private:
             //- Get the vertices of the current moving tet. Two values are
             //  returned for each vertex. The first is a constant, and the
             //  second is a linear coefficient of the track fraction.
-            void movingTetGeometry
+            inline void movingTetGeometry
             (
                 const scalar endStepFraction,
                 Pair<vector>& centre,
@@ -213,7 +213,7 @@ private:
             //  This is of the same form as for the static case. As with the
             //  moving geometry, a linear function of the tracking fraction is
             //  returned for each component.
-            Pair<barycentricTensor> movingTetTransform
+            inline Pair<barycentricTensor> movingTetTransform
             (
                 const scalar endStepFraction
             ) const;
@@ -570,7 +570,7 @@ public:
     // Patch data
 
         //- Get the normal and velocity of the current patch location
-        void patchData(vector& n, vector& U) const;
+        inline void patchData(vector& n, vector& U) const;
 
 
     // Transformations

src/lagrangian/basic/particle/particleTemplates.C
@@ -122,7 +122,9 @@ void Foam::particle::hitFace
     }
     else if (onBoundaryFace())
     {
-        if(!p.hitPatch(cloud, ttd))
+        changeToMasterPatch();
+
+        if (!p.hitPatch(cloud, ttd))
         {
             const polyPatch& patch = mesh_.boundaryMesh()[p.patch()];
 
@@ -177,11 +179,6 @@ void Foam::particle::trackToAndHitFace
 )
 {
     trackToFace(direction, fraction);
-
-    if (onBoundaryFace())
-    {
-        changeToMasterPatch();
-    }
 
     hitFace(direction, cloud, td);
 }

src/lagrangian/basic/particle/particle.C
@@ -42,34 +42,6 @@ namespace Foam
 
 // * * * * * * * * * * * * * Private Member Functions  * * * * * * * * * * * //
 
-void Foam::particle::stationaryTetGeometry
-(
-    vector& centre,
-    vector& base,
-    vector& vertex1,
-    vector& vertex2
-) const
-{
-    const triFace triIs(currentTetIndices().faceTriIs(mesh_));
-    const vectorField& ccs = mesh_.cellCentres();
-    const pointField& pts = mesh_.points();
-
-    centre = ccs[celli_];
-    base = pts[triIs[0]];
-    vertex1 = pts[triIs[1]];
-    vertex2 = pts[triIs[2]];
-}
-
-
-Foam::barycentricTensor Foam::particle::stationaryTetTransform() const
-{
-    vector centre, base, vertex1, vertex2;
-    stationaryTetGeometry(centre, base, vertex1, vertex2);
-
-    return barycentricTensor(centre, base, vertex1, vertex2);
-}
-
-
 void Foam::particle::stationaryTetReverseTransform
 (
     vector& centre,
@@ -96,61 +68,6 @@ void Foam::particle::stationaryTetReverseTransform
         ad ^ ab,
         ab ^ ac
     );
-}
-
-
-void Foam::particle::movingTetGeometry
-(
-    const scalar fraction,
-    Pair<vector>& centre,
-    Pair<vector>& base,
-    Pair<vector>& vertex1,
-    Pair<vector>& vertex2
-) const
-{
-    const triFace triIs(currentTetIndices().faceTriIs(mesh_));
-    const pointField& ptsOld = mesh_.oldPoints();
-    const pointField& ptsNew = mesh_.points();
-
-    // !!! <-- We would be better off using mesh_.cellCentres() here. However,
-    // we need to put a mesh_.oldCellCentres() method in for this to work. The
-    // values obtained from the mesh and those obtained from the cell do not
-    // necessarily match. See mantis #1993.
-    const vector ccOld = mesh_.cells()[celli_].centre(ptsOld, mesh_.faces());
-    const vector ccNew = mesh_.cells()[celli_].centre(ptsNew, mesh_.faces());
-
-    // Old and new points and cell centres are not sub-cycled. If we are sub-
-    // cycling, then we have to account for the timestep change here by
-    // modifying the fractions that we take of the old and new geometry.
-    const Pair<scalar> s = stepFractionSpan();
-    const scalar f0 = s[0] + stepFraction_*s[1], f1 = fraction*s[1];
-
-    centre[0] = ccOld + f0*(ccNew - ccOld);
-    base[0] = ptsOld[triIs[0]] + f0*(ptsNew[triIs[0]] - ptsOld[triIs[0]]);
-    vertex1[0] = ptsOld[triIs[1]] + f0*(ptsNew[triIs[1]] - ptsOld[triIs[1]]);
-    vertex2[0] = ptsOld[triIs[2]] + f0*(ptsNew[triIs[2]] - ptsOld[triIs[2]]);
-
-    centre[1] = f1*(ccNew - ccOld);
-    base[1] = f1*(ptsNew[triIs[0]] - ptsOld[triIs[0]]);
-    vertex1[1] = f1*(ptsNew[triIs[1]] - ptsOld[triIs[1]]);
-    vertex2[1] = f1*(ptsNew[triIs[2]] - ptsOld[triIs[2]]);
-}
-
-
-Foam::Pair<Foam::barycentricTensor> Foam::particle::movingTetTransform
-(
-    const scalar fraction
-) const
-{
-    Pair<vector> centre, base, vertex1, vertex2;
-    movingTetGeometry(fraction, centre, base, vertex1, vertex2);
-
-    return
-        Pair<barycentricTensor>
-        (
-            barycentricTensor(centre[0], base[0], vertex1[0], vertex2[0]),
-            barycentricTensor(centre[1], base[1], vertex1[1], vertex2[1])
-        );
 }
 
 
@@ -957,47 +874,6 @@ Foam::vector Foam::particle::deviationFromMeshCentre() const
     else
     {
         return vector::zero;
-    }
-}
-
-
-void Foam::particle::patchData(vector& n, vector& U) const
-{
-    if (!onBoundaryFace())
-    {
-        FatalErrorInFunction
-            << "Patch data was requested for a particle that isn't on a patch"
-            << exit(FatalError);
-    }
-
-    if (mesh_.moving())
-    {
-        Pair<vector> centre, base, vertex1, vertex2;
-        movingTetGeometry(1, centre, base, vertex1, vertex2);
-
-        n = triPointRef(base[0], vertex1[0], vertex2[0]).normal();
-        n /= mag(n);
-
-        // Interpolate the motion of the three face vertices to the current
-        // coordinates
-        U =
-            coordinates_.b()*base[1]
-          + coordinates_.c()*vertex1[1]
-          + coordinates_.d()*vertex2[1];
-
-        // The movingTetGeometry method gives the motion as a displacement
-        // across the time-step, so we divide by the time-step to get velocity
-        U /= mesh_.time().deltaTValue();
-    }
-    else
-    {
-        vector centre, base, vertex1, vertex2;
-        stationaryTetGeometry(centre, base, vertex1, vertex2);
-
-        n = triPointRef(base, vertex1, vertex2).normal();
-        n /= mag(n);
-
-        U = Zero;
     }
 }
 

src/lagrangian/basic/particle/particleI.H
@@ -2,7 +2,7 @@
   =========                 |
   \\      /  F ield         | OpenFOAM: The Open Source CFD Toolbox
    \\    /   O peration     |
-    \\  /    A nd           | Copyright (C) 2011-2017 OpenFOAM Foundation
+    \\  /    A nd           | Copyright (C) 2011-2018 OpenFOAM Foundation
      \\/     M anipulation  |
 -------------------------------------------------------------------------------
 License
@@ -25,6 +25,91 @@ License
 
 #include "polyMesh.H"
 #include "Time.H"
+
+// * * * * * * * * * * * * * Private Member Functions  * * * * * * * * * * * //
+
+void Foam::particle::stationaryTetGeometry
+(
+    vector& centre,
+    vector& base,
+    vector& vertex1,
+    vector& vertex2
+) const
+{
+    const triFace triIs(currentTetIndices().faceTriIs(mesh_));
+    const vectorField& ccs = mesh_.cellCentres();
+    const pointField& pts = mesh_.points();
+
+    centre = ccs[celli_];
+    base = pts[triIs[0]];
+    vertex1 = pts[triIs[1]];
+    vertex2 = pts[triIs[2]];
+}
+
+
+inline Foam::barycentricTensor Foam::particle::stationaryTetTransform() const
+{
+    vector centre, base, vertex1, vertex2;
+    stationaryTetGeometry(centre, base, vertex1, vertex2);
+
+    return barycentricTensor(centre, base, vertex1, vertex2);
+}
+
+
+inline void Foam::particle::movingTetGeometry
+(
+    const scalar fraction,
+    Pair<vector>& centre,
+    Pair<vector>& base,
+    Pair<vector>& vertex1,
+    Pair<vector>& vertex2
+) const
+{
+    const triFace triIs(currentTetIndices().faceTriIs(mesh_));
+    const pointField& ptsOld = mesh_.oldPoints();
+    const pointField& ptsNew = mesh_.points();
+
+    // !!! <-- We would be better off using mesh_.cellCentres() here. However,
+    // we need to put a mesh_.oldCellCentres() method in for this to work. The
+    // values obtained from the mesh and those obtained from the cell do not
+    // necessarily match. See mantis #1993.
+    const vector ccOld = mesh_.cells()[celli_].centre(ptsOld, mesh_.faces());
+    const vector ccNew = mesh_.cells()[celli_].centre(ptsNew, mesh_.faces());
+
+    // Old and new points and cell centres are not sub-cycled. If we are sub-
+    // cycling, then we have to account for the timestep change here by
+    // modifying the fractions that we take of the old and new geometry.
+    const Pair<scalar> s = stepFractionSpan();
+    const scalar f0 = s[0] + stepFraction_*s[1], f1 = fraction*s[1];
+
+    centre[0] = ccOld + f0*(ccNew - ccOld);
+    base[0] = ptsOld[triIs[0]] + f0*(ptsNew[triIs[0]] - ptsOld[triIs[0]]);
+    vertex1[0] = ptsOld[triIs[1]] + f0*(ptsNew[triIs[1]] - ptsOld[triIs[1]]);
+    vertex2[0] = ptsOld[triIs[2]] + f0*(ptsNew[triIs[2]] - ptsOld[triIs[2]]);
+
+    centre[1] = f1*(ccNew - ccOld);
+    base[1] = f1*(ptsNew[triIs[0]] - ptsOld[triIs[0]]);
+    vertex1[1] = f1*(ptsNew[triIs[1]] - ptsOld[triIs[1]]);
+    vertex2[1] = f1*(ptsNew[triIs[2]] - ptsOld[triIs[2]]);
+}
+
+
+inline Foam::Pair<Foam::barycentricTensor> Foam::particle::movingTetTransform
+(
+    const scalar fraction
+) const
+{
+    Pair<vector> centre, base, vertex1, vertex2;
+    movingTetGeometry(fraction, centre, base, vertex1, vertex2);
+
+    return
+        Pair<barycentricTensor>
+        (
+            barycentricTensor(centre[0], base[0], vertex1[0], vertex2[0]),
+            barycentricTensor(centre[1], base[1], vertex1[1], vertex2[1])
+        );
+}
+
 
 // * * * * * * * * * * * * * * * Member Functions  * * * * * * * * * * * * * //
 
@@ -204,6 +289,47 @@ inline Foam::label Foam::particle::patch() const
 inline Foam::vector Foam::particle::position() const
 {
     return currentTetTransform() & coordinates_;
+}
+
+
+void Foam::particle::patchData(vector& n, vector& U) const
+{
+    if (!onBoundaryFace())
+    {
+        FatalErrorInFunction
+            << "Patch data was requested for a particle that isn't on a patch"
+            << exit(FatalError);
+    }
+
+    if (mesh_.moving())
+    {
+        Pair<vector> centre, base, vertex1, vertex2;
+        movingTetGeometry(1, centre, base, vertex1, vertex2);
+
+        n = triPointRef(base[0], vertex1[0], vertex2[0]).normal();
+        n /= mag(n);
+
+        // Interpolate the motion of the three face vertices to the current
+        // coordinates
+        U =
+            coordinates_.b()*base[1]
+          + coordinates_.c()*vertex1[1]
+          + coordinates_.d()*vertex2[1];
+
+        // The movingTetGeometry method gives the motion as a displacement
+        // across the time-step, so we divide by the time-step to get velocity
+        U /= mesh_.time().deltaTValue();
+    }
+    else
+    {
+        vector centre, base, vertex1, vertex2;
+        stationaryTetGeometry(centre, base, vertex1, vertex2);
+
+        n = triPointRef(base, vertex1, vertex2).normal();
+        n /= mag(n);
+
+        U = Zero;
+    }
 }
 
 

commit ea02796f3186772ddd81afae9ed8eb36c8a5c318

particle: Fixed ACMI transfers
The optimisation work done as commit 81947c80 introduced a failure mode
where an ACMI interaction could repeat indefinitely. This has now been
corrected.

Resolves bug report https://bugs.openfoam.org/view.php?id=3166

src/lagrangian/basic/particle/particle.H
@@ -2,7 +2,7 @@
   =========                 |
   \\      /  F ield         | OpenFOAM: The Open Source CFD Toolbox
    \\    /   O peration     | Website:  https://openfoam.org
-    \\  /    A nd           | Copyright (C) 2011-2018 OpenFOAM Foundation
+    \\  /    A nd           | Copyright (C) 2011-2019 OpenFOAM Foundation
      \\/     M anipulation  |
 -------------------------------------------------------------------------------
 License
@@ -581,6 +581,17 @@ public:
         //  interact the particle with the relevant patch.
         template<class TrackCloudType>
         void hitFace
+        (
+            const vector& displacement,
+            const scalar fraction,
+            TrackCloudType& cloud,
+            trackingData& td
+        );
+
+        //- As above, but does not change the master patch. Needed in order for
+        //  ACMI to be able to delegate a hit to the non-overlap patch.
+        template<class TrackCloudType>
+        void hitFaceNoChangeToMasterPatch
         (
             const vector& displacement,
             const scalar fraction,

src/lagrangian/basic/particle/particleTemplates.C
@@ -2,7 +2,7 @@
   =========                 |
   \\      /  F ield         | OpenFOAM: The Open Source CFD Toolbox
    \\    /   O peration     | Website:  https://openfoam.org
-    \\  /    A nd           | Copyright (C) 2011-2018 OpenFOAM Foundation
+    \\  /    A nd           | Copyright (C) 2011-2019 OpenFOAM Foundation
      \\/     M anipulation  |
 -------------------------------------------------------------------------------
 License
@@ -114,6 +114,29 @@ void Foam::particle::hitFace
         Info << "Particle " << origId() << nl << FUNCTION_NAME << nl << endl;
     }
 
+    if (onBoundaryFace())
+    {
+        changeToMasterPatch();
+    }
+
+    hitFaceNoChangeToMasterPatch(displacement, fraction, cloud, td);
+}
+
+
+template<class TrackCloudType>
+void Foam::particle::hitFaceNoChangeToMasterPatch
+(
+    const vector& displacement,
+    const scalar fraction,
+    TrackCloudType& cloud,
+    trackingData& td
+)
+{
+    if (debug)
+    {
+        Info << "Particle " << origId() << nl << FUNCTION_NAME << nl << endl;
+    }
+
     typename TrackCloudType::particleType& p =
         static_cast<typename TrackCloudType::particleType&>(*this);
     typename TrackCloudType::particleType::trackingData& ttd =
@@ -129,8 +152,6 @@ void Foam::particle::hitFace
     }
     else if (onBoundaryFace())
     {
-        changeToMasterPatch();
-
         if (!p.hitPatch(cloud, ttd))
         {
             const polyPatch& patch = mesh_.boundaryMesh()[p.patch()];
@@ -467,7 +488,7 @@ void Foam::particle::hitCyclicACMIPatch
         // Move to the face associated with the non-overlap patch and redo the
         // face interaction.
         tetFacei_ = facei_ = cpp.nonOverlapPatch().start() + localFacei;
-        p.hitFace(displacement, fraction, cloud, td);
+        p.hitFaceNoChangeToMasterPatch(displacement, fraction, cloud, td);
     }
 }
 