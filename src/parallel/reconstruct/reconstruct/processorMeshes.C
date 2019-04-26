6d3f5cdd6a266ffee37c739a5fd4832a8b09f085
Will Bainbridge <http://cfd.direct>
2019-04-26 08:45:48 +0100

    reconstructPar: Prevent moving flag from being set
    Setting the moving flag as a result of a reconstruction of the mesh at a
    new time was causing lagrangian reconstruction to fail, as the necessary
    moving mesh data had not been cached.

--- src/parallel/reconstruct/reconstruct/processorMeshes.C
+++ src/parallel/reconstruct/reconstruct/processorMeshes.C
@@ -2,7 +2,7 @@
   =========                 |
   \\      /  F ield         | OpenFOAM: The Open Source CFD Toolbox
    \\    /   O peration     | Website:  https://openfoam.org
-    \\  /    A nd           | Copyright (C) 2011-2018 OpenFOAM Foundation
+    \\  /    A nd           | Copyright (C) 2011-2019 OpenFOAM Foundation
      \\/     M anipulation  |
 -------------------------------------------------------------------------------
 License
@@ -255,6 +255,7 @@ void Foam::processorMeshes::reconstructPoints(fvMesh& mesh)
     }
 
     mesh.movePoints(newPoints);
+    mesh.moving(false);
     mesh.write();
 }
 
