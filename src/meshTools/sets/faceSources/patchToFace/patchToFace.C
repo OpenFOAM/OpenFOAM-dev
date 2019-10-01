5e09cccd9cac5050f2be634a37aeab585eebbda2
Will Bainbridge <http://cfd.direct>
2019-10-01 14:57:17 +0100

    topoSet: Corrected output of size of zones and patches in parallel

--- src/meshTools/sets/faceSources/patchToFace/patchToFace.C
+++ src/meshTools/sets/faceSources/patchToFace/patchToFace.C
@@ -2,7 +2,7 @@
   =========                 |
   \\      /  F ield         | OpenFOAM: The Open Source CFD Toolbox
    \\    /   O peration     | Website:  https://openfoam.org
-    \\  /    A nd           | Copyright (C) 2011-2018 OpenFOAM Foundation
+    \\  /    A nd           | Copyright (C) 2011-2019 OpenFOAM Foundation
      \\/     M anipulation  |
 -------------------------------------------------------------------------------
 License
@@ -63,7 +63,8 @@ void Foam::patchToFace::combine(topoSet& set, const bool add) const
         const polyPatch& pp = mesh_.boundaryMesh()[patchi];
 
         Info<< "    Found matching patch " << pp.name()
-            << " with " << pp.size() << " faces." << endl;
+            << " with " << returnReduce(pp.size(), sumOp<label>())
+            << " faces." << endl;
 
         for
         (
