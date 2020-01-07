9c5dd0345680641e64d9fe5c926fde90864246bc
Will Bainbridge <http://cfd.direct>
2020-01-07 12:53:25 +0000

    snappyHexMesh: Fixed "cannot find triSurfaceMesh" error message

--- src/meshTools/searchableSurfaces/triSurfaceMesh/triSurfaceMesh.C
+++ src/meshTools/searchableSurfaces/triSurfaceMesh/triSurfaceMesh.C
@@ -2,7 +2,7 @@
   =========                 |
   \\      /  F ield         | OpenFOAM: The Open Source CFD Toolbox
    \\    /   O peration     | Website:  https://openfoam.org
-    \\  /    A nd           | Copyright (C) 2011-2019 OpenFOAM Foundation
+    \\  /    A nd           | Copyright (C) 2011-2020 OpenFOAM Foundation
      \\/     M anipulation  |
 -------------------------------------------------------------------------------
 License
@@ -106,7 +106,7 @@ Foam::fileName Foam::triSurfaceMesh::checkFile
         if (!exists(fName))
         {
             FatalErrorInFunction
-                << "Cannot find triSurfaceMesh at " << io.path(dictFName)
+                << "Cannot find triSurfaceMesh at " << io.path()/dictFName
                 << exit(FatalError);
         }
     }
