97cda40634c22a09970d128dce458410227ab791
Will Bainbridge <http://cfd.direct>
2020-03-03 11:27:34 +0000

    rhoPimpleFoam: Operate in SIMPLE mode
    rhoPimpleFoam now produces identical results to rhoSimpleFoam when run
    with a steady-state time-scheme. The intention is that this solver can
    now be used as a reference when adding steady-state support to other
    compressible solvers for which no SIMPLE variant exists.
    
    rhoReactingFoam has also been updated to support SIMPLE operation, as it
    shares a pressure equation with rhoPimpleFoam.

--- applications/solvers/compressible/rhoPimpleFoam/rhoPimpleFoam.C
+++ applications/solvers/compressible/rhoPimpleFoam/rhoPimpleFoam.C
@@ -2,7 +2,7 @@
   =========                 |
   \\      /  F ield         | OpenFOAM: The Open Source CFD Toolbox
    \\    /   O peration     | Website:  https://openfoam.org
-    \\  /    A nd           | Copyright (C) 2011-2019 OpenFOAM Foundation
+    \\  /    A nd           | Copyright (C) 2011-2020 OpenFOAM Foundation
      \\/     M anipulation  |
 -------------------------------------------------------------------------------
 License
@@ -140,7 +140,12 @@ int main(int argc, char *argv[])
                 }
             }
 
-            if (pimple.firstPimpleIter() && !pimple.simpleRho())
+            if
+            (
+                !mesh.steady()
+             && !pimple.simpleRho()
+             && pimple.firstPimpleIter()
+            )
             {
                 #include "rhoEqn.H"
             }
@@ -167,7 +172,10 @@ int main(int argc, char *argv[])
             }
         }
 
-        rho = thermo.rho();
+        if (!mesh.steady())
+        {
+            rho = thermo.rho();
+        }
 
         runTime.write();
 
