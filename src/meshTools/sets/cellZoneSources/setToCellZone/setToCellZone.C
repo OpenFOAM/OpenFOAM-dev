04d5e6a72a5b24f5a16ef686de742d93b8d9c931
Will Bainbridge <http://cfd.direct>
2020-03-31 12:08:39 +0100

    setToCellZone, setToFaceZone, setToPointZone: More flexible and consistent input
    All of these sets will now take either "set" as the input entry, or
    "cellSet"/"faceSet"/"pointSet" as appropriate. Previously cell and point
    only accepted the "set" keyword whilst face only took "faceSet".

--- src/meshTools/sets/cellZoneSources/setToCellZone/setToCellZone.C
+++ src/meshTools/sets/cellZoneSources/setToCellZone/setToCellZone.C
@@ -2,7 +2,7 @@
   =========                 |
   \\      /  F ield         | OpenFOAM: The Open Source CFD Toolbox
    \\    /   O peration     | Website:  https://openfoam.org
-    \\  /    A nd           | Copyright (C) 2011-2018 OpenFOAM Foundation
+    \\  /    A nd           | Copyright (C) 2011-2020 OpenFOAM Foundation
      \\/     M anipulation  |
 -------------------------------------------------------------------------------
 License
@@ -66,7 +66,14 @@ Foam::setToCellZone::setToCellZone
 )
 :
     topoSetSource(mesh),
-    setName_(dict.lookup("set"))
+    setName_
+    (
+        dict.found("set")
+      ? dict.lookup("set")
+      : dict.found("cellSet")
+      ? dict.lookup("cellSet")
+      : dict.lookup("set")
+    )
 {}
 
 
