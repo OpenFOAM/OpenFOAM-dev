73a594cbcee9feeffcb33856ec96b8bc2ae1c15a
Henry Weller <http://openfoam.org>
2020-04-30 14:08:44 +0100

    nonUniformTableThermophysicalFunction: New non-uniform table thermophysicalFunction for liquid properties
    Description
        Non-uniform tabulated property function that linearly interpolates between
        the values.
    
        To speed-up the search of the non-uniform table a uniform jump-table is
        created on construction which is used for fast indirect addressing into
        the table.
    
    Usage
        \nonUniformTable
            Property    | Description
            values      | List of (temperature property) value pairs
        \endnonUniformTable
    
        Example for the density of water between 280 and 350K
        \verbatim
        rho
        {
            type    nonUniformTable;
    
            values
            (
                (280 999.87)
                (300 995.1)
                (350 973.7)
            );
        }
        \endverbatim

---
+++ applications/test/nonUniformTable/Test-nonUniformTable.C
@@ -0,0 +1,72 @@
+/*---------------------------------------------------------------------------*\
+  =========                 |
+  \\      /  F ield         | OpenFOAM: The Open Source CFD Toolbox
+   \\    /   O peration     | Website:  https://openfoam.org
+    \\  /    A nd           | Copyright (C) 2020 OpenFOAM Foundation
+     \\/     M anipulation  |
+-------------------------------------------------------------------------------
+License
+    This file is part of OpenFOAM.
+
+    OpenFOAM is free software: you can redistribute it and/or modify it
+    under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    OpenFOAM is distributed in the hope that it will be useful, but WITHOUT
+    ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+    FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License
+    for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with OpenFOAM.  If not, see <http://www.gnu.org/licenses/>.
+
+Application
+    Test-nonUniformTable
+
+Description
+    Tests the lookup of values of a linear function from and non-uniform
+    table and reports any error.
+
+\*---------------------------------------------------------------------------*/
+
+#include "nonUniformTableThermophysicalFunction.H"
+#include "IFstream.H"
+
+using namespace Foam;
+
+// * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * //
+// Main program:
+
+int main(int argc, char *argv[])
+{
+    dictionary dict(IFstream("thermoDict")());
+
+    thermophysicalFunctions::nonUniformTable table(dict);
+
+    const label n = 1000;
+    const scalar T0 = table.values().first().first();
+    const scalar Tn = table.values().last().first();
+    const scalar deltaT = (Tn - T0)/n;
+
+    for (int i = 0; i<n + 1; i++)
+    {
+        const scalar T = T0 + i*deltaT;
+        const scalar f = table.f(0, T);
+
+        if (mag(T - f) > small)
+        {
+            FatalError<< "failed" << exit(FatalError) << endl;
+        }
+    }
+
+    Info<< "\nLookup of a linear function from a non-uniform table is correct\n"
+        << endl;
+
+    Info<< "\nEnd\n" << endl;
+
+    return 0;
+}
+
+
+// ************************************************************************* //
