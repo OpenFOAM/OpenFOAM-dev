17c40d2aa57b1f922cdddc315d8cf3471c9bc600
Henry Weller <http://cfd.direct>
2020-08-17 14:04:42 +0100

    UList,FixedList: Updated reverse_iterators for STL compliance
    Resolves bug-report https://bugs.openfoam.org/view.php?id=3534

--- applications/test/List/Test-List.C
+++ applications/test/List/Test-List.C
@@ -2,7 +2,7 @@
   =========                 |
   \\      /  F ield         | OpenFOAM: The Open Source CFD Toolbox
    \\    /   O peration     | Website:  https://openfoam.org
-    \\  /    A nd           | Copyright (C) 2011-2019 OpenFOAM Foundation
+    \\  /    A nd           | Copyright (C) 2011-2020 OpenFOAM Foundation
      \\/     M anipulation  |
 -------------------------------------------------------------------------------
 License
@@ -41,8 +41,6 @@ See also
 #include "scalar.H"
 #include "vector.H"
 #include "ListOps.H"
-
-#include<list>
 
 using namespace Foam;
 
