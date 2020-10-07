48cc85c19b21a0e0988fd8102760422af98fb24c
Will Bainbridge <http://cfd.direct>
2020-10-07 10:10:07 +0100

    setWaves: Fix typo in documentation

--- applications/utilities/preProcessing/setWaves/setWaves.C
+++ applications/utilities/preProcessing/setWaves/setWaves.C
@@ -2,7 +2,7 @@
   =========                 |
   \\      /  F ield         | OpenFOAM: The Open Source CFD Toolbox
    \\    /   O peration     | Website:  https://openfoam.org
-    \\  /    A nd           | Copyright (C) 2017-2018 OpenFOAM Foundation
+    \\  /    A nd           | Copyright (C) 2017-2020 OpenFOAM Foundation
      \\/     M anipulation  |
 -------------------------------------------------------------------------------
 License
@@ -68,7 +68,7 @@ int main(int argc, char *argv[])
     argList::addBoolOption
     (
         "gas",
-        "the volume fraction field is that that of the gas above the wave"
+        "the volume fraction field is that of the gas above the wave"
     );
 
     #include "setRootCase.H"
