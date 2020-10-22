89b9e2d24cf8706ca21fb1effb5c02f366cf093f
Will Bainbridge <http://cfd.direct>
2020-10-22 10:00:16 +0100

    topoSet: searchableSurfaceToFaceZone: Improved robustness
    This set now produces a contiguous face zone even when cell centres fall
    exactly on the plane of the searchable surface.
    
    The set now calculates the intersections between the
    cell-centre-to-cell-centre vectors for every face, and uses those to to
    categorise the adjacent cells either side of the set as either above or
    below the surface. Then, faces which border both an above and a below
    cell are included. This guarantees a contiguous result.
    
    Previously faces for which an intersection was found were included
    directly, which resulted in a non-contiguous result when the
    intersections became ambiguous; i.e., when the surface passed very close
    to the cell centres.

--- src/meshTools/sets/faceZoneSources/searchableSurfaceToFaceZone/searchableSurfaceToFaceZone.C
+++ src/meshTools/sets/faceZoneSources/searchableSurfaceToFaceZone/searchableSurfaceToFaceZone.C
@@ -2,7 +2,7 @@
   =========                 |
   \\      /  F ield         | OpenFOAM: The Open Source CFD Toolbox
    \\    /   O peration     | Website:  https://openfoam.org
-    \\  /    A nd           | Copyright (C) 2012-2018 OpenFOAM Foundation
+    \\  /    A nd           | Copyright (C) 2012-2020 OpenFOAM Foundation
      \\/     M anipulation  |
 -------------------------------------------------------------------------------
 License
@@ -53,6 +53,148 @@ Foam::topoSetSource::addToUsageTable Foam::searchableSurfaceToFaceZone::usage_
     "    Select all faces whose cell-cell centre vector intersects the surface "
     "\n"
 );
+
+
+// * * * * * * * * * * * * * Private Member Functions  * * * * * * * * * * * //
+
+void Foam::searchableSurfaceToFaceZone::combine
+(
+    faceZoneSet& fzSet,
+    const bool add
+) const
+{
+    // Get a list of "interior" faces; i.e., either internal or coupled
+    labelList interiorFaceFaces(mesh_.nFaces());
+    {
+        forAll(mesh_.faceNeighbour(), facei)
+        {
+            interiorFaceFaces[facei] = facei;
+        }
+        label nInteriorFaces = mesh_.nInternalFaces();
+        forAll(mesh_.boundaryMesh(), patchi)
+        {
+            const polyPatch& patch = mesh_.boundaryMesh()[patchi];
+            if (patch.coupled())
+            {
+                forAll(patch, patchFacei)
+                {
+                    const label facei = patch.start() + patchFacei;
+                    interiorFaceFaces[nInteriorFaces] = facei;
+                    ++ nInteriorFaces;
+                }
+            }
+        }
+        interiorFaceFaces.resize(nInteriorFaces);
+    }
+
+    // Create owner and neighbour cell centre lists for all interior faces
+    pointField ownCc(interiorFaceFaces.size());
+    pointField nbrCc(interiorFaceFaces.size());
+    {
+        const pointField& cc = mesh_.cellCentres();
+
+        vectorField boundaryNbrCc;
+        syncTools::swapBoundaryCellPositions(mesh_, cc, boundaryNbrCc);
+
+        forAll(ownCc, interiorFacei)
+        {
+            const label facei = interiorFaceFaces[interiorFacei];
+
+            ownCc[interiorFacei] = cc[mesh_.faceOwner()[facei]];
+            nbrCc[interiorFacei] =
+                mesh_.isInternalFace(facei)
+              ? cc[mesh_.faceNeighbour()[facei]]
+              : boundaryNbrCc[facei - mesh_.nInternalFaces()];
+        }
+    }
+
+    // Do intersection tests on the vectors between the owner and neighbour
+    // cell centres
+    List<pointIndexHit> hits;
+    pointField normals;
+    surfacePtr_().findLine(ownCc, nbrCc, hits);
+    surfacePtr_().getNormal(hits, normals);
+
+    // Create a list of labels indicating what side of the surface a cell
+    // is on; -1 is below, +1 is above, and 0 is too far from the surface
+    // for the sidedness to be calculable
+    labelList side(mesh_.nCells(), 0);
+    forAll(hits, interiorFacei)
+    {
+        if (hits[interiorFacei].hit())
+        {
+            const label facei = interiorFaceFaces[interiorFacei];
+
+            const vector d = nbrCc[interiorFacei] - ownCc[interiorFacei];
+            const bool sign = (normals[interiorFacei] & d) < 0;
+
+            side[mesh_.faceOwner()[facei]] = sign ? -1 : +1;
+
+            if (mesh_.isInternalFace(facei))
+            {
+                side[mesh_.faceNeighbour()[facei]] = sign ? +1 : -1;
+            }
+        }
+    }
+    labelList boundaryNbrSide;
+    syncTools::swapBoundaryCellList(mesh_, side, boundaryNbrSide);
+
+    // Create a face direction list indicating which direction a given face
+    // intersects the surface; -1 is backward, +1 is forward, and 0
+    // indicates that the face does not intersect the surface
+    labelList direction(mesh_.nFaces(), 0);
+    forAll(interiorFaceFaces, interiorFacei)
+    {
+        const label facei = interiorFaceFaces[interiorFacei];
+
+        const label ownSide = side[mesh_.faceOwner()[facei]];
+        const label nbrSide =
+            mesh_.isInternalFace(facei)
+          ? side[mesh_.faceNeighbour()[facei]]
+          : boundaryNbrSide[facei - mesh_.nInternalFaces()];
+
+        direction[facei] = ownSide*nbrSide < 0 ? ownSide - nbrSide : 0;
+    }
+
+    // Select intersected faces
+    DynamicList<label> newAddressing;
+    DynamicList<bool> newFlipMap;
+    if (add)
+    {
+        // Start from copy
+        newAddressing = DynamicList<label>(fzSet.addressing());
+        newFlipMap = DynamicList<bool>(fzSet.flipMap());
+
+        // Add everything with a direction that is not already in the set
+        forAll(direction, facei)
+        {
+            if (direction[facei] != 0 && !fzSet.found(facei))
+            {
+                newAddressing.append(facei);
+                newFlipMap.append(direction[facei] < 0);
+            }
+        }
+    }
+    else
+    {
+        // Start from empty
+        newAddressing = DynamicList<label>(fzSet.addressing().size());
+        newFlipMap = DynamicList<bool>(fzSet.flipMap().size());
+
+        // Add everything from the zone set that does not have a direction
+        forAll(fzSet.addressing(), i)
+        {
+            if (direction[fzSet.addressing()[i]] == 0)
+            {
+                newAddressing.append(fzSet.addressing()[i]);
+                newFlipMap.append(fzSet.flipMap()[i]);
+            }
+        }
+    }
+    fzSet.addressing().transfer(newAddressing);
+    fzSet.flipMap().transfer(newFlipMap);
+    fzSet.updateSet();
+}
 
 
 // * * * * * * * * * * * * * * * * Constructors  * * * * * * * * * * * * * * //
@@ -107,106 +249,19 @@ void Foam::searchableSurfaceToFaceZone::applyToSet
     {
         faceZoneSet& fzSet = refCast<faceZoneSet>(set);
 
-        // Get cell-cell centre vectors
-        // ~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-
-        pointField start(mesh_.nFaces());
-        pointField end(mesh_.nFaces());
-
-        const pointField& cc = mesh_.cellCentres();
-
-        // Internal faces
-        for (label facei = 0; facei < mesh_.nInternalFaces(); facei++)
-        {
-            start[facei] = cc[mesh_.faceOwner()[facei]];
-            end[facei] = cc[mesh_.faceNeighbour()[facei]];
-        }
-
-        // Boundary faces
-        vectorField nbrCellCentres;
-        syncTools::swapBoundaryCellPositions(mesh_, cc, nbrCellCentres);
-
-        const polyBoundaryMesh& pbm = mesh_.boundaryMesh();
-
-        forAll(pbm, patchi)
-        {
-            const polyPatch& pp = pbm[patchi];
-
-            if (pp.coupled())
-            {
-                forAll(pp, i)
-                {
-                    label facei = pp.start()+i;
-                    start[facei] = cc[mesh_.faceOwner()[facei]];
-                    end[facei] = nbrCellCentres[facei-mesh_.nInternalFaces()];
-                }
-            }
-            else
-            {
-                forAll(pp, i)
-                {
-                    label facei = pp.start()+i;
-                    start[facei] = cc[mesh_.faceOwner()[facei]];
-                    end[facei] = mesh_.faceCentres()[facei];
-                }
-            }
-        }
-
-
-        // Do all intersection tests
-        // ~~~~~~~~~~~~~~~~~~~~~~~~~
-
-        List<pointIndexHit> hits;
-        surfacePtr_().findLine(start, end, hits);
-        pointField normals;
-        surfacePtr_().getNormal(hits, normals);
-
-
-        // Select intersected faces
-        // ~~~~~~~~~~~~~~~~~~~~~~~~
-
         if ((action == topoSetSource::NEW) || (action == topoSetSource::ADD))
         {
             Info<< "    Adding all faces from surface "
                 << surfacePtr_().name() << " ..." << endl;
 
-            DynamicList<label> newAddressing(fzSet.addressing());
-            DynamicList<bool> newFlipMap(fzSet.flipMap());
-
-            forAll(hits, facei)
-            {
-                if (hits[facei].hit() && !fzSet.found(facei))
-                {
-                    newAddressing.append(facei);
-                    vector d = end[facei]-start[facei];
-                    newFlipMap.append((normals[facei] & d) < 0);
-                }
-            }
-
-            fzSet.addressing().transfer(newAddressing);
-            fzSet.flipMap().transfer(newFlipMap);
-            fzSet.updateSet();
+            combine(fzSet, true);
         }
         else if (action == topoSetSource::DELETE)
         {
             Info<< "    Removing all faces from surface "
                 << surfacePtr_().name() << " ..." << endl;
 
-            // Start off empty
-            DynamicList<label> newAddressing(fzSet.addressing().size());
-            DynamicList<bool> newFlipMap(fzSet.flipMap().size());
-
-            forAll(fzSet.addressing(), i)
-            {
-                if (!hits[fzSet.addressing()[i]].hit())
-                {
-                    newAddressing.append(fzSet.addressing()[i]);
-                    newFlipMap.append(fzSet.flipMap()[i]);
-                }
-            }
-            fzSet.addressing().transfer(newAddressing);
-            fzSet.flipMap().transfer(newFlipMap);
-            fzSet.updateSet();
+            combine(fzSet, false);
         }
     }
 }
