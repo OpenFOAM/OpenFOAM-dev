d7d1221cd47833c0e12fbbe5e38bc5f3db95a467
Henry Weller <http://cfd.direct>
2020-11-23 12:05:29 +0000

    surfaceFilmModels::curvatureSeparation: Added deltaStable control
    to provide the same numerical stability control as the other injection models:
    BrunDrippingInjection, drippingInjection
    
            //- Stable film thickness - drips only formed if thickness
            //  exceeds this threshold value
            scalar deltaStable_;
    
    Resolves bug-report https://bugs.openfoam.org/view.php?id=2456

--- src/regionModels/surfaceFilmModels/submodels/kinematic/injectionModel/curvatureSeparation/curvatureSeparation.C
+++ src/regionModels/surfaceFilmModels/submodels/kinematic/injectionModel/curvatureSeparation/curvatureSeparation.C
@@ -2,7 +2,7 @@
   =========                 |
   \\      /  F ield         | OpenFOAM: The Open Source CFD Toolbox
    \\    /   O peration     | Website:  https://openfoam.org
-    \\  /    A nd           | Copyright (C) 2011-2018 OpenFOAM Foundation
+    \\  /    A nd           | Copyright (C) 2011-2020 OpenFOAM Foundation
      \\/     M anipulation  |
 -------------------------------------------------------------------------------
 License
@@ -24,16 +24,9 @@ License
 \*---------------------------------------------------------------------------*/
 
 #include "curvatureSeparation.H"
-#include "addToRunTimeSelectionTable.H"
-#include "fvMesh.H"
-#include "Time.H"
-#include "volFields.H"
 #include "kinematicSingleLayer.H"
-#include "surfaceInterpolate.H"
-#include "fvcDiv.H"
 #include "fvcGrad.H"
-#include "stringListOps.H"
-#include "cyclicPolyPatch.H"
+#include "addToRunTimeSelectionTable.H"
 
 // * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * //
 
@@ -61,37 +54,30 @@ tmp<volScalarField> curvatureSeparation::calcInvR1
     const volVectorField& U
 ) const
 {
-    // method 1
-/*
-    tmp<volScalarField> tinvR1
-    (
-        volScalarField::New("invR1", fvc::div(film().nHat()))
-    );
-*/
+    const dimensionedScalar smallU("smallU", dimVelocity, rootVSmall);
+    const volVectorField UHat(U/(mag(U) + smallU));
 
-    // method 2
-    dimensionedScalar smallU("smallU", dimVelocity, rootVSmall);
-    volVectorField UHat(U/(mag(U) + smallU));
     tmp<volScalarField> tinvR1
     (
         volScalarField::New("invR1", UHat & (UHat & gradNHat_))
     );
 
-
     scalarField& invR1 = tinvR1.ref().primitiveFieldRef();
 
-    // apply defined patch radii
+    // Apply defined patch radii
     const scalar rMin = 1e-6;
     const fvMesh& mesh = film().regionMesh();
     const polyBoundaryMesh& pbm = mesh.boundaryMesh();
+
     forAll(definedPatchRadii_, i)
     {
-        label patchi = definedPatchRadii_[i].first();
-        scalar definedInvR1 = 1.0/max(rMin, definedPatchRadii_[i].second());
+        const label patchi = definedPatchRadii_[i].first();
+        const scalar definedInvR1 =
+            1.0/max(rMin, definedPatchRadii_[i].second());
         UIndirectList<scalar>(invR1, pbm[patchi].faceCells()) = definedInvR1;
     }
 
-    // filter out large radii
+    // Filter out large radii
     const scalar rMax = 1e6;
     forAll(invR1, i)
     {
@@ -121,11 +107,12 @@ tmp<scalarField> curvatureSeparation::calcCosAngle
     const unallocLabelList& nbr = mesh.neighbour();
 
     scalarField phiMax(mesh.nCells(), -great);
-    scalarField cosAngle(mesh.nCells(), 0.0);
+    scalarField cosAngle(mesh.nCells(), 0);
+
     forAll(nbr, facei)
     {
-        label cellO = own[facei];
-        label cellN = nbr[facei];
+        const label cellO = own[facei];
+        const label cellN = nbr[facei];
 
         if (phi[facei] > phiMax[cellO])
         {
@@ -147,7 +134,7 @@ tmp<scalarField> curvatureSeparation::calcCosAngle
         const vectorField nf(pp.nf());
         forAll(phip, i)
         {
-            label celli = faceCells[i];
+            const label celli = faceCells[i];
             if (phip[i] > phiMax[celli])
             {
                 phiMax[celli] = phip[i];
@@ -155,43 +142,7 @@ tmp<scalarField> curvatureSeparation::calcCosAngle
             }
         }
     }
-/*
-    // correction for cyclics - use cyclic pairs' face normal instead of
-    // local face normal
-    const fvBoundaryMesh& pbm = mesh.boundary();
-    forAll(phi.boundaryField(), patchi)
-    {
-        if (isA<cyclicPolyPatch>(pbm[patchi]))
-        {
-            const scalarField& phip = phi.boundaryField()[patchi];
-            const vectorField nf(pbm[patchi].nf());
-            const labelList& faceCells = pbm[patchi].faceCells();
-            const label sizeBy2 = pbm[patchi].size()/2;
 
-            for (label face0=0; face0<sizeBy2; face0++)
-            {
-                label face1 = face0 + sizeBy2;
-                label cell0 = faceCells[face0];
-                label cell1 = faceCells[face1];
-
-                // flux leaving half 0, entering half 1
-                if (phip[face0] > phiMax[cell0])
-                {
-                    phiMax[cell0] = phip[face0];
-                    cosAngle[cell0] = -gHat_ & -nf[face1];
-                }
-
-                // flux leaving half 1, entering half 0
-                if (-phip[face1] > phiMax[cell1])
-                {
-                    phiMax[cell1] = -phip[face1];
-                    cosAngle[cell1] = -gHat_ & nf[face0];
-                }
-            }
-        }
-    }
-*/
-    // checks
     if (debug && mesh.time().writeTime())
     {
         volScalarField volCosAngle
@@ -226,7 +177,11 @@ curvatureSeparation::curvatureSeparation
 :
     injectionModel(type(), film, dict),
     gradNHat_(fvc::grad(film.nHat())),
-    deltaByR1Min_(coeffDict_.lookupOrDefault<scalar>("deltaByR1Min", 0.0)),
+    deltaByR1Min_
+    (
+        coeffDict_.lookupOrDefault<scalar>("deltaByR1Min", scalar(0))
+    ),
+    deltaStable_(coeffDict_.lookupOrDefault("deltaStable", scalar(0))),
     definedPatchRadii_(),
     magG_(mag(film.g().value())),
     gHat_(Zero)
@@ -240,7 +195,10 @@ curvatureSeparation::curvatureSeparation
 
     gHat_ = film.g().value()/magG_;
 
-    List<Tuple2<word, scalar>> prIn(coeffDict_.lookup("definedPatchRadii"));
+    const List<Tuple2<word, scalar>> prIn
+    (
+        coeffDict_.lookup("definedPatchRadii")
+    );
     const wordList& allPatchNames = film.regionMesh().boundaryMesh().names();
 
     DynamicList<Tuple2<label, scalar>> prData(allPatchNames.size());
@@ -297,37 +255,38 @@ void curvatureSeparation::correct
     const scalarField invR1(calcInvR1(U));
     const scalarField cosAngle(calcCosAngle(phi));
 
-    // calculate force balance
+    // Calculate force balance
     const scalar Fthreshold = 1e-10;
-    scalarField Fnet(mesh.nCells(), 0.0);
-    scalarField separated(mesh.nCells(), 0.0);
+    scalarField Fnet(mesh.nCells(), 0);
+    scalarField separated(mesh.nCells(), 0);
+
     forAll(invR1, i)
     {
         if ((invR1[i] > 0) && (delta[i]*invR1[i] > deltaByR1Min_))
         {
-            scalar R1 = 1.0/(invR1[i] + rootVSmall);
-            scalar R2 = R1 + delta[i];
+            const scalar R1 = 1.0/(invR1[i] + rootVSmall);
+            const scalar R2 = R1 + delta[i];
 
-            // inertial force
-            scalar Fi = -delta[i]*rho[i]*magSqrU[i]*72.0/60.0*invR1[i];
+            // Inertial force
+            const scalar Fi = -delta[i]*rho[i]*magSqrU[i]*72.0/60.0*invR1[i];
 
-            // body force
-            scalar Fb =
+            // Body force
+            const scalar Fb =
               - 0.5*rho[i]*magG_*invR1[i]*(sqr(R1) - sqr(R2))*cosAngle[i];
 
-            // surface force
-            scalar Fs = sigma[i]/R2;
+            // Surface force
+            const scalar Fs = sigma[i]/R2;
 
             Fnet[i] = Fi + Fb + Fs;
 
-            if (Fnet[i] + Fthreshold < 0)
+            if (Fnet[i] + Fthreshold < 0 && delta[i] > deltaStable_)
             {
                 separated[i] = 1.0;
             }
         }
     }
 
-    // inject all available mass
+    // Inject all available mass
     massToInject = separated*availableMass;
     diameterToInject = separated*delta;
     availableMass -= separated*availableMass;
