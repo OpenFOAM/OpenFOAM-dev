2caad45a9b5ec6db05687aefcafe275877edc776
Will Bainbridge <http://cfd.direct>
2021-01-22 19:18:51 +0000

    functionObjects/setTimeStep: Compatibility with 'adjustableRunTime'
    setTimeStep is now compatible with a 'writeControl adjustableRunTime;'
    setting in the systemControlDict. If 'adjustableRunTime' is selected
    then the time-step values set by this function object will not be
    exactly as specified, but write intervals will be matched exactly.
    
    All function object time adjustment is now done during the execute
    methods, so the specific setTimeStep hooks have been removed.

--- src/OpenFOAM/db/Time/Time.C
+++ src/OpenFOAM/db/Time/Time.C
@@ -2,7 +2,7 @@
   =========                 |
   \\      /  F ield         | OpenFOAM: The Open Source CFD Toolbox
    \\    /   O peration     | Website:  https://openfoam.org
-    \\  /    A nd           | Copyright (C) 2011-2020 OpenFOAM Foundation
+    \\  /    A nd           | Copyright (C) 2011-2021 OpenFOAM Foundation
      \\/     M anipulation  |
 -------------------------------------------------------------------------------
 License
@@ -969,8 +969,6 @@ void Foam::Time::setDeltaT(const dimensionedScalar& deltaT)
 void Foam::Time::setDeltaT(const scalar deltaT)
 {
     setDeltaTNoAdjust(deltaT);
-
-    functionObjects_.setTimeStep();
 
     if (writeControl_ == writeControl::adjustableRunTime)
     {
--- src/OpenFOAM/db/functionObjects/functionObject/functionObject.C
+++ src/OpenFOAM/db/functionObjects/functionObject/functionObject.C
@@ -2,7 +2,7 @@
   =========                 |
   \\      /  F ield         | OpenFOAM: The Open Source CFD Toolbox
    \\    /   O peration     | Website:  https://openfoam.org
-    \\  /    A nd           | Copyright (C) 2011-2020 OpenFOAM Foundation
+    \\  /    A nd           | Copyright (C) 2011-2021 OpenFOAM Foundation
      \\/     M anipulation  |
 -------------------------------------------------------------------------------
 License
@@ -147,12 +147,6 @@ bool Foam::functionObject::executeAtStart() const
 bool Foam::functionObject::end()
 {
     return true;
-}
-
-
-bool Foam::functionObject::setTimeStep()
-{
-    return false;
 }
 
 
