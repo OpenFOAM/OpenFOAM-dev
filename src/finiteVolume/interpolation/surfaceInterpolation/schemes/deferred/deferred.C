ed2ca63244d235a9545d71e455d8a5bcb1678d1f
Henry Weller <http://cfd.direct>
2021-04-27 20:17:34 +0100

    surfaceInterpolationSchemes::deferred: New convection interpolation scheme which provides conservative diagonal dominance
    Description
        Deferred correction interpolation scheme derived from upwind
        which returns upwind weighting factors and an explicit correction obtained
        from the specified scheme.
    
        This ensures that the transport matrix generated is at least diagonally
        equal and avoids the need for relaxation of the matrix (which can affect
        conservation) for transient running.
    
    e.g.
    
    divSchemes
    {
        default             none;
        div(phi,U)          Gauss deferred limitedLinear 1;
        .
        .
        .
    
    and with this setting relaxation of the U-equation is not necessary irrespective
    of the Courant number.

--- src/finiteVolume/interpolation/surfaceInterpolation/schemes/deferred/deferred.C
+++ src/finiteVolume/interpolation/surfaceInterpolation/schemes/deferred/deferred.C
@@ -2,7 +2,7 @@
   =========                 |
   \\      /  F ield         | OpenFOAM: The Open Source CFD Toolbox
    \\    /   O peration     | Website:  https://openfoam.org
-    \\  /    A nd           | Copyright (C) 2012-2021 OpenFOAM Foundation
+    \\  /    A nd           | Copyright (C) 2021 OpenFOAM Foundation
      \\/     M anipulation  |
 -------------------------------------------------------------------------------
 License
@@ -23,13 +23,13 @@ License
 
 \*---------------------------------------------------------------------------*/
 
-#include "CoBlended.H"
+#include "deferred.H"
 
 // * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * //
 
 namespace Foam
 {
-    makeSurfaceInterpolationScheme(CoBlended);
+    makeSurfaceInterpolationScheme(deferred)
 }
 
 // ************************************************************************* //
