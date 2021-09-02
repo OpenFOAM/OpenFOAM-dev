e6fdd180e8dc73e6914073908f159e58d622bd1d
Henry Weller <http://cfd.direct>
2021-09-02 19:45:14 +0100

    mapFieldsPar: Corrected handling of argList and reverted change to createTimes.H

--- applications/utilities/preProcessing/mapFieldsPar/mapFieldsPar.C
+++ applications/utilities/preProcessing/mapFieldsPar/mapFieldsPar.C
@@ -2,7 +2,7 @@
   =========                 |
   \\      /  F ield         | OpenFOAM: The Open Source CFD Toolbox
    \\    /   O peration     | Website:  https://openfoam.org
-    \\  /    A nd           | Copyright (C) 2011-2018 OpenFOAM Foundation
+    \\  /    A nd           | Copyright (C) 2011-2021 OpenFOAM Foundation
      \\/     M anipulation  |
 -------------------------------------------------------------------------------
 License
@@ -204,7 +204,7 @@ int main(int argc, char *argv[])
         "skip mapping lagrangian positions and fields"
     );
 
-    argList args(argc, argv);
+    #include "setRootCase.H"
 
     fileName rootDirTarget(args.rootPath());
     fileName caseDirTarget(args.globalCaseName());
