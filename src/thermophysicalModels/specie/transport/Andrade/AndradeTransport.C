26f0e47d4bce4a3d3a039f80b867f51048e856e3
Henry Weller <http://cfd.direct>
2021-11-25 16:10:07 +0000

    AndradeTransport: New specie transport model for liquids
    Description
        Transport package using the Andrade function for the natural logarithm of
        dynamic viscosity and thermal conductivity of liquids:
    
        \verbatim
            log(mu) = muCoeffs[0] + muCoeffs[1]*T + muCoeffs[2]*sqr(T)
              + muCoeffs_[3]/(muCoeffs_[4] + T)
    
            log(kappa) = kappaCoeffs[0] + kappaCoeffs[1]*T + kappaCoeffs[2]*sqr(T)
              + kappaCoeffs_[3]/(kappaCoeffs_[4] + T)
        );
        \endverbatim
    
        References:
        \verbatim
            Andrade, E. D. C. (1934).
            XLI. A theory of the viscosity of liquids.—Part I.
            The London, Edinburgh, and Dublin Philosophical Magazine
            and Journal of Science, 17(112), 497-511.
    
            Andrade, E. D. C. (1934).
            LVIII. A theory of the viscosity of liquids.—Part II.
            The London, Edinburgh, and Dublin Philosophical Magazine
            and Journal of Science, 17(113), 698-732.
        \endverbatim
    
    Usage
        \table
            Property        | Description
            muCoeffs        | Dynamic viscosity polynomial coefficients
            kappaCoeffs     | Thermal conductivity polynomial coefficients
        \endtable
    
        Example of the specification of the transport properties for water@200bar:
        \verbatim
        transport
        {
            muCoeffs    (-25.8542 0.031256 -2.2e-05 3289.918 -11.4784);
            kappaCoeffs (-2.56543 0.008794 -9.8e-06 100.368 0);
        }
        \endverbatim

---
+++ src/thermophysicalModels/specie/transport/Andrade/AndradeTransport.C
@@ -0,0 +1,73 @@
+/*---------------------------------------------------------------------------*\
+  =========                 |
+  \\      /  F ield         | OpenFOAM: The Open Source CFD Toolbox
+   \\    /   O peration     | Website:  https://openfoam.org
+    \\  /    A nd           | Copyright (C) 2021 OpenFOAM Foundation
+     \\/     M anipulation  |
+-------------------------------------------------------------------------------
+License
+    This file is part of OpenFOAM.
+
+    OpenFOAM is free software: you can redistribute it and/or modify it
+    under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    OpenFOAM is distributed in the hope that it will be useful, but WITHOUT
+    ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+    FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License
+    for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with OpenFOAM.  If not, see <http://www.gnu.org/licenses/>.
+
+\*---------------------------------------------------------------------------*/
+
+#include "AndradeTransport.H"
+#include "IOstreams.H"
+
+// * * * * * * * * * * * * * * * * Constructors  * * * * * * * * * * * * * * //
+
+template<class Thermo>
+Foam::AndradeTransport<Thermo>::AndradeTransport(const dictionary& dict)
+:
+    Thermo(dict),
+    muCoeffs_(dict.subDict("transport").lookup("muCoeffs")),
+    kappaCoeffs_(dict.subDict("transport").lookup("kappaCoeffs"))
+{}
+
+
+// * * * * * * * * * * * * * * * Member Functions  * * * * * * * * * * * * * //
+
+template<class Thermo>
+void Foam::AndradeTransport<Thermo>::write(Ostream& os) const
+{
+    os  << this->name() << endl;
+    os  << token::BEGIN_BLOCK << incrIndent << nl;
+
+    Thermo::write(os);
+
+    dictionary dict("transport");
+    dict.add("muCoeffs", muCoeffs_);
+    dict.add("kappaCoeffs", kappaCoeffs_);
+    os  << indent << dict.dictName() << dict;
+
+    os  << decrIndent << token::END_BLOCK << nl;
+}
+
+
+// * * * * * * * * * * * * * * * IOstream Operators  * * * * * * * * * * * * //
+
+template<class Thermo>
+Foam::Ostream& Foam::operator<<
+(
+    Ostream& os,
+    const AndradeTransport<Thermo>& pt
+)
+{
+    pt.write(os);
+    return os;
+}
+
+
+// ************************************************************************* //
