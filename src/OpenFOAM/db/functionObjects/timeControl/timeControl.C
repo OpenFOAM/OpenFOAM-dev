842a6021f7311fb728763fe2ece1cf6ab39c23f7
Henry Weller <http://cfd.direct>
2022-02-18 16:34:53 +0000

    functionObjects::timeControl: Converted writeInterval and executeInterval to user-time
    Resolves bug-report https://bugs.openfoam.org/view.php?id=3807

--- src/OpenFOAM/db/functionObjects/timeControl/timeControl.C
+++ src/OpenFOAM/db/functionObjects/timeControl/timeControl.C
@@ -2,7 +2,7 @@
   =========                 |
   \\      /  F ield         | OpenFOAM: The Open Source CFD Toolbox
    \\    /   O peration     | Website:  https://openfoam.org
-    \\  /    A nd           | Copyright (C) 2011-2021 OpenFOAM Foundation
+    \\  /    A nd           | Copyright (C) 2011-2022 OpenFOAM Foundation
      \\/     M anipulation  |
 -------------------------------------------------------------------------------
 License
@@ -125,7 +125,7 @@ void Foam::timeControl::read(const dictionary& dict)
         case timeControls::cpuTime:
         case timeControls::adjustableRunTime:
         {
-            interval_ = dict.lookup<scalar>(intervalName);
+            interval_ = time_.userTimeToTime(dict.lookup<scalar>(intervalName));
             break;
         }
 
