b4bcb29d6a8d8cc0b7576934ece1f0fafaddfccc
Will Bainbridge <http://cfd.direct>
2022-03-30 10:49:20 +0100

    multiphaseEulerFoam: Added models
    Added solid particle coalescence model by Adachi, Stuart and Fokkink
    (1994), solid particle breakage model by Kusters (1991) and spherical
    particle lift force model by Saffman-Mei (1992).
    
    Patch contributed by Kasper Gram Bilde and Institute of Fluid Dynamics,
    Helmholtz-Zentrum Dresden - Rossendorf (HZDR)

---
+++ applications/solvers/multiphase/multiphaseEulerFoam/phaseSystems/populationBalanceModel/coalescenceModels/AdachiStuartFokkink/AdachiStuartFokkink.C
@@ -0,0 +1,88 @@
+/*---------------------------------------------------------------------------*\
+  =========                 |
+  \\      /  F ield         | OpenFOAM: The Open Source CFD Toolbox
+   \\    /   O peration     | Website:  https://openfoam.org
+    \\  /    A nd           | Copyright (C) 2022 OpenFOAM Foundation
+     \\/     M anipulation  |
+-------------------------------------------------------------------------------
+License
+    This file is part of OpenFOAM.
+
+    OpenFOAM is free software: you can redistribute it and/or modify it
+    under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    OpenFOAM is distributed in the hope that it will be useful, but WITHOUT
+    ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+    FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License
+    for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with OpenFOAM.  If not, see <http://www.gnu.org/licenses/>.
+
+\*---------------------------------------------------------------------------*/
+
+#include "AdachiStuartFokkink.H"
+#include "addToRunTimeSelectionTable.H"
+
+// * * * * * * * * * * * * * * Static Data Members * * * * * * * * * * * * * //
+
+namespace Foam
+{
+namespace diameterModels
+{
+namespace coalescenceModels
+{
+    defineTypeNameAndDebug(AdachiStuartFokkink, 0);
+    addToRunTimeSelectionTable
+    (
+        coalescenceModel,
+        AdachiStuartFokkink,
+        dictionary
+    );
+}
+}
+}
+
+using Foam::constant::mathematical::pi;
+
+
+// * * * * * * * * * * * * * * * * Constructors  * * * * * * * * * * * * * * //
+
+Foam::diameterModels::coalescenceModels::AdachiStuartFokkink::
+AdachiStuartFokkink
+(
+    const populationBalanceModel& popBal,
+    const dictionary& dict
+)
+:
+    coalescenceModel(popBal, dict)
+{}
+
+
+// * * * * * * * * * * * * * * * Member Functions  * * * * * * * * * * * * * //
+
+void Foam::diameterModels::coalescenceModels::AdachiStuartFokkink::
+addToCoalescenceRate
+(
+    volScalarField& coalescenceRate,
+    const label i,
+    const label j
+)
+{
+    const sizeGroup& fi = popBal_.sizeGroups()[i];
+    const sizeGroup& fj = popBal_.sizeGroups()[j];
+
+    coalescenceRate +=
+        (4.0/3.0)
+       *sqrt
+        (
+            0.3*pi*popBal_.continuousTurbulence().epsilon()
+           /popBal_.continuousPhase().thermo().nu()
+        )
+       *pow3(fi.d() + fj.d());
+}
+
+
+// ************************************************************************* //
