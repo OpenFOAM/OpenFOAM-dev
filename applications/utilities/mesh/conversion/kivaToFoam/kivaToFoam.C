48b7805d37eb46800cb25523b9fd38a2d3cf65c0
Henry Weller <http://cfd.direct>
2022-07-05 17:06:07 +0100

    kivaToFoam: Corrected usage printing

--- applications/utilities/mesh/conversion/kivaToFoam/kivaToFoam.C
+++ applications/utilities/mesh/conversion/kivaToFoam/kivaToFoam.C
@@ -2,7 +2,7 @@
   =========                 |
   \\      /  F ield         | OpenFOAM: The Open Source CFD Toolbox
    \\    /   O peration     | Website:  https://openfoam.org
-    \\  /    A nd           | Copyright (C) 2011-2020 OpenFOAM Foundation
+    \\  /    A nd           | Copyright (C) 2011-2022 OpenFOAM Foundation
      \\/     M anipulation  |
 -------------------------------------------------------------------------------
 License
@@ -99,12 +99,11 @@ int main(int argc, char *argv[])
         }
         else
         {
+            args.printUsage();
+
             FatalErrorInFunction
                 << "KIVA file version " << versionName << " not supported"
                 << exit(FatalError);
-
-            args.printUsage();
-            FatalError.exit(1);
         }
     }
 
