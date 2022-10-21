2f709bf22b66c14752b2d956285c4fefefdb34c9
Will Bainbridge <http://cfd.direct>
2022-10-21 11:05:41 +0100

    gradSchemes: leastSquares: Fix bug preventing use with NCC

--- src/finiteVolume/finiteVolume/gradSchemes/leastSquaresGrad/leastSquaresVectors.C
+++ src/finiteVolume/finiteVolume/gradSchemes/leastSquaresGrad/leastSquaresVectors.C
@@ -2,7 +2,7 @@
   =========                 |
   \\      /  F ield         | OpenFOAM: The Open Source CFD Toolbox
    \\    /   O peration     | Website:  https://openfoam.org
-    \\  /    A nd           | Copyright (C) 2011-2018 OpenFOAM Foundation
+    \\  /    A nd           | Copyright (C) 2011-2022 OpenFOAM Foundation
      \\/     M anipulation  |
 -------------------------------------------------------------------------------
 License
@@ -123,7 +123,7 @@ void Foam::leastSquaresVectors::calcLeastSquaresVectors()
         const fvsPatchScalarField& pMagSf = magSf.boundaryField()[patchi];
 
         const fvPatch& p = pw.patch();
-        const labelUList& faceCells = p.patch().faceCells();
+        const labelUList& faceCells = p.faceCells();
 
         // Build the d-vectors
         vectorField pd(p.delta());
