29b82422d74d1ac5ea7116a33e011399d6fada8a
Henry Weller <http://cfd.direct>
2022-11-16 23:28:59 +0000

    applications/solvers/modules: Reorganised to match the structure of tutorials/modules
    Given that the number of solid solver modules is currently 1 and unlikely to
    exceed 3 it is not very useful to maintain solid and fluid sub-directories and
    easier to see the correspondence between the solver modules and tutorial cases
    without.

---
+++ applications/solvers/modules/multiphaseEuler/phaseSystems/phaseModel/phaseModel/phaseModelNew.C
@@ -0,0 +1,60 @@
+/*---------------------------------------------------------------------------*\
+  =========                 |
+  \\      /  F ield         | OpenFOAM: The Open Source CFD Toolbox
+   \\    /   O peration     | Website:  https://openfoam.org
+    \\  /    A nd           | Copyright (C) 2015-2022 OpenFOAM Foundation
+     \\/     M anipulation  |
+-------------------------------------------------------------------------------
+License
+    This file is part of OpenFOAM.
+
+    OpenFOAM is free software: you can redistribute it and/or modify it
+    under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    OpenFOAM is distributed in the hope that it will be useful, but WITHOUT
+    ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+    FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License
+    for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with OpenFOAM.  If not, see <http://www.gnu.org/licenses/>.
+
+\*---------------------------------------------------------------------------*/
+
+#include "phaseModel.H"
+#include "phaseSystem.H"
+
+// * * * * * * * * * * * * * * * * Selector  * * * * * * * * * * * * * * * * //
+
+Foam::autoPtr<Foam::phaseModel> Foam::phaseModel::New
+(
+    const phaseSystem& fluid,
+    const word& phaseName,
+    const bool referencePhase,
+    const label index
+)
+{
+    word phaseModelType(fluid.subDict(phaseName).lookup("type"));
+
+    Info<< "Selecting phaseModel for "
+        << phaseName << ": " << phaseModelType << endl;
+
+    phaseSystemConstructorTable::iterator cstrIter =
+        phaseSystemConstructorTablePtr_->find(phaseModelType);
+
+    if (cstrIter == phaseSystemConstructorTablePtr_->end())
+    {
+        FatalErrorInFunction
+            << "Unknown phaseModelType type "
+            << phaseModelType << endl << endl
+            << "Valid phaseModel types are : " << endl
+            << phaseSystemConstructorTablePtr_->sortedToc()
+            << exit(FatalError);
+    }
+
+    return cstrIter()(fluid, phaseName, referencePhase, index);
+}
+
+// ************************************************************************* //
---
+++ applications/solvers/modules/multiphaseEuler/phaseSystems/populationBalanceModel/driftModels/driftModel/driftModel.C
@@ -0,0 +1,90 @@
+/*---------------------------------------------------------------------------*\
+  =========                 |
+  \\      /  F ield         | OpenFOAM: The Open Source CFD Toolbox
+   \\    /   O peration     | Website:  https://openfoam.org
+    \\  /    A nd           | Copyright (C) 2017-2022 OpenFOAM Foundation
+     \\/     M anipulation  |
+-------------------------------------------------------------------------------
+License
+    This file is part of OpenFOAM.
+
+    OpenFOAM is free software: you can redistribute it and/or modify it
+    under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    OpenFOAM is distributed in the hope that it will be useful, but WITHOUT
+    ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+    FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License
+    for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with OpenFOAM.  If not, see <http://www.gnu.org/licenses/>.
+
+\*---------------------------------------------------------------------------*/
+
+#include "driftModel.H"
+#include "phaseSystem.H"
+
+// * * * * * * * * * * * * * * Static Data Members * * * * * * * * * * * * * //
+
+namespace Foam
+{
+namespace diameterModels
+{
+    defineTypeNameAndDebug(driftModel, 0);
+    defineRunTimeSelectionTable(driftModel, dictionary);
+}
+}
+
+
+// * * * * * * * * * * * * * * * * Selector  * * * * * * * * * * * * * * * * //
+
+Foam::autoPtr<Foam::diameterModels::driftModel>
+Foam::diameterModels::driftModel::New
+(
+    const word& type,
+    const populationBalanceModel& popBal,
+    const dictionary& dict
+)
+{
+    Info<< "Selecting drift model for "
+        << popBal.name() << ": " << type << endl;
+
+    dictionaryConstructorTable::iterator cstrIter =
+        dictionaryConstructorTablePtr_->find(type);
+
+    if (cstrIter == dictionaryConstructorTablePtr_->end())
+    {
+        FatalErrorInFunction
+            << "Unknown drift model type "
+            << type << nl << nl
+            << "Valid drift model types : " << endl
+            << dictionaryConstructorTablePtr_->sortedToc()
+            << exit(FatalError);
+    }
+
+    return autoPtr<driftModel>(cstrIter()(popBal, dict));
+}
+
+
+// * * * * * * * * * * * * * * * * Constructors  * * * * * * * * * * * * * * //
+
+Foam::diameterModels::driftModel::driftModel
+(
+    const populationBalanceModel& popBal,
+    const dictionary& dict
+)
+:
+    popBal_(popBal),
+    dict_(dict)
+{}
+
+
+// * * * * * * * * * * * * * * * Member Functions  * * * * * * * * * * * * * //
+
+void Foam::diameterModels::driftModel::precompute()
+{}
+
+
+// ************************************************************************* //
