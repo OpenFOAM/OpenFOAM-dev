35256e52809f2c9166ac8fee40e5ec112b1d61f3
Henry Weller <http://cfd.direct>
2023-01-10 18:48:41 +0000

    rhoCentralFoam: Added 'const' to constant field declarations to improve readability

--- applications/solvers/compressible/rhoCentralFoam/rhoCentralFoam.C
+++ applications/solvers/compressible/rhoCentralFoam/rhoCentralFoam.C
@@ -2,7 +2,7 @@
   =========                 |
   \\      /  F ield         | OpenFOAM: The Open Source CFD Toolbox
    \\    /   O peration     | Website:  https://openfoam.org
-    \\  /    A nd           | Copyright (C) 2011-2022 OpenFOAM Foundation
+    \\  /    A nd           | Copyright (C) 2011-2023 OpenFOAM Foundation
      \\/     M anipulation  |
 -------------------------------------------------------------------------------
 License
@@ -86,24 +86,24 @@ int main(int argc, char *argv[])
 
         // --- Directed interpolation of primitive fields onto faces
 
-        surfaceScalarField rho_pos(interpolate(rho, pos));
-        surfaceScalarField rho_neg(interpolate(rho, neg));
+        const surfaceScalarField rho_pos(interpolate(rho, pos));
+        const surfaceScalarField rho_neg(interpolate(rho, neg));
 
-        surfaceVectorField rhoU_pos(interpolate(rhoU, pos, U.name()));
-        surfaceVectorField rhoU_neg(interpolate(rhoU, neg, U.name()));
+        const surfaceVectorField rhoU_pos(interpolate(rhoU, pos, U.name()));
+        const surfaceVectorField rhoU_neg(interpolate(rhoU, neg, U.name()));
 
-        volScalarField rPsi("rPsi", 1.0/psi);
-        surfaceScalarField rPsi_pos(interpolate(rPsi, pos, T.name()));
-        surfaceScalarField rPsi_neg(interpolate(rPsi, neg, T.name()));
+        const volScalarField rPsi("rPsi", 1.0/psi);
+        const surfaceScalarField rPsi_pos(interpolate(rPsi, pos, T.name()));
+        const surfaceScalarField rPsi_neg(interpolate(rPsi, neg, T.name()));
 
-        surfaceScalarField e_pos(interpolate(e, pos, T.name()));
-        surfaceScalarField e_neg(interpolate(e, neg, T.name()));
+        const surfaceScalarField e_pos(interpolate(e, pos, T.name()));
+        const surfaceScalarField e_neg(interpolate(e, neg, T.name()));
 
-        surfaceVectorField U_pos("U_pos", rhoU_pos/rho_pos);
-        surfaceVectorField U_neg("U_neg", rhoU_neg/rho_neg);
+        const surfaceVectorField U_pos("U_pos", rhoU_pos/rho_pos);
+        const surfaceVectorField U_neg("U_neg", rhoU_neg/rho_neg);
 
-        surfaceScalarField p_pos("p_pos", rho_pos*rPsi_pos);
-        surfaceScalarField p_neg("p_neg", rho_neg*rPsi_neg);
+        const surfaceScalarField p_pos("p_pos", rho_pos*rPsi_pos);
+        const surfaceScalarField p_neg("p_neg", rho_neg*rPsi_neg);
 
         surfaceScalarField phiv_pos("phiv_pos", U_pos & mesh.Sf());
         surfaceScalarField phiv_neg("phiv_neg", U_neg & mesh.Sf());
@@ -115,24 +115,24 @@ int main(int argc, char *argv[])
             phiv_neg -= mesh.phi();
         }
 
-        volScalarField c("c", sqrt(thermo.Cp()/thermo.Cv()*rPsi));
-        surfaceScalarField cSf_pos
+        const volScalarField c("c", sqrt(thermo.Cp()/thermo.Cv()*rPsi));
+        const surfaceScalarField cSf_pos
         (
             "cSf_pos",
             interpolate(c, pos, T.name())*mesh.magSf()
         );
-        surfaceScalarField cSf_neg
+        const surfaceScalarField cSf_neg
         (
             "cSf_neg",
             interpolate(c, neg, T.name())*mesh.magSf()
         );
 
-        surfaceScalarField ap
+        const surfaceScalarField ap
         (
             "ap",
             max(max(phiv_pos + cSf_pos, phiv_neg + cSf_neg), v_zero)
         );
-        surfaceScalarField am
+        const surfaceScalarField am
         (
             "am",
             min(min(phiv_pos - cSf_pos, phiv_neg - cSf_neg), v_zero)
@@ -150,13 +150,13 @@ int main(int argc, char *argv[])
             a_pos = 0.5;
         }
 
-        surfaceScalarField a_neg("a_neg", 1.0 - a_pos);
+        const surfaceScalarField a_neg("a_neg", 1.0 - a_pos);
 
         phiv_pos *= a_pos;
         phiv_neg *= a_neg;
 
-        surfaceScalarField aphiv_pos("aphiv_pos", phiv_pos - aSf);
-        surfaceScalarField aphiv_neg("aphiv_neg", phiv_neg + aSf);
+        const surfaceScalarField aphiv_pos("aphiv_pos", phiv_pos - aSf);
+        const surfaceScalarField aphiv_neg("aphiv_neg", phiv_neg + aSf);
 
         // Reuse amaxSf for the maximum positive and negative fluxes
         // estimated by the central scheme
@@ -174,98 +174,102 @@ int main(int argc, char *argv[])
 
         phi = aphiv_pos*rho_pos + aphiv_neg*rho_neg;
 
-        surfaceVectorField phiUp
-        (
-            (aphiv_pos*rhoU_pos + aphiv_neg*rhoU_neg)
-          + (a_pos*p_pos + a_neg*p_neg)*mesh.Sf()
-        );
-
-        surfaceScalarField phiEp
-        (
-            "phiEp",
-            aphiv_pos*(rho_pos*(e_pos + 0.5*magSqr(U_pos)) + p_pos)
-          + aphiv_neg*(rho_neg*(e_neg + 0.5*magSqr(U_neg)) + p_neg)
-          + aSf*p_pos - aSf*p_neg
-        );
-
-        // Make flux for pressure-work absolute
-        if (mesh.moving())
-        {
-            phiEp += mesh.phi()*(a_pos*p_pos + a_neg*p_neg);
-        }
-
-        volScalarField muEff("muEff", rho*turbulence->nuEff());
-        volTensorField tauMC("tauMC", muEff*dev2(Foam::T(fvc::grad(U))));
-
         // --- Solve density
         solve(fvm::ddt(rho) + fvc::div(phi));
 
         turbulence->predict();
         thermophysicalTransport->predict();
 
+        const volScalarField muEff("muEff", rho*turbulence->nuEff());
+        const volTensorField tauMC("tauMC", muEff*dev2(Foam::T(fvc::grad(U))));
+
         // --- Solve momentum
-        solve(fvm::ddt(rhoU) + fvc::div(phiUp));
-
-        U.ref() =
-            rhoU()
-           /rho();
-        U.correctBoundaryConditions();
-        rhoU.boundaryFieldRef() == rho.boundaryField()*U.boundaryField();
-
-        if (!inviscid)
         {
-            solve
+            const surfaceVectorField phiUp
             (
-                fvm::ddt(rho, U) - fvc::ddt(rho, U)
-              - fvm::laplacian(muEff, U)
-              - fvc::div(tauMC)
+                (aphiv_pos*rhoU_pos + aphiv_neg*rhoU_neg)
+              + (a_pos*p_pos + a_neg*p_neg)*mesh.Sf()
             );
-            rhoU = rho*U;
+
+            solve(fvm::ddt(rhoU) + fvc::div(phiUp));
+
+            U.ref() =
+                rhoU()
+               /rho();
+            U.correctBoundaryConditions();
+            rhoU.boundaryFieldRef() == rho.boundaryField()*U.boundaryField();
+
+            if (!inviscid)
+            {
+                solve
+                (
+                    fvm::ddt(rho, U) - fvc::ddt(rho, U)
+                  - fvm::laplacian(muEff, U)
+                  - fvc::div(tauMC)
+                );
+                rhoU = rho*U;
+            }
         }
 
         // --- Solve energy
-        surfaceScalarField sigmaDotU
-        (
-            "sigmaDotU",
+        {
+            surfaceScalarField phiEp
             (
-                fvc::interpolate(muEff)*mesh.magSf()*fvc::snGrad(U)
-              + fvc::dotInterpolate(mesh.Sf(), tauMC)
-            )
-          & (a_pos*U_pos + a_neg*U_neg)
-        );
-
-        solve
-        (
-            fvm::ddt(rhoE)
-          + fvc::div(phiEp)
-          - fvc::div(sigmaDotU)
-        );
-
-        e = rhoE/rho - 0.5*magSqr(U);
-        e.correctBoundaryConditions();
-        thermo.correct();
-        rhoE.boundaryFieldRef() ==
-            rho.boundaryField()*
-            (
-                e.boundaryField() + 0.5*magSqr(U.boundaryField())
+                "phiEp",
+                aphiv_pos*(rho_pos*(e_pos + 0.5*magSqr(U_pos)) + p_pos)
+              + aphiv_neg*(rho_neg*(e_neg + 0.5*magSqr(U_neg)) + p_neg)
+              + aSf*p_pos - aSf*p_neg
             );
 
-        if (!inviscid)
-        {
+            // Make flux for pressure-work absolute
+            if (mesh.moving())
+            {
+                phiEp += mesh.phi()*(a_pos*p_pos + a_neg*p_neg);
+            }
+
+            const surfaceScalarField sigmaDotU
+            (
+                "sigmaDotU",
+                (
+                    fvc::interpolate(muEff)*mesh.magSf()*fvc::snGrad(U)
+                  + fvc::dotInterpolate(mesh.Sf(), tauMC)
+                )
+              & (a_pos*U_pos + a_neg*U_neg)
+            );
+
             solve
             (
-                fvm::ddt(rho, e) - fvc::ddt(rho, e)
-              + thermophysicalTransport->divq(e)
+                fvm::ddt(rhoE)
+              + fvc::div(phiEp)
+              - fvc::div(sigmaDotU)
             );
+
+            e = rhoE/rho - 0.5*magSqr(U);
+            e.correctBoundaryConditions();
             thermo.correct();
-            rhoE = rho*(e + 0.5*magSqr(U));
+            rhoE.boundaryFieldRef() ==
+                rho.boundaryField()*
+                (
+                    e.boundaryField() + 0.5*magSqr(U.boundaryField())
+                );
+
+            if (!inviscid)
+            {
+                solve
+                (
+                    fvm::ddt(rho, e) - fvc::ddt(rho, e)
+                  + thermophysicalTransport->divq(e)
+                );
+                thermo.correct();
+                rhoE = rho*(e + 0.5*magSqr(U));
+            }
         }
 
-        p.ref() =
-            rho()
-           /psi();
-        p.correctBoundaryConditions();
-        rho.boundaryFieldRef() == psi.boundaryField()*p.boundaryField();
+        {
+            p.ref() = rho()/psi();
+            p.correctBoundaryConditions();
+            rho.boundaryFieldRef() == psi.boundaryField()*p.boundaryField();
+        }
 
         turbulence->correct();
         thermophysicalTransport->correct();
