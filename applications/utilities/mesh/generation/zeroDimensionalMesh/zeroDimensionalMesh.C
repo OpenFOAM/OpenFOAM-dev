099505df9cf59054cd8ac19047dea091413fcf56
Will Bainbridge <http://cfd.direct>
2023-01-26 14:39:08 +0000

    zeroDimensionalFvMesh: New utility to create zero dimensional mesh
    This utility can be run with no arguments or configuration, and will
    create a unit cube mesh with empty patches on all sides.

---
+++ applications/utilities/mesh/generation/zeroDimensionalMesh/zeroDimensionalMesh.C
@@ -0,0 +1,58 @@
+/*---------------------------------------------------------------------------*\
+  =========                 |
+  \\      /  F ield         | OpenFOAM: The Open Source CFD Toolbox
+   \\    /   O peration     | Website:  https://openfoam.org
+    \\  /    A nd           | Copyright (C) 2023 OpenFOAM Foundation
+     \\/     M anipulation  |
+-------------------------------------------------------------------------------
+License
+    This file is part of OpenFOAM.
+
+    OpenFOAM is free software: you can redistribute it and/or modify it
+    under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    OpenFOAM is distributed in the hope that it will be useful, but WITHOUT
+    ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+    FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License
+    for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with OpenFOAM.  If not, see <http://www.gnu.org/licenses/>.
+
+Application
+    zeroDimensionalMesh
+
+Description
+    Creates a zero-dimensional mesh
+
+\*---------------------------------------------------------------------------*/
+
+#include "argList.H"
+#include "zeroDimensionalFvMesh.H"
+
+using namespace Foam;
+
+// * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * //
+
+int main(int argc, char *argv[])
+{
+    argList::noParallel();
+
+    #include "setRootCase.H"
+    #include "createTime.H"
+
+    fvMesh mesh(zeroDimensionalFvMesh(runTime));
+
+    mesh.setInstance(runTime.constant());
+
+    mesh.write();
+
+    Info<< "End" << nl << endl;
+
+    return 0;
+}
+
+
+// ************************************************************************* //
