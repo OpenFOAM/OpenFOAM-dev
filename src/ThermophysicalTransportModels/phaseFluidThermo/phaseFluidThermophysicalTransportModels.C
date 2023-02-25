a838fa9a910ca74a12dcf17ab3820ab032b951bc
Henry Weller <http://cfd.direct>
2023-02-25 23:14:37 +0000

    ThermophysicalTransportModels: separated macros in makeThermophysicalTransportModel
    to allow tables for specific types of ThermophysicalTransportModel to be
    created, i.e. laminar, RAS or LES rather than always creating all three.

--- src/ThermophysicalTransportModels/phaseFluidThermo/phaseFluidThermophysicalTransportModels.C
+++ src/ThermophysicalTransportModels/phaseFluidThermo/phaseFluidThermophysicalTransportModels.C
@@ -2,7 +2,7 @@
   =========                 |
   \\      /  F ield         | OpenFOAM: The Open Source CFD Toolbox
    \\    /   O peration     | Website:  https://openfoam.org
-    \\  /    A nd           | Copyright (C) 2020-2021 OpenFOAM Foundation
+    \\  /    A nd           | Copyright (C) 2020-2023 OpenFOAM Foundation
      \\/     M anipulation  |
 -------------------------------------------------------------------------------
 License
@@ -27,7 +27,7 @@ License
 
 // * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * //
 
-makeThermophysicalTransportModels
+makeThermophysicalTransportModelTables
 (
     PhaseThermophysicalTransportModel,
     phaseCompressibleMomentumTransportModel,
