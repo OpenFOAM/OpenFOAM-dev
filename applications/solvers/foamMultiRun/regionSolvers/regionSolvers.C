9621cbf704409d992026abaa956239cc4a3ac59c
Henry Weller <http://cfd.direct>
2023-03-07 15:37:55 +0000

    regionSolvers: Added #include "solver.H"

--- applications/solvers/foamMultiRun/regionSolvers/regionSolvers.C
+++ applications/solvers/foamMultiRun/regionSolvers/regionSolvers.C
@@ -24,6 +24,7 @@ License
 \*---------------------------------------------------------------------------*/
 
 #include "regionSolvers.H"
+#include "solver.H"
 #include "Time.H"
 
 // * * * * * * * * * * * * * * * * Constructors  * * * * * * * * * * * * * * //
