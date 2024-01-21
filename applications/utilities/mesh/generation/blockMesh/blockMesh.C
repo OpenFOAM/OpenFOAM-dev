d308752ea4554ec2e8dba1623c6df587ca92cf71
Henry Weller <http://cfd.direct>
2024-01-21 09:30:55 +0000

    blockMesh: Removed unused -noFunctionObjects option

--- applications/utilities/mesh/generation/blockMesh/blockMesh.C
+++ applications/utilities/mesh/generation/blockMesh/blockMesh.C
@@ -2,7 +2,7 @@
   =========                 |
   \\      /  F ield         | OpenFOAM: The Open Source CFD Toolbox
    \\    /   O peration     | Website:  https://openfoam.org
-    \\  /    A nd           | Copyright (C) 2011-2023 OpenFOAM Foundation
+    \\  /    A nd           | Copyright (C) 2011-2024 OpenFOAM Foundation
      \\/     M anipulation  |
 -------------------------------------------------------------------------------
 License
@@ -65,6 +65,7 @@ using namespace Foam;
 int main(int argc, char *argv[])
 {
     argList::noParallel();
+    Foam::argList::removeOption("noFunctionObjects");
     #include "addDictOption.H"
     argList::addBoolOption
     (
