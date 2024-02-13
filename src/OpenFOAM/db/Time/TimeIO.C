fd18d7e528ee65cc12370250ced8e2cd77981838
Henry Weller <http://cfd.direct>
2024-02-13 21:23:49 +0000

    Time: Maintain the endTime value if present even if not needed

--- src/OpenFOAM/db/Time/TimeIO.C
+++ src/OpenFOAM/db/Time/TimeIO.C
@@ -2,7 +2,7 @@
   =========                 |
   \\      /  F ield         | OpenFOAM: The Open Source CFD Toolbox
    \\    /   O peration     | Website:  https://openfoam.org
-    \\  /    A nd           | Copyright (C) 2011-2023 OpenFOAM Foundation
+    \\  /    A nd           | Copyright (C) 2011-2024 OpenFOAM Foundation
      \\/     M anipulation  |
 -------------------------------------------------------------------------------
 License
@@ -128,7 +128,7 @@ void Foam::Time::readDict()
     {
         stopAt_ = stopAtControlNames.read(controlDict_.lookup("stopAt"));
 
-        if (stopAt_ == stopAtControl::endTime)
+        if (stopAt_ == stopAtControl::endTime || controlDict_.found("endTime"))
         {
             endTime_ = userTimeToTime(controlDict_.lookup<scalar>("endTime"));
         }
