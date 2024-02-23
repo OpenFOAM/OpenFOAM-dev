ef1b4fd3860aab040547059f7bac324110d31c9d
Henry Weller <http://cfd.direct>
2024-02-23 15:34:34 +0000

    primitiveMeshCheck: Added "***" to the "Faces with invalid vertex labels found" error message

--- src/meshCheck/primitiveMeshCheck/primitiveMeshCheck.C
+++ src/meshCheck/primitiveMeshCheck/primitiveMeshCheck.C
@@ -2,7 +2,7 @@
   =========                 |
   \\      /  F ield         | OpenFOAM: The Open Source CFD Toolbox
    \\    /   O peration     | Website:  https://openfoam.org
-    \\  /    A nd           | Copyright (C) 2011-2023 OpenFOAM Foundation
+    \\  /    A nd           | Copyright (C) 2011-2024 OpenFOAM Foundation
      \\/     M anipulation  |
 -------------------------------------------------------------------------------
 License
@@ -1564,7 +1564,7 @@ bool Foam::meshCheck::checkFaceVertices
     {
         if (report)
         {
-            Info<< "    Faces with invalid vertex labels found, "
+            Info<< " ***Faces with invalid vertex labels found, "
                 << " number of faces: " << nErrorFaces << endl;
         }
 
