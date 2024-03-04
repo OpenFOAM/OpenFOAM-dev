c1615395978d8f44c0b23973649ccb63ab1ab6d3
Henry Weller <http://cfd.direct>
2024-03-04 15:40:03 +0000

    filmEjectionModels::BrunDripping: Removed unnecessary ejectionModel::correct()

--- applications/modules/isothermalFilm/fvModels/filmCloudTransfer/ejectionModels/BrunDripping/BrunDripping.C
+++ applications/modules/isothermalFilm/fvModels/filmCloudTransfer/ejectionModels/BrunDripping/BrunDripping.C
@@ -2,7 +2,7 @@
   =========                 |
   \\      /  F ield         | OpenFOAM: The Open Source CFD Toolbox
    \\    /   O peration     | Website:  https://openfoam.org
-    \\  /    A nd           | Copyright (C) 2016-2023 OpenFOAM Foundation
+    \\  /    A nd           | Copyright (C) 2016-2024 OpenFOAM Foundation
      \\/     M anipulation  |
 -------------------------------------------------------------------------------
 License
@@ -134,8 +134,6 @@ void BrunDripping::correct()
             }
         }
     }
-
-    ejectionModel::correct();
 }
 
 
