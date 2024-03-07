fbfd35dfc4100e8faa1867834573e837716eaba3
Will Bainbridge <http://cfd.direct>
2024-03-07 11:56:24 +0000

    prghCyclicPressure: New cyclic boundary condition for p_rgh
    This boundary condition provides a cyclic condition for p_rgh. It applies
    corrections to the value and gradient on both sides of the cyclic to
    account for the non-cylicity of the gravitational force.
    
    This condition is only needed when the cyclic patches have a transformation
    and a normal component in the direction of gravity. If the cyclic patches
    are orthogonal to the direction gravity, then a normal cyclic boundary
    condition can be used instead.
    
    Care must be taken when using this boundary condition that the simulation
    is actually cyclic. The following constraints apply:
    
    - Both cyclic patches must be oriented in the same way with respect to
      gravity. In practice this means that applicability is limited to cyclics
      with translational transformations.
    
    - The model cannot have any dependence on the absolute value of the
      pressure field. The absolute value of the pressure, in reality, varies
      between each repetition of the geometry; it is not actually formally
      cyclic. Only the gradient of the pressure field can be truly cyclic. This
      model is therefore only valid if the absolute value of the pressure is
      arbitrary, and only the gradient has an effect on the solution. This is
      the case for incompressible multiphase solutions or incompressible
      Boussinesq-like models of density variation. It is not true if (for
      example) a compressible thermodynamic model is being used.
    
    Specification is as follows. A "patchType" entry must be provided to
    indicate that this condition overrides the underlying cyclic constraint,
    and a "rhoInf" entry is needed (by the owner patch only) to specify the
    density of the far-field environment. For example:
    
        cyclicA
        {
            type            prghCyclicPressure;
            patchType       cyclic;
            rhoInf          1; // [kg/m^3]
        }
    
        cyclicB
        {
            type            prghCyclicPressure;
            patchType       cyclic;
        }
    
    A tutorial, incompressibleVoF/trayedPipe, has been added to demonstrate
    usage of this boundary condition.

---
+++ src/finiteVolume/fields/fvPatchFields/derived/prghCyclicPressure/prghCyclicPressureFvPatchScalarField.C
@@ -0,0 +1,207 @@
+/*---------------------------------------------------------------------------*\
+  =========                 |
+  \\      /  F ield         | OpenFOAM: The Open Source CFD Toolbox
+   \\    /   O peration     | Website:  https://openfoam.org
+    \\  /    A nd           | Copyright (C) 2024 OpenFOAM Foundation
+     \\/     M anipulation  |
+-------------------------------------------------------------------------------
+License
+    This file is part of OpenFOAM.
+
+    OpenFOAM is free software: you can redistribute it and/or modify it
+    under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    OpenFOAM is distributed in the hope that it will be useful, but WITHOUT
+    ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+    FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License
+    for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with OpenFOAM.  If not, see <http://www.gnu.org/licenses/>.
+
+\*---------------------------------------------------------------------------*/
+
+#include "prghCyclicPressureFvPatchScalarField.H"
+#include "addToRunTimeSelectionTable.H"
+#include "fieldMapper.H"
+#include "volFields.H"
+#include "surfaceFields.H"
+#include "fvcSnGrad.H"
+
+// * * * * * * * * * * * * * * * * Constructors  * * * * * * * * * * * * * * //
+
+Foam::prghCyclicPressureFvPatchScalarField::prghCyclicPressureFvPatchScalarField
+(
+    const fvPatch& p,
+    const DimensionedField<scalar, volMesh>& iF,
+    const dictionary& dict
+)
+:
+    jumpCyclicFvPatchScalarField(p, iF, dict),
+    rhoName_
+    (
+        cyclicPatch().owner()
+      ? dict.lookupOrDefault<word>("rho", "rho")
+      : word::null
+    ),
+    rhoInf_(cyclicPatch().owner() ? dict.lookup<scalar>("rhoInf") : NaN),
+    jump_(p.size(), Zero)
+{
+    if (dict.found("jump"))
+    {
+        jump_ = scalarField("jump", dict, p.size());
+    }
+
+    evaluateNoUpdateCoeffs();
+}
+
+
+Foam::prghCyclicPressureFvPatchScalarField::prghCyclicPressureFvPatchScalarField
+(
+    const prghCyclicPressureFvPatchScalarField& ptf,
+    const fvPatch& p,
+    const DimensionedField<scalar, volMesh>& iF,
+    const fieldMapper& mapper
+)
+:
+    jumpCyclicFvPatchScalarField(ptf, p, iF, mapper),
+    rhoName_(ptf.rhoName_),
+    rhoInf_(ptf.rhoInf_),
+    jump_(mapper(ptf.jump_))
+{}
+
+
+Foam::prghCyclicPressureFvPatchScalarField::prghCyclicPressureFvPatchScalarField
+(
+    const prghCyclicPressureFvPatchScalarField& ptf,
+    const DimensionedField<scalar, volMesh>& iF
+)
+:
+    jumpCyclicFvPatchScalarField(ptf, iF),
+    rhoName_(ptf.rhoName_),
+    rhoInf_(ptf.rhoInf_),
+    jump_(ptf.jump_)
+{}
+
+
+// * * * * * * * * * * * * * * * Member Functions  * * * * * * * * * * * * * //
+
+Foam::tmp<Foam::scalarField>
+Foam::prghCyclicPressureFvPatchScalarField::jump() const
+{
+    return jump_;
+}
+
+
+void Foam::prghCyclicPressureFvPatchScalarField::map
+(
+    const fvPatchScalarField& ptf,
+    const fieldMapper& mapper
+)
+{
+    jumpCyclicFvPatchScalarField::map(ptf, mapper);
+
+    const prghCyclicPressureFvPatchScalarField& tiptf =
+        refCast<const prghCyclicPressureFvPatchScalarField>(ptf);
+
+    mapper(jump_, tiptf.jump_);
+}
+
+
+void Foam::prghCyclicPressureFvPatchScalarField::reset
+(
+    const fvPatchScalarField& ptf
+)
+{
+    jumpCyclicFvPatchScalarField::reset(ptf);
+
+    const prghCyclicPressureFvPatchScalarField& tiptf =
+        refCast<const prghCyclicPressureFvPatchScalarField>(ptf);
+
+    jump_.reset(tiptf.jump_);
+}
+
+
+void Foam::prghCyclicPressureFvPatchScalarField::updateCoeffs()
+{
+    if (updated())
+    {
+        return;
+    }
+
+    const volScalarField& vf =
+        static_cast<const volScalarField&>(internalField());
+
+    const prghCyclicPressureFvPatchScalarField& nbrPf =
+        refCast<const prghCyclicPressureFvPatchScalarField>(nbrPatchField());
+
+    const label patchi = patch().index(), nbrPatchi = nbrPf.patch().index();
+
+    // Buoyancy fields
+    const volScalarField& rhoVf =
+        db().lookupObject<volScalarField>
+        (cyclicPatch().owner() ? rhoName_ : nbrPf.rhoName_);
+    const volScalarField::Boundary& rhoBf = rhoVf.boundaryField();
+    const surfaceScalarField::Boundary& ghfBf =
+        db().lookupObject<surfaceScalarField>("ghf").boundaryField();
+
+    // Pressure solution fields
+    const surfaceScalarField::Boundary& rAUfBf =
+        db().lookupObject<surfaceScalarField>("rAUf").boundaryField();
+    const surfaceScalarField::Boundary& phiHbyABf =
+        db().lookupObject<surfaceScalarField>("phiHbyA").boundaryField();
+
+    // Delta coefficients for this field
+    const tmp<surfaceScalarField> deltaCoeffsSf =
+        fv::snGradScheme<scalar>::New
+        (
+            vf.mesh(),
+            vf.mesh().schemes().snGrad(internalField().name())
+        )->deltaCoeffs(vf);
+    const scalarField& deltaCoeffsPf =
+        deltaCoeffsSf->boundaryField()[patch().index()];
+
+    // Calculate the jump
+    jump_ =
+        (rhoBf[patchi] - (cyclicPatch().owner() ? rhoInf_ : nbrPf.rhoInf_))
+       *(ghfBf[nbrPatchi] - ghfBf[patchi])
+      + (
+            phiHbyABf[patchi]/rAUfBf[patchi]/patch().magSf()
+          + phiHbyABf[nbrPatchi]/rAUfBf[nbrPatchi]/nbrPf.patch().magSf()
+        )*patch().weights()/deltaCoeffsPf;
+
+    jumpCyclicFvPatchScalarField::updateCoeffs();
+}
+
+
+void Foam::prghCyclicPressureFvPatchScalarField::write
+(
+    Ostream& os
+) const
+{
+    fvPatchScalarField::write(os);
+
+    if (cyclicPatch().owner())
+    {
+        writeEntryIfDifferent<word>(os, "rho", "rho", rhoName_);
+        writeEntry(os, "rhoInf", rhoInf_);
+    }
+
+    writeEntry(os, "jump", jump_);
+}
+
+
+// * * * * * * * * * * * * * * Build Macro Function  * * * * * * * * * * * * //
+
+namespace Foam
+{
+    makePatchTypeField
+    (
+        fvPatchScalarField,
+        prghCyclicPressureFvPatchScalarField
+    );
+}
+
+// ************************************************************************* //
