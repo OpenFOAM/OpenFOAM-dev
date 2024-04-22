28e55568646c7f3a4c7a54e3506eefb84ffd3310
Henry Weller <http://cfd.direct>
2024-04-22 11:53:28 +0100

    refinementRegions: Updated triSurfacePointScalarField constructor call

--- src/mesh/snappyHexMesh/refinementRegions/refinementRegions.C
+++ src/mesh/snappyHexMesh/refinementRegions/refinementRegions.C
@@ -2,7 +2,7 @@
   =========                 |
   \\      /  F ield         | OpenFOAM: The Open Source CFD Toolbox
    \\    /   O peration     | Website:  https://openfoam.org
-    \\  /    A nd           | Copyright (C) 2011-2023 OpenFOAM Foundation
+    \\  /    A nd           | Copyright (C) 2011-2024 OpenFOAM Foundation
      \\/     M anipulation  |
 -------------------------------------------------------------------------------
 License
@@ -604,9 +604,7 @@ Foam::refinementRegions::refinementRegions
                                 surface.searchableSurface::time(),
                                 IOobject::MUST_READ
                             ),
-                            tsm,
-                            dimLength,
-                            true
+                            tsm
                         )
                     );
                 }
