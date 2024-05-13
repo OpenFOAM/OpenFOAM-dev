23946e83477296932f0eb7b0a504d1e0ed54cca7
Henry Weller <http://cfd.direct>
2024-05-13 12:00:53 +0100

    incompressibleDenseParticleFluid: call storeGlobalPositions for redistribution

--- applications/modules/incompressibleDenseParticleFluid/incompressibleDenseParticleFluid.C
+++ applications/modules/incompressibleDenseParticleFluid/incompressibleDenseParticleFluid.C
@@ -2,7 +2,7 @@
   =========                 |
   \\      /  F ield         | OpenFOAM: The Open Source CFD Toolbox
    \\    /   O peration     | Website:  https://openfoam.org
-    \\  /    A nd           | Copyright (C) 2023 OpenFOAM Foundation
+    \\  /    A nd           | Copyright (C) 2023-2024 OpenFOAM Foundation
      \\/     M anipulation  |
 -------------------------------------------------------------------------------
 License
@@ -267,7 +267,7 @@ void Foam::solvers::incompressibleDenseParticleFluid::preSolve()
     }
 
     // Store the particle positions
-    if (mesh.topoChanging())
+    if (mesh.topoChanging() || mesh.distributing())
     {
         clouds.storeGlobalPositions();
     }
