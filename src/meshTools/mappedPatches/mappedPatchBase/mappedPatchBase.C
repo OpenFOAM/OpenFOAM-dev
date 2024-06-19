4751a775bcd44edc04392845cfec3491635fd56a
Will Bainbridge <http://cfd.direct>
2024-06-19 13:57:09 +0100

    mappedPatchBase: Corrected copy construct of reMapAfterMove

--- src/meshTools/mappedPatches/mappedPatchBase/mappedPatchBase.C
+++ src/meshTools/mappedPatches/mappedPatchBase/mappedPatchBase.C
@@ -2,7 +2,7 @@
   =========                 |
   \\      /  F ield         | OpenFOAM: The Open Source CFD Toolbox
    \\    /   O peration     | Website:  https://openfoam.org
-    \\  /    A nd           | Copyright (C) 2011-2023 OpenFOAM Foundation
+    \\  /    A nd           | Copyright (C) 2011-2024 OpenFOAM Foundation
      \\/     M anipulation  |
 -------------------------------------------------------------------------------
 License
@@ -400,7 +400,7 @@ Foam::mappedPatchBase::mappedPatchBase
       : nullptr
     ),
     matchTol_(mpb.matchTol_),
-    reMapAfterMove_(true),
+    reMapAfterMove_(mpb.reMapAfterMove_),
     reMapNbr_(false)
 {}
 
