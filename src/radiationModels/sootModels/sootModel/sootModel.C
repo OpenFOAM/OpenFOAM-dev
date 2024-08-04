a512d3fce088334bc045e2fdff975cc5ecc02cbb
Henry Weller <http://cfd.direct>
2024-08-04 22:43:53 +0100

    radiationModels::sootModel: Removed unnecessary dictionary copy

--- src/radiationModels/sootModels/sootModel/sootModel.C
+++ src/radiationModels/sootModels/sootModel/sootModel.C
@@ -2,7 +2,7 @@
   =========                 |
   \\      /  F ield         | OpenFOAM: The Open Source CFD Toolbox
    \\    /   O peration     | Website:  https://openfoam.org
-    \\  /    A nd           | Copyright (C) 2013-2019 OpenFOAM Foundation
+    \\  /    A nd           | Copyright (C) 2013-2024 OpenFOAM Foundation
      \\/     M anipulation  |
 -------------------------------------------------------------------------------
 License
@@ -45,7 +45,6 @@ Foam::radiationModels::sootModel::sootModel
     const word& modelType
 )
 :
-    dict_(dict),
     mesh_(mesh)
 {}
 
