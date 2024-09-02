f7f9be714d8f511c8b765f3e32ab88d902b4d850
Henry Weller <http://cfd.direct>
2024-09-02 19:56:16 +0100

    multiCycleConstantbXiIgnition: New XiFluid ignition model for multi-cycle simulations with EGR
    In addition to the ignition duration the combustion duration is also specified,
    at the end of which the igniting state is set false, the regress variable reset
    to 1 and the egr field updated.  An example specification for this combustion
    model is as follows:
    
    ignition
    {
        type            multiCycleConstantbXiIgnition;
    
        select          cellZone;
        cellZone        ignition;
    
        period          720;
    
        start           717;
        duration        15;
        strength        20;
        combustionDuration 243;
    
        XiCorr
        {
            type            cylindrical;
            select          all;
            thickness       0.05;
            cylinderFraction 1;
        }
    }

---
+++ applications/modules/XiFluid/fvModels/ignition/multiCycleConstantbXiIgnition/multiCycleConstantbXiIgnition.C
@@ -0,0 +1,153 @@
+/*---------------------------------------------------------------------------*\
+  =========                 |
+  \\      /  F ield         | OpenFOAM: The Open Source CFD Toolbox
+   \\    /   O peration     | Website:  https://openfoam.org
+    \\  /    A nd           | Copyright (C) 2024 OpenFOAM Foundation
+     \\/     M anipulation  |
+-------------------------------------------------------------------------------
+License
+    This file is part of OpenFOAM.
+
+    OpenFOAM is free software: you can redistribute it and/or modify it
+    under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    OpenFOAM is distributed in the hope that it will be useful, but WITHOUT
+    ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+    FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License
+    for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with OpenFOAM.  If not, see <http://www.gnu.org/licenses/>.
+
+\*---------------------------------------------------------------------------*/
+
+#include "multiCycleConstantbXiIgnition.H"
+#include "psiuMulticomponentThermo.H"
+
+// * * * * * * * * * * * * * Static Member Functions * * * * * * * * * * * * //
+
+namespace Foam
+{
+    namespace fv
+    {
+        defineTypeNameAndDebug(multiCycleConstantbXiIgnition, 0);
+
+        addToRunTimeSelectionTable
+        (
+            fvModel,
+            multiCycleConstantbXiIgnition,
+            dictionary
+        );
+    }
+}
+
+
+// * * * * * * * * * * * * * Private Member Functions  * * * * * * * * * * * //
+
+void Foam::fv::multiCycleConstantbXiIgnition::readCoeffs(const dictionary& dict)
+{
+    period_.read(dict, mesh().time().userUnits());
+}
+
+
+// * * * * * * * * * * * * * * * * Constructors  * * * * * * * * * * * * * * //
+
+Foam::fv::multiCycleConstantbXiIgnition::multiCycleConstantbXiIgnition
+(
+    const word& name,
+    const word& modelType,
+    const fvMesh& mesh,
+    const dictionary& dict
+)
+:
+    constantbXiIgnition(name, modelType, mesh, dict),
+    period_("period", mesh().time().userUnits(), dict),
+    combustionDuration_("combustionDuration", mesh().time().userUnits(), dict),
+    ignited_(false),
+    reset_(true)
+{}
+
+
+// * * * * * * * * * * * * * * * Member Functions  * * * * * * * * * * * * * //
+
+Foam::scalar Foam::fv::multiCycleConstantbXiIgnition::repeatTime
+(
+    const scalar t
+) const
+{
+    return std::fmod(t - start_.value(), period_.value());
+}
+
+
+bool Foam::fv::multiCycleConstantbXiIgnition::igniting() const
+{
+    const scalar curTime = mesh().time().value();
+    const scalar deltaT = mesh().time().deltaTValue();
+
+    const bool igniting
+    (
+        repeatTime(curTime) > -0.5*deltaT
+     && repeatTime(curTime) < max(duration_.value(), 0.5*deltaT)
+    );
+
+    if (igniting)
+    {
+        ignited_ = true;
+        reset_ = false;
+    }
+
+    return igniting;
+}
+
+
+bool Foam::fv::multiCycleConstantbXiIgnition::ignited() const
+{
+    const scalar curTime = mesh().time().value();
+    const scalar deltaT = mesh().time().deltaTValue();
+
+    igniting();
+
+    if
+    (
+        ignited_
+     && !reset_
+     && repeatTime(curTime) > combustionDuration_.value() - 0.5*deltaT
+    )
+    {
+        ignited_ = false;
+        reset_ = true;
+
+        psiuMulticomponentThermo& thermo
+        (
+            mesh().lookupObjectRef<psiuMulticomponentThermo>
+            (
+                physicalProperties::typeName
+            )
+        );
+
+        thermo.reset();
+    }
+
+    return ignited_;
+}
+
+
+bool Foam::fv::multiCycleConstantbXiIgnition::read(const dictionary& dict)
+{
+    if (constantbXiIgnition::read(dict))
+    {
+        readCoeffs(coeffs(dict));
+        return true;
+    }
+    else
+    {
+        return false;
+    }
+
+    return false;
+}
+
+
+// ************************************************************************* //
