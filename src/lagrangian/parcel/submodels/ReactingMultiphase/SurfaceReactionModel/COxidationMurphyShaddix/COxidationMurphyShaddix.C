436361e6bceb4b832d78038ca2c959b8e8ef29e9
Will Bainbridge <http://cfd.direct>
2024-10-29 10:06:03 +0000

    reactingMultiphaseCloud: Removed hard-coded phase-ordering
    Simulations should now be insensitive to the order in which phases are
    specified within the singleMixtureFraction model coefficients.

--- src/lagrangian/parcel/submodels/ReactingMultiphase/SurfaceReactionModel/COxidationMurphyShaddix/COxidationMurphyShaddix.C
+++ src/lagrangian/parcel/submodels/ReactingMultiphase/SurfaceReactionModel/COxidationMurphyShaddix/COxidationMurphyShaddix.C
@@ -2,7 +2,7 @@
   =========                 |
   \\      /  F ield         | OpenFOAM: The Open Source CFD Toolbox
    \\    /   O peration     | Website:  https://openfoam.org
-    \\  /    A nd           | Copyright (C) 2011-2023 OpenFOAM Foundation
+    \\  /    A nd           | Copyright (C) 2011-2024 OpenFOAM Foundation
      \\/     M anipulation  |
 -------------------------------------------------------------------------------
 License
@@ -133,7 +133,7 @@ Foam::scalar Foam::COxidationMurphyShaddix<CloudType>::calculate
 ) const
 {
     // Fraction of remaining combustible material
-    const label idSolid = CloudType::parcelType::SLD;
+    const label idSolid = this->owner().composition().idSolid();
     const scalar fComb = YMixture[idSolid]*YSolid[CsLocalId_];
 
     // Surface combustion until combustible fraction is consumed
