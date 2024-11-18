9ee3025cb3ad91728dfef02ca38cd8d12e3a684a
Henry Weller <http://cfd.direct>
2024-11-18 14:44:41 +0000

    multicomponentThermo: Read Ydefault from beginTime rather than specifically time 0

--- src/thermophysicalModels/multicomponentThermo/multicomponentThermo/multicomponentThermo.C
+++ src/thermophysicalModels/multicomponentThermo/multicomponentThermo/multicomponentThermo.C
@@ -2,7 +2,7 @@
   =========                 |
   \\      /  F ield         | OpenFOAM: The Open Source CFD Toolbox
    \\    /   O peration     | Website:  https://openfoam.org
-    \\  /    A nd           | Copyright (C) 2023 OpenFOAM Foundation
+    \\  /    A nd           | Copyright (C) 2023-2024 OpenFOAM Foundation
      \\/     M anipulation  |
 -------------------------------------------------------------------------------
 License
@@ -163,7 +163,7 @@ Foam::multicomponentThermo::implementation::implementation
                 typeIOobject<volScalarField> time0IO
                 (
                     YdefaultName,
-                    Time::timeName(0),
+                    mesh.time().beginTime().name(),
                     mesh,
                     IOobject::MUST_READ,
                     IOobject::NO_WRITE
