f60f97e030835c240667a726086205a32cb74473
Henry Weller <http://cfd.direct>
2025-02-24 17:50:33 +0000

    Time::findTimes: Call fileOperation::findTimes
    Avoid using the specific fileHandler::findTimes to search a given directory for
    valid time directories, rather than the fileHandler decomposition directory.
    
    Resolves bug-report https://bugs.openfoam.org/view.php?id=4217

--- src/OpenFOAM/db/Time/findTimes.C
+++ src/OpenFOAM/db/Time/findTimes.C
@@ -2,7 +2,7 @@
   =========                 |
   \\      /  F ield         | OpenFOAM: The Open Source CFD Toolbox
    \\    /   O peration     | Website:  https://openfoam.org
-    \\  /    A nd           | Copyright (C) 2011-2018 OpenFOAM Foundation
+    \\  /    A nd           | Copyright (C) 2011-2025 OpenFOAM Foundation
      \\/     M anipulation  |
 -------------------------------------------------------------------------------
 License
@@ -38,7 +38,7 @@ Foam::instantList Foam::Time::findTimes
     const word& constantName
 )
 {
-    return fileHandler().findTimes(directory, constantName);
+    return fileHandler().fileOperation::findTimes(directory, constantName);
 }
 
 
