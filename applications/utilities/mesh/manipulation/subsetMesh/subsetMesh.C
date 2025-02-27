b8a74b0889c39911a2856cbfa9b8a8a90baae9a0
Will Bainbridge <http://cfd.direct>
2025-02-27 09:36:04 +0000

    subsetMesh: Support hexRef8 data and reduce verbosity of field handling

--- applications/utilities/mesh/manipulation/subsetMesh/subsetMesh.C
+++ applications/utilities/mesh/manipulation/subsetMesh/subsetMesh.C
@@ -2,7 +2,7 @@
   =========                 |
   \\      /  F ield         | OpenFOAM: The Open Source CFD Toolbox
    \\    /   O peration     | Website:  https://openfoam.org
-    \\  /    A nd           | Copyright (C) 2011-2024 OpenFOAM Foundation
+    \\  /    A nd           | Copyright (C) 2011-2025 OpenFOAM Foundation
      \\/     M anipulation  |
 -------------------------------------------------------------------------------
 License
@@ -41,147 +41,43 @@ Description
 #include "cellSet.H"
 #include "IOobjectList.H"
 #include "volFields.H"
+#include "polyTopoChangeMap.H"
+#include "hexRef8Data.H"
 
 using namespace Foam;
 
 // * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * //
 
-template<class Type>
-void subsetVolFields
+template<class GeoField>
+void subsetFields
 (
     const fvMeshSubset& subsetter,
+    const typename GeoField::Mesh& mesh,
     const wordList& fieldNames,
-    PtrList<VolField<Type>>& subFields
+    PtrList<GeoField>& subFields
 )
 {
-    const fvMesh& baseMesh = subsetter.baseMesh();
-
     forAll(fieldNames, i)
     {
-        const word& fieldName = fieldNames[i];
+        Info<< "Subsetting " << GeoField::typeName
+            << " " << fieldNames[i] << endl;
 
-        Info<< "Subsetting field " << fieldName << endl;
-
-        VolField<Type> fld
+        GeoField fld
         (
             IOobject
             (
-                fieldName,
-                baseMesh.time().name(),
-                baseMesh,
+                fieldNames[i],
+                subsetter.baseMesh().time().name(),
+                subsetter.baseMesh(),
                 IOobject::MUST_READ,
                 IOobject::NO_WRITE
             ),
-            baseMesh
+            mesh
         );
 
         subFields.set(i, subsetter.interpolate(fld));
     }
 }
-
-
-template<class Type>
-void subsetSurfaceFields
-(
-    const fvMeshSubset& subsetter,
-    const wordList& fieldNames,
-    PtrList<SurfaceField<Type>>& subFields
-)
-{
-    const fvMesh& baseMesh = subsetter.baseMesh();
-
-    forAll(fieldNames, i)
-    {
-        const word& fieldName = fieldNames[i];
-
-        Info<< "Subsetting field " << fieldName << endl;
-
-        SurfaceField<Type> fld
-        (
-            IOobject
-            (
-                fieldName,
-                baseMesh.time().name(),
-                baseMesh,
-                IOobject::MUST_READ,
-                IOobject::NO_WRITE
-            ),
-            baseMesh
-        );
-
-        subFields.set(i, subsetter.interpolate(fld));
-    }
-}
-
-
-template<class Type>
-void subsetPointFields
-(
-    const fvMeshSubset& subsetter,
-    const pointMesh& pMesh,
-    const wordList& fieldNames,
-    PtrList<PointField<Type>>& subFields
-)
-{
-    const fvMesh& baseMesh = subsetter.baseMesh();
-
-    forAll(fieldNames, i)
-    {
-        const word& fieldName = fieldNames[i];
-
-        Info<< "Subsetting field " << fieldName << endl;
-
-        PointField<Type> fld
-        (
-            IOobject
-            (
-                fieldName,
-                baseMesh.time().name(),
-                baseMesh,
-                IOobject::MUST_READ,
-                IOobject::NO_WRITE
-            ),
-            pMesh
-        );
-
-        subFields.set(i, subsetter.interpolate(fld));
-    }
-}
-
-
-template<class Type>
-void subsetDimensionedFields
-(
-    const fvMeshSubset& subsetter,
-    const wordList& fieldNames,
-    PtrList<DimensionedField<Type, volMesh>>& subFields
-)
-{
-    const fvMesh& baseMesh = subsetter.baseMesh();
-
-    forAll(fieldNames, i)
-    {
-        const word& fieldName = fieldNames[i];
-
-        Info<< "Subsetting field " << fieldName << endl;
-
-        DimensionedField<Type, volMesh> fld
-        (
-            IOobject
-            (
-                fieldName,
-                baseMesh.time().name(),
-                baseMesh,
-                IOobject::MUST_READ,
-                IOobject::NO_WRITE
-            ),
-            baseMesh
-        );
-
-        subFields.set(i, subsetter.interpolate(fld));
-    }
-}
-
 
 
 int main(int argc, char *argv[])
@@ -273,183 +169,55 @@ int main(int argc, char *argv[])
     }
 
 
+    // Read hexRef8 data, if any
+    hexRef8Data refData
+    (
+        IOobject
+        (
+            "dummy",
+            mesh.facesInstance(),
+            polyMesh::meshSubDir,
+            mesh,
+            IOobject::READ_IF_PRESENT,
+            IOobject::NO_WRITE,
+            false
+        )
+    );
+
+
     cellSet currentSet(mesh, setName);
     subsetter.setLargeCellSubset(currentSet, patchi, true);
 
 
     if (fields)
     {
-        IOobjectList objects(mesh, runTime.name());
-
-        // Read vol fields and subset
-        // ~~~~~~~~~~~~~~~~~~~~~~~~~~
-
-        wordList scalarNames(objects.names(volScalarField::typeName));
-        PtrList<volScalarField> scalarFlds(scalarNames.size());
-        subsetVolFields(subsetter, scalarNames, scalarFlds);
-
-        wordList vectorNames(objects.names(volVectorField::typeName));
-        PtrList<volVectorField> vectorFlds(vectorNames.size());
-        subsetVolFields(subsetter, vectorNames, vectorFlds);
-
-        wordList sphericalTensorNames
-        (
-            objects.names(volSphericalTensorField::typeName)
-        );
-        PtrList<volSphericalTensorField> sphericalTensorFlds
-        (
-            sphericalTensorNames.size()
-        );
-        subsetVolFields(subsetter, sphericalTensorNames, sphericalTensorFlds);
-
-        wordList symmTensorNames(objects.names(volSymmTensorField::typeName));
-        PtrList<volSymmTensorField> symmTensorFlds(symmTensorNames.size());
-        subsetVolFields(subsetter, symmTensorNames, symmTensorFlds);
-
-        wordList tensorNames(objects.names(volTensorField::typeName));
-        PtrList<volTensorField> tensorFlds(tensorNames.size());
-        subsetVolFields(subsetter, tensorNames, tensorFlds);
-
-
-        // Read surface fields and subset
-        // ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-
-        wordList surfScalarNames(objects.names(surfaceScalarField::typeName));
-        PtrList<surfaceScalarField> surfScalarFlds(surfScalarNames.size());
-        subsetSurfaceFields(subsetter, surfScalarNames, surfScalarFlds);
-
-        wordList surfVectorNames(objects.names(surfaceVectorField::typeName));
-        PtrList<surfaceVectorField> surfVectorFlds(surfVectorNames.size());
-        subsetSurfaceFields(subsetter, surfVectorNames, surfVectorFlds);
-
-        wordList surfSphericalTensorNames
-        (
-            objects.names(surfaceSphericalTensorField::typeName)
-        );
-        PtrList<surfaceSphericalTensorField> surfSphericalTensorFlds
-        (
-            surfSphericalTensorNames.size()
-        );
-        subsetSurfaceFields
-        (
-            subsetter,
-            surfSphericalTensorNames,
-            surfSphericalTensorFlds
-        );
-
-        wordList surfSymmTensorNames
-        (
-            objects.names(surfaceSymmTensorField::typeName)
-        );
-        PtrList<surfaceSymmTensorField> surfSymmTensorFlds
-        (
-            surfSymmTensorNames.size()
-        );
-        subsetSurfaceFields(subsetter, surfSymmTensorNames, surfSymmTensorFlds);
-
-        wordList surfTensorNames(objects.names(surfaceTensorField::typeName));
-        PtrList<surfaceTensorField> surfTensorFlds(surfTensorNames.size());
-        subsetSurfaceFields(subsetter, surfTensorNames, surfTensorFlds);
-
-
-        // Read point fields and subset
-        // ~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-
         const pointMesh& pMesh = pointMesh::New(mesh);
 
-        wordList pointScalarNames(objects.names(pointScalarField::typeName));
-        PtrList<pointScalarField> pointScalarFlds(pointScalarNames.size());
-        subsetPointFields(subsetter, pMesh, pointScalarNames, pointScalarFlds);
+        IOobjectList objects(mesh, runTime.name());
 
-        wordList pointVectorNames(objects.names(pointVectorField::typeName));
-        PtrList<pointVectorField> pointVectorFlds(pointVectorNames.size());
-        subsetPointFields(subsetter, pMesh, pointVectorNames, pointVectorFlds);
+        // Subset all the fields
+        #define SubsetTypeGeoFields(Type, GeoField, mesh)                      \
+            wordList Type##GeoField##Names                                     \
+            (                                                                  \
+                objects.names(GeoField<Type>::typeName)                        \
+            );                                                                 \
+            PtrList<GeoField<Type>> Type##GeoField##s                          \
+            (                                                                  \
+                Type##GeoField##Names.size()                                   \
+            );                                                                 \
+            subsetFields                                                       \
+            (                                                                  \
+                subsetter,                                                     \
+                mesh,                                                          \
+                Type##GeoField##Names,                                         \
+                Type##GeoField##s                                              \
+            );
+        FOR_ALL_FIELD_TYPES(SubsetTypeGeoFields, VolField, mesh);
+        FOR_ALL_FIELD_TYPES(SubsetTypeGeoFields, VolInternalField, mesh);
+        FOR_ALL_FIELD_TYPES(SubsetTypeGeoFields, SurfaceField, mesh);
+        FOR_ALL_FIELD_TYPES(SubsetTypeGeoFields, PointField, pMesh);
 
-        wordList pointSphericalTensorNames
-        (
-            objects.names(pointSphericalTensorField::typeName)
-        );
-        PtrList<pointSphericalTensorField> pointSphericalTensorFlds
-        (
-            pointSphericalTensorNames.size()
-        );
-        subsetPointFields
-        (
-            subsetter,
-            pMesh,
-            pointSphericalTensorNames,
-            pointSphericalTensorFlds
-        );
-
-        wordList pointSymmTensorNames
-        (
-            objects.names(pointSymmTensorField::typeName)
-        );
-        PtrList<pointSymmTensorField> pointSymmTensorFlds
-        (
-            pointSymmTensorNames.size()
-        );
-        subsetPointFields
-        (
-            subsetter,
-            pMesh,
-            pointSymmTensorNames,
-            pointSymmTensorFlds
-        );
-
-        wordList pointTensorNames(objects.names(pointTensorField::typeName));
-        PtrList<pointTensorField> pointTensorFlds(pointTensorNames.size());
-        subsetPointFields(subsetter, pMesh, pointTensorNames, pointTensorFlds);
-
-
-        // Read dimensioned fields and subset
-        // ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-
-        typedef volScalarField::Internal dimScalType;
-        wordList scalarDimNames(objects.names(dimScalType::typeName));
-        PtrList<dimScalType> scalarDimFlds(scalarDimNames.size());
-        subsetDimensionedFields(subsetter, scalarDimNames, scalarDimFlds);
-
-        typedef volVectorField::Internal dimVecType;
-        wordList vectorDimNames(objects.names(dimVecType::typeName));
-        PtrList<dimVecType> vectorDimFlds(vectorDimNames.size());
-        subsetDimensionedFields(subsetter, vectorDimNames, vectorDimFlds);
-
-        typedef volSphericalTensorField::Internal dimSphereType;
-        wordList sphericalTensorDimNames
-        (
-            objects.names(dimSphereType::typeName)
-        );
-        PtrList<dimSphereType> sphericalTensorDimFlds
-        (
-            sphericalTensorDimNames.size()
-        );
-        subsetDimensionedFields
-        (
-            subsetter,
-            sphericalTensorDimNames,
-            sphericalTensorDimFlds
-        );
-
-        typedef volSymmTensorField::Internal dimSymmTensorType;
-        wordList symmTensorDimNames(objects.names(dimSymmTensorType::typeName));
-        PtrList<dimSymmTensorType> symmTensorDimFlds(symmTensorDimNames.size());
-        subsetDimensionedFields
-        (
-            subsetter,
-            symmTensorDimNames,
-            symmTensorDimFlds
-        );
-
-        typedef volTensorField::Internal dimTensorType;
-        wordList tensorDimNames(objects.names(dimTensorType::typeName));
-        PtrList<dimTensorType> tensorDimFlds(tensorDimNames.size());
-        subsetDimensionedFields(subsetter, tensorDimNames, tensorDimFlds);
-
-
-        // Write mesh and fields to new time
-        // ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-
+        // Set the instance
         if (overwrite || specifiedInstance)
         {
             runTime.setTime(instant(fieldsInstance), 0);
@@ -465,119 +233,21 @@ int main(int argc, char *argv[])
 
         subsetter.subMesh().write();
 
-        // Subsetting adds 'subset' prefix. Rename field to be like original.
-        forAll(scalarFlds, i)
-        {
-            scalarFlds[i].rename(scalarNames[i]);
-            scalarFlds[i].write();
-        }
-        forAll(vectorFlds, i)
-        {
-            vectorFlds[i].rename(vectorNames[i]);
-            vectorFlds[i].write();
-        }
-        forAll(sphericalTensorFlds, i)
-        {
-            sphericalTensorFlds[i].rename(sphericalTensorNames[i]);
-            sphericalTensorFlds[i].write();
-        }
-        forAll(symmTensorFlds, i)
-        {
-            symmTensorFlds[i].rename(symmTensorNames[i]);
-            symmTensorFlds[i].write();
-        }
-        forAll(tensorFlds, i)
-        {
-            tensorFlds[i].rename(tensorNames[i]);
-            tensorFlds[i].write();
-        }
-
-        // Surface ones.
-        forAll(surfScalarFlds, i)
-        {
-            surfScalarFlds[i].rename(surfScalarNames[i]);
-            surfScalarFlds[i].write();
-        }
-        forAll(surfVectorFlds, i)
-        {
-            surfVectorFlds[i].rename(surfVectorNames[i]);
-            surfVectorFlds[i].write();
-        }
-        forAll(surfSphericalTensorFlds, i)
-        {
-            surfSphericalTensorFlds[i].rename(surfSphericalTensorNames[i]);
-            surfSphericalTensorFlds[i].write();
-        }
-        forAll(surfSymmTensorFlds, i)
-        {
-            surfSymmTensorFlds[i].rename(surfSymmTensorNames[i]);
-            surfSymmTensorFlds[i].write();
-        }
-        forAll(surfTensorNames, i)
-        {
-            surfTensorFlds[i].rename(surfTensorNames[i]);
-            surfTensorFlds[i].write();
-        }
-
-        // Point ones
-        forAll(pointScalarFlds, i)
-        {
-            pointScalarFlds[i].rename(pointScalarNames[i]);
-            pointScalarFlds[i].write();
-        }
-        forAll(pointVectorFlds, i)
-        {
-            pointVectorFlds[i].rename(pointVectorNames[i]);
-            pointVectorFlds[i].write();
-        }
-        forAll(pointSphericalTensorFlds, i)
-        {
-            pointSphericalTensorFlds[i].rename(pointSphericalTensorNames[i]);
-            pointSphericalTensorFlds[i].write();
-        }
-        forAll(pointSymmTensorFlds, i)
-        {
-            pointSymmTensorFlds[i].rename(pointSymmTensorNames[i]);
-            pointSymmTensorFlds[i].write();
-        }
-        forAll(pointTensorNames, i)
-        {
-            pointTensorFlds[i].rename(pointTensorNames[i]);
-            pointTensorFlds[i].write();
-        }
-
-        // DimensionedFields
-        forAll(scalarDimFlds, i)
-        {
-            scalarDimFlds[i].rename(scalarDimNames[i]);
-            scalarDimFlds[i].write();
-        }
-        forAll(vectorDimFlds, i)
-        {
-            vectorDimFlds[i].rename(vectorDimNames[i]);
-            vectorDimFlds[i].write();
-        }
-        forAll(sphericalTensorDimFlds, i)
-        {
-            sphericalTensorDimFlds[i].rename(sphericalTensorDimNames[i]);
-            sphericalTensorDimFlds[i].write();
-        }
-        forAll(symmTensorDimFlds, i)
-        {
-            symmTensorDimFlds[i].rename(symmTensorDimNames[i]);
-            symmTensorDimFlds[i].write();
-        }
-        forAll(tensorDimFlds, i)
-        {
-            tensorDimFlds[i].rename(tensorDimNames[i]);
-            tensorDimFlds[i].write();
-        }
+        // Subsetting adds a 'subset' prefix. Rename the fields to remove this.
+        #define RenameTypeGeoFields(Type, GeoField)                            \
+            forAll(Type##GeoField##s, i)                                       \
+            {                                                                  \
+                Type##GeoField##s[i].rename(Type##GeoField##Names[i]);         \
+                Type##GeoField##s[i].write();                                  \
+            }
+        FOR_ALL_FIELD_TYPES(RenameTypeGeoFields, VolField);
+        FOR_ALL_FIELD_TYPES(RenameTypeGeoFields, VolInternalField);
+        FOR_ALL_FIELD_TYPES(RenameTypeGeoFields, SurfaceField);
+        FOR_ALL_FIELD_TYPES(RenameTypeGeoFields, PointField);
     }
     else
     {
-        // Write mesh to new time
-        // ~~~~~~~~~~~~~~~~~~~~~~
-
+        // Set the instance
         if (overwrite || specifiedInstance)
         {
             runTime.setTime(instant(fieldsInstance), 0);
@@ -593,6 +263,34 @@ int main(int argc, char *argv[])
 
         subsetter.subMesh().write();
     }
+
+
+    // Map the hexRef8 data
+    polyTopoChangeMap map
+    (
+        mesh,
+        mesh.nPoints(),                 // nOldPoints,
+        mesh.nFaces(),                  // nOldFaces,
+        mesh.nCells(),                  // nOldCells,
+        labelList(subsetter.pointMap()),// pointMap,
+        List<objectMap>(0),             // pointsFromPoints,
+        labelList(0),                   // faceMap,
+        List<objectMap>(0),             // facesFromFaces,
+        labelList(subsetter.cellMap()), // cellMap,
+        List<objectMap>(0),             // cellsFromCells,
+        labelList(0),                   // reversePointMap,
+        labelList(0),                   // reverseFaceMap,
+        labelList(0),                   // reverseCellMap,
+        labelHashSet(0),                // flipFaceFlux,
+        labelListList(0),               // patchPointMap,
+        labelList(0),                   // oldPatchSizes,
+        labelList(0),                   // oldPatchStarts,
+        labelList(0),                   // oldPatchNMeshPoints,
+        autoPtr<scalarField>()          // oldCellVolumesPtr
+    );
+    refData.topoChange(map);
+    refData.write();
+
 
     Info<< "End\n" << endl;
 
