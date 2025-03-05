753040f59e808693c4901fb9fefaa63663b3e982
Henry Weller <http://cfd.direct>
2025-03-05 16:57:33 +0000

    symmTensor: Added symmetric specific versions of eigenValues and eigenVectors

--- src/OpenFOAM/primitives/Tensor/tensor/tensor.C
+++ src/OpenFOAM/primitives/Tensor/tensor/tensor.C
@@ -2,7 +2,7 @@
   =========                 |
   \\      /  F ield         | OpenFOAM: The Open Source CFD Toolbox
    \\    /   O peration     | Website:  https://openfoam.org
-    \\  /    A nd           | Copyright (C) 2011-2023 OpenFOAM Foundation
+    \\  /    A nd           | Copyright (C) 2011-2025 OpenFOAM Foundation
      \\/     M anipulation  |
 -------------------------------------------------------------------------------
 License
@@ -136,16 +136,78 @@ Foam::vector Foam::eigenValues(const tensor& t)
 }
 
 
+Foam::vector Foam::eigenValues(const symmTensor& t)
+{
+    // Coefficients of the characteristic cubic polynomial (a = 1)
+    const scalar b =
+      - t.xx() - t.yy() - t.zz();
+    const scalar c =
+        t.xx()*t.yy() + t.xx()*t.zz() + t.yy()*t.zz()
+      - t.xy()*t.yx() - t.yz()*t.zy() - t.zx()*t.xz();
+    const scalar d =
+      - t.xx()*t.yy()*t.zz()
+      - t.xy()*t.yz()*t.zx() - t.xz()*t.zy()*t.yx()
+      + t.xx()*t.yz()*t.zy() + t.yy()*t.zx()*t.xz() + t.zz()*t.xy()*t.yx();
+
+    // Solve
+    Roots<3> roots = cubicEqn(1, b, c, d).roots(true);
+
+    // Check the root types
+    vector lambda = vector::zero;
+    forAll(roots, i)
+    {
+        switch (roots.type(i))
+        {
+            case rootType::real:
+                lambda[i] = roots[i];
+                break;
+            case rootType::complex:
+                FatalErrorInFunction
+                    << "Complex eigenvalues detected for symmTensor: " << t
+                    << exit(FatalError);
+                break;
+            case rootType::posInf:
+                lambda[i] = vGreat;
+                break;
+            case rootType::negInf:
+                lambda[i] = - vGreat;
+                break;
+            case rootType::nan:
+                FatalErrorInFunction
+                    << "Eigenvalue calculation failed for symmTensor: " << t
+                    << exit(FatalError);
+        }
+    }
+
+    // Sort the eigenvalues into ascending order
+    if (lambda.x() > lambda.y())
+    {
+        Swap(lambda.x(), lambda.y());
+    }
+    if (lambda.y() > lambda.z())
+    {
+        Swap(lambda.y(), lambda.z());
+    }
+    if (lambda.x() > lambda.y())
+    {
+        Swap(lambda.x(), lambda.y());
+    }
+
+    return lambda;
+}
+
+
+template<class TensorType>
 Foam::vector Foam::eigenVector
 (
-    const tensor& T,
+    const TensorType& T,
     const scalar lambda,
     const vector& direction1,
     const vector& direction2
 )
 {
     // Construct the linear system for this eigenvalue
-    tensor A(T - lambda*I);
+    TensorType A(T - lambda*I);
 
     // Determinants of the 2x2 sub-matrices used to find the eigenvectors
     scalar sd0, sd1, sd2;
@@ -254,41 +316,29 @@ Foam::tensor Foam::eigenVectors(const tensor& T, const vector& lambdas)
 }
 
 
+Foam::tensor Foam::eigenVectors(const symmTensor& T, const vector& lambdas)
+{
+    vector Ux(1, 0, 0), Uy(0, 1, 0), Uz(0, 0, 1);
+
+    Ux = eigenVector(T, lambdas.x(), Uy, Uz);
+    Uy = eigenVector(T, lambdas.y(), Uz, Ux);
+    Uz = eigenVector(T, lambdas.z(), Ux, Uy);
+
+    return tensor(Ux, Uy, Uz);
+}
+
+
 Foam::tensor Foam::eigenVectors(const tensor& T)
 {
     const vector lambdas(eigenValues(T));
-
     return eigenVectors(T, lambdas);
-}
-
-
-Foam::vector Foam::eigenValues(const symmTensor& T)
-{
-    return eigenValues(tensor(T));
-}
-
-
-Foam::vector Foam::eigenVector
-(
-    const symmTensor& T,
-    const scalar lambda,
-    const vector& direction1,
-    const vector& direction2
-)
-{
-    return eigenVector(tensor(T), lambda, direction1, direction2);
-}
-
-
-Foam::tensor Foam::eigenVectors(const symmTensor& T, const vector& lambdas)
-{
-    return eigenVectors(tensor(T), lambdas);
 }
 
 
 Foam::tensor Foam::eigenVectors(const symmTensor& T)
 {
-    return eigenVectors(tensor(T));
+    const vector lambdas(eigenValues(T));
+    return eigenVectors(T, lambdas);
 }
 
 
