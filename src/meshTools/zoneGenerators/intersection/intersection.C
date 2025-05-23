9a0fb6a3ac86345280d1a6c43ef38abcf0d5ea64
Henry Weller <http://cfd.direct>
2025-05-23 16:28:54 +0100

    zoneGenerators::intersection: New zoneGenerator which selects the elements which are in all the zones generated by the given list of zoneGenerators
    Class
        Foam::zoneGenerators::intersection
    
    Description
        A zoneGenerator which selects the elements which are in all the zones
        generated by the given list of zoneGenerators
    
    Usage
        \table
            Property     | Description             | Required | Default value
            type         | Type: intersection             | yes      |
            name         | Name of the zone        | no       | zoneGenerator name
            zoneType     | Type of zone            | no       | all the zone types
            moveUpdate   | Switch to update after mesh motion | no | false
        \endtable
        These options are followed by a list of any number of zoneGenerators
        which can either generate the zones locally or return zones from the lists
        of zones held by the mesh using the zoneGenerators::lookup zoneGenerator.
    
        Each zoneGenerator returns a zoneSet containing a pointZone and/or a
        cellZone and/or a faceZone, the zoneGenerators::intersection operation
        combines each of the zone types unless the \c zoneType is specified in which
        case only the zones of that type are combined.
    
        A cellZone named \c coneAnnulus containing the cells with centres inside the
        intersection of the predefined \c cone1 cellZone which is looked-up from the
        cellZoneList and are also inside the locally defined annulus is be
        generated by
        \verbatim
            coneAnnulus
            {
                type        intersection;
    
                cone1;
    
                annulus1
                {
                    type        annulus;
                    zoneType    cell;
    
                    point1      (-0.0075 0 -1);
                    point2      (-0.003 0.0025 1);
                    outerRadius 0.002;
                    innerRadius 0.001;
                }
            }
        \endverbatim
    
    See also
        zoneGenerators::Union
        zoneGenerators::difference
        zoneGeneratorList

---
+++ src/meshTools/zoneGenerators/intersection/intersection.C
@@ -0,0 +1,205 @@
+/*---------------------------------------------------------------------------*\
+  =========                 |
+  \\      /  F ield         | OpenFOAM: The Open Source CFD Toolbox
+   \\    /   O peration     | Website:  https://openfoam.org
+    \\  /    A nd           | Copyright (C) 2025 OpenFOAM Foundation
+     \\/     M anipulation  |
+-------------------------------------------------------------------------------
+License
+    This file is part of OpenFOAM.
+
+    OpenFOAM is free software: you can redistribute it and/or modify it
+    under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    OpenFOAM is distributed in the hope that it will be useful, but WITHOUT
+    ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+    FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License
+    for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with OpenFOAM.  If not, see <http://www.gnu.org/licenses/>.
+
+\*---------------------------------------------------------------------------*/
+
+#include "intersection.H"
+#include "polyMesh.H"
+#include "addToRunTimeSelectionTable.H"
+
+// * * * * * * * * * * * * * * Static Data Members * * * * * * * * * * * * * //
+
+namespace Foam
+{
+    namespace zoneGenerators
+    {
+        defineTypeNameAndDebug(intersection, 0);
+        addToRunTimeSelectionTable(zoneGenerator, intersection, dictionary);
+    }
+}
+
+
+// * * * * * * * * * * * * * Private Member Functions  * * * * * * * * * * * //
+
+void Foam::zoneGenerators::intersection::countIntersections
+(
+    labelList& nIntersections,
+    const labelList& indices
+) const
+{
+    forAll(indices, i)
+    {
+        nIntersections[indices[i]]++;
+    }
+}
+
+
+void Foam::zoneGenerators::intersection::countIntersections
+(
+    labelList& nIntersections,
+    boolList& flipMap,
+    const labelList& indices,
+    const boolList& zoneFlipMap
+) const
+{
+    forAll(indices, i)
+    {
+        nIntersections[indices[i]]++;
+        flipMap[indices[i]] = zoneFlipMap[i];
+    }
+}
+
+
+Foam::labelList Foam::zoneGenerators::intersection::indices
+(
+    const labelList& nIntersections
+) const
+{
+    const label nZones = zoneGenerators_.size();
+    label nSelected = 0;
+    forAll(nIntersections, i)
+    {
+        if (nIntersections[i] == nZones)
+        {
+            nSelected++;
+        }
+    }
+
+    labelList intersectionIndices(nSelected);
+
+    label ui = 0;
+    forAll(nIntersections, i)
+    {
+        if (nIntersections[i] == nZones)
+        {
+            intersectionIndices[ui++] = i;
+        }
+    }
+
+    return intersectionIndices;
+}
+
+
+// * * * * * * * * * * * * * * * * Constructors  * * * * * * * * * * * * * * //
+
+Foam::zoneGenerators::intersection::intersection
+(
+    const word& name,
+    const polyMesh& mesh,
+    const dictionary& dict
+)
+:
+    zoneGenerator(name, mesh, dict),
+    zoneGenerators_(mesh, dict)
+{
+    moveUpdate_ = zoneGenerators_.moveUpdate();
+}
+
+
+// * * * * * * * * * * * * * * * * Destructor  * * * * * * * * * * * * * * * //
+
+Foam::zoneGenerators::intersection::~intersection()
+{}
+
+
+// * * * * * * * * * * * * * * * Member Functions  * * * * * * * * * * * * * //
+
+Foam::zoneSet Foam::zoneGenerators::intersection::generate() const
+{
+    labelList nPointIntersections;
+    labelList nCellIntersections;
+    labelList nFaceIntersections;
+    boolList flipMap;
+
+    forAll(zoneGenerators_, i)
+    {
+        zoneSet zs(zoneGenerators_[i].generate());
+
+        if (zs.pZone.valid())
+        {
+            nPointIntersections.setSize(mesh_.nPoints(), 0);
+            countIntersections(nPointIntersections, zs.pZone);
+        }
+
+        if (zs.cZone.valid())
+        {
+            nCellIntersections.setSize(mesh_.nCells(), 0);
+            countIntersections(nCellIntersections, zs.cZone);
+        }
+
+        if (zs.fZone.valid())
+        {
+            nFaceIntersections.setSize(mesh_.nFaces(), 0);
+            flipMap.setSize(mesh_.nFaces(), false);
+            countIntersections
+            (
+                nFaceIntersections,
+                flipMap,
+                zs.fZone(),
+                zs.fZone().flipMap()
+            );
+        }
+    }
+
+    moveUpdate_ = zoneGenerators_.moveUpdate();
+
+    labelList faceIndices(indices(nFaceIntersections));
+
+    return zoneSet
+    (
+        nPointIntersections.size()
+      ? new pointZone
+        (
+            zoneName_,
+            indices(nPointIntersections),
+            mesh_.pointZones(),
+            moveUpdate_,
+            true
+        )
+      : nullptr,
+        nCellIntersections.size()
+      ? new cellZone
+        (
+            zoneName_,
+            indices(nCellIntersections),
+            mesh_.cellZones(),
+            moveUpdate_,
+            true
+        )
+      : nullptr,
+        nFaceIntersections.size()
+      ? new faceZone
+        (
+            zoneName_,
+            faceIndices,
+            boolList(flipMap, faceIndices),
+            mesh_.faceZones(),
+            moveUpdate_,
+            true
+        )
+      : nullptr
+    );
+}
+
+
+// ************************************************************************* //
