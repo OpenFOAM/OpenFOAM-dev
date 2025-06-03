a653936e85e98fc5d151eb0b1c82d443f4eb2737
Henry Weller <http://cfd.direct>
2025-06-03 15:17:35 +0100

    zoneGenerators::faceZoneGenerator: New zoneGenerator to create a faceZone from a list of zones
    The faceZoneGenerator zoneGenerator creates a faceZone from the faces selected
    from cellZones and/or faceZones generated or looked-up by a list of
    zoneGenerators.  Currently conversion from pointZones is not supported but this
    functionality will be added.
    
    Description
        A zoneGenerator which converts the point, cell and face zones
        from a list of zoneGenerators into a faceZone
    
        If a faceZone with the same name as the faceZone to be generated is
        returned by a zoneGenerator in the list it is ignored.
    
    Usage
        \table
            Property     | Description             | Required | Default value
            type         | Type: face              | yes      |
            name         | Name of the zone        | no       | zoneGenerator name
            cellFaces    | Cell face selection option | no    | all
            moveUpdate   | Switch to update after mesh motion | no | false
        \endtable
    
        cellFaces face selection controls:
        \table
            Option        | Description
            all           | Select all faces of the cells in the cellZone
            inner         | Select the faces inside the cellZone
            outer         | Select the faces on the outer surface of the cellZone
            outerInternal | Select the internal faces on the outer surface \\
                            of the cellZone
        \endtable
    
        To convert cellZone named \c cone1 generated by
        \verbatim
            cone1
            {
                type        truncatedCone;
                zoneType    cell;
    
                point1      (-0.0075 0 -1);
                point2      (-0.003 0.0025 1);
                radius1     0.001;
                radius2     0.0005;
            }
        \endverbatim
        into a faceZone also named \c cone1, the following zoneGenerator named \c
        cone1Faces can be used
        \verbatim
            cone1Faces
            {
                type        face;
                name        cone1;
    
                cone1;
            }
        \endverbatim
        To create a faceZone which includes only the internal mesh faces on the
        outer surface of a pre-generated cellZone named \c leftFluid the \c
        outerInternal \c cellFaces face selection option can be used:
        \verbatim
            cycLeft
            {
                type        face;
                cellFaces   outerInternal;
                leftFluid;
            }
        \endverbatim
    
    The incompressibleFluid/ballValve, multiRegion/CHT/heatExchanger and
    multicomponentFluid/filter tutorial cases have been updated to utilise this new
    functionality which replaces the deprecated cellToFace topoSetSource.

---
+++ src/meshTools/zoneGenerators/conversion/face/faceZoneGenerator.C
@@ -0,0 +1,336 @@
+/*---------------------------------------------------------------------------*\
+  =========                 |
+  \\      /  F ield         | OpenFOAM: The Open Source CFD Toolbox
+   \\    /   O peration     | Website:  https://openfoam.org
+    \\  /    A nd           | Copyright (C) 2025 OpenFOAM Foundation
+     \\/     M anipulation  |
+-------------------------------------------------------------------------------
+License
+    This file is part of OpenFOAM.
+
+    OpenFOAM is free software: you can redistribute it and/or modify it
+    under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    OpenFOAM is distributed in the hope that it will be useful, but WITHOUT
+    ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+    FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License
+    for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with OpenFOAM.  If not, see <http://www.gnu.org/licenses/>.
+
+\*---------------------------------------------------------------------------*/
+
+#include "faceZoneGenerator.H"
+#include "polyMesh.H"
+#include "syncTools.H"
+#include "addToRunTimeSelectionTable.H"
+
+// * * * * * * * * * * * * * * Static Data Members * * * * * * * * * * * * * //
+
+namespace Foam
+{
+    namespace zoneGenerators
+    {
+        defineTypeNameAndDebug(faceZoneGenerator, 0);
+        addToRunTimeSelectionTable
+        (
+            zoneGenerator,
+            faceZoneGenerator,
+            dictionary
+        );
+    }
+}
+
+template<>
+const char* Foam::NamedEnum
+<
+    Foam::zoneGenerators::faceZoneGenerator::cellFaces,
+    4
+>::names[] =
+{
+    "all",
+    "inner",
+    "outer",
+    "outerInternal"
+};
+
+const Foam::NamedEnum
+<
+    Foam::zoneGenerators::faceZoneGenerator::cellFaces,
+    4
+> Foam::zoneGenerators::faceZoneGenerator::cellFacesNames;
+
+
+// * * * * * * * * * * * * * * * * Constructors  * * * * * * * * * * * * * * //
+
+Foam::zoneGenerators::faceZoneGenerator::faceZoneGenerator
+(
+    const word& name,
+    const polyMesh& mesh,
+    const dictionary& dict
+)
+:
+    zoneGenerator(name, mesh, dict),
+    zoneGenerators_(mesh, dict),
+    cellFaces_
+    (
+        cellFacesNames.lookupOrDefault
+        (
+            "cellFaces",
+            dict,
+            cellFaces::all
+        )
+    )
+{}
+
+
+// * * * * * * * * * * * * * * * * Destructor  * * * * * * * * * * * * * * * //
+
+Foam::zoneGenerators::faceZoneGenerator::~faceZoneGenerator()
+{}
+
+
+// * * * * * * * * * * * * * * * Member Functions  * * * * * * * * * * * * * //
+
+Foam::zoneSet Foam::zoneGenerators::faceZoneGenerator::generate() const
+{
+    boolList selectedFaces(mesh_.nFaces(), false);
+    boolList flipMap(mesh_.nFaces(), false);
+
+    forAll(zoneGenerators_, i)
+    {
+        zoneSet zs(zoneGenerators_[i].generate());
+
+        if (zs.pZone.valid())
+        {
+            // const labelList& zonePoints = zs.pZone();
+            NotImplemented;
+        }
+
+        if (zs.cZone.valid())
+        {
+            const labelList& zoneCells = zs.cZone();
+
+            switch (cellFaces_)
+            {
+                case cellFaces::all:
+                {
+                    forAll(zoneCells, zci)
+                    {
+                        const labelList& cellFaces =
+                            mesh_.cells()[zoneCells[zci]];
+
+                        forAll(cellFaces, cFacei)
+                        {
+                            selectedFaces[cellFaces[cFacei]] = true;
+                        }
+                    }
+                    break;
+                }
+
+                case cellFaces::inner:
+                {
+                    const labelHashSet cellSet(zoneCells);
+
+                    const label nInt = mesh_.nInternalFaces();
+                    const labelList& own = mesh_.faceOwner();
+                    const labelList& nei = mesh_.faceNeighbour();
+                    const polyBoundaryMesh& patches = mesh_.boundaryMesh();
+
+                    // Check all internal faces
+                    for (label facei = 0; facei < nInt; facei++)
+                    {
+                        if
+                        (
+                            cellSet.found(own[facei])
+                         && cellSet.found(nei[facei])
+                        )
+                        {
+                            selectedFaces[facei] = true;
+                        }
+                    }
+
+
+                    // Get coupled cell status
+                    boolList neiInSet(mesh_.nFaces()-nInt, false);
+
+                    forAll(patches, patchi)
+                    {
+                        const polyPatch& pp = patches[patchi];
+
+                        if (pp.coupled())
+                        {
+                            label facei = pp.start();
+                            forAll(pp, i)
+                            {
+                                neiInSet[facei-nInt] =
+                                    cellSet.found(own[facei]);
+                                facei++;
+                            }
+                        }
+                    }
+                    syncTools::swapBoundaryFaceList(mesh_, neiInSet);
+
+                    // Check all boundary faces
+                    forAll(patches, patchi)
+                    {
+                        const polyPatch& pp = patches[patchi];
+
+                        if (pp.coupled())
+                        {
+                            label facei = pp.start();
+                            forAll(pp, i)
+                            {
+                                if
+                                (
+                                    cellSet.found(own[facei])
+                                 && neiInSet[facei-nInt]
+                                )
+                                {
+                                    selectedFaces[facei] = true;
+                                }
+                                facei++;
+                            }
+                        }
+                    }
+                    break;
+                }
+
+                case cellFaces::outer:
+                case cellFaces::outerInternal:
+                {
+                    const labelHashSet cellSet(zoneCells);
+
+                    const label nInt = mesh_.nInternalFaces();
+                    const labelList& own = mesh_.faceOwner();
+                    const labelList& nei = mesh_.faceNeighbour();
+                    const polyBoundaryMesh& patches = mesh_.boundaryMesh();
+
+                    // Check all internal faces
+                    for (label facei = 0; facei < nInt; facei++)
+                    {
+                        const bool inOwn = cellSet.found(own[facei]);
+                        const bool inNei = cellSet.found(nei[facei]);
+
+                        if (inOwn && !inNei)
+                        {
+                            selectedFaces[facei] = true;
+                            flipMap[facei] = false;
+                        }
+                        else if (!inOwn && inNei)
+                        {
+                            selectedFaces[facei] = true;
+                            flipMap[facei] = true;
+                        }
+                    }
+
+
+                    // Get coupled cell status
+                    boolList neiInSet(mesh_.nFaces()-nInt, false);
+
+                    forAll(patches, patchi)
+                    {
+                        const polyPatch& pp = patches[patchi];
+
+                        if (pp.coupled())
+                        {
+                            label facei = pp.start();
+                            forAll(pp, i)
+                            {
+                                neiInSet[facei-nInt] =
+                                    cellSet.found(own[facei]);
+                                facei++;
+                            }
+                        }
+                        else if
+                        (
+                            cellFaces_ != cellFaces::outerInternal
+                        )
+                        {
+                            label facei = pp.start();
+                            forAll(pp, i)
+                            {
+                                if (cellSet.found(own[facei]))
+                                {
+                                    selectedFaces[facei] = true;
+                                }
+
+                                facei++;
+                            }
+                        }
+                    }
+
+                    syncTools::swapBoundaryFaceList(mesh_, neiInSet);
+
+                    // Check all boundary faces
+                    forAll(patches, patchi)
+                    {
+                        const polyPatch& pp = patches[patchi];
+
+                        if (pp.coupled())
+                        {
+                            label facei = pp.start();
+                            forAll(pp, i)
+                            {
+                                const bool inOwn = cellSet.found(own[facei]);
+                                const bool inNei = neiInSet[facei-nInt];
+
+                                if (inOwn && !inNei)
+                                {
+                                    selectedFaces[facei] = true;
+                                    flipMap[facei] = false;
+                                }
+                                else if (!inOwn && inNei)
+                                {
+                                    selectedFaces[facei] = true;
+                                    flipMap[facei] = true;
+                                }
+
+                                facei++;
+                            }
+                        }
+                    }
+                    break;
+                }
+            }
+        }
+
+        if (zs.fZone.valid() && zs.fZone().name() != zoneName_)
+        {
+            const labelList& zoneFaces = zs.fZone();
+
+            forAll(zoneFaces, zfi)
+            {
+                const face& f = mesh_.faces()[zoneFaces[zfi]];
+
+                forAll(f, fp)
+                {
+                    selectedFaces[f[fp]] = true;
+                }
+            }
+        }
+    }
+
+    moveUpdate_ = zoneGenerators_.moveUpdate();
+
+    const labelList faceIndices(indices(selectedFaces));
+
+    return zoneSet
+    (
+        new faceZone
+        (
+            zoneName_,
+            faceIndices,
+            boolList(flipMap, faceIndices),
+            mesh_.faceZones(),
+            moveUpdate_,
+            true
+        )
+    );
+}
+
+
+// ************************************************************************* //
