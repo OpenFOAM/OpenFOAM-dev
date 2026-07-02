f896da1e2da6a6940a970adea6a2a71bd107ff59
Henry Weller <http://cfd.direct>
2026-07-02 15:13:02 +0100

    DimensionedFieldFunctions::waveVelocity: DimensionedFieldFunctionvectorFvMesh -> DimensionedFieldFunctionVectorFvMesh

--- src/waves/dimensionedFieldFunctions/waveVelocity/waveVelocity_DimensionedFieldFunction.C
+++ src/waves/dimensionedFieldFunctions/waveVelocity/waveVelocity_DimensionedFieldFunction.C
@@ -42,10 +42,10 @@ namespace DimensionedFieldFunctions
     );
 
     typedef DimensionedFieldFunction<DimensionedField<vector, fvMesh>>
-        DimensionedFieldFunctionvectorFvMesh;
+        DimensionedFieldFunctionVectorFvMesh;
     addToRunTimeSelectionTable
     (
-        DimensionedFieldFunctionvectorFvMesh,
+        DimensionedFieldFunctionVectorFvMesh,
         waveVelocity,
         dictionary
     );
