103eb479a77ede36cd623ea4e8ea3e3341a0859f
Will Bainbridge <http://cfd.direct>
2026-07-14 16:26:06 +0100

    LagrangianFieldValue: Removed unnecessary comments from output

--- src/Lagrangian/LagrangianFunctionObjects/LagrangianFieldValue/LagrangianFieldValue.C
+++ src/Lagrangian/LagrangianFunctionObjects/LagrangianFieldValue/LagrangianFieldValue.C
@@ -2,7 +2,7 @@
   =========                 |
   \\      /  F ield         | OpenFOAM: The Open Source CFD Toolbox
    \\    /   O peration     | Website:  https://openfoam.org
-    \\  /    A nd           | Copyright (C) 2025 OpenFOAM Foundation
+    \\  /    A nd           | Copyright (C) 2025-2026 OpenFOAM Foundation
      \\/     M anipulation  |
 -------------------------------------------------------------------------------
 License
@@ -465,7 +465,6 @@ bool Foam::functionObjects::LagrangianFieldValue::writeFieldValue
 
 void Foam::functionObjects::LagrangianFieldValue::writeFileHeader(const label)
 {
-    writeHeader(file(), "Lagrangian Sum");
     writeCommented(file(), "Time");
 
     forAll(fields_, fieldi)
