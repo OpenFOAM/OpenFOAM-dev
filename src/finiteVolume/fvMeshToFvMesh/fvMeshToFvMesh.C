f17cbaa1a58cedea4034374d785d2557432cc9b0
Will Bainbridge <http://cfd.direct>
2026-08-12 12:18:38 +0100

    fvMeshToFvMesh: Fix to writing of target coverage

--- src/finiteVolume/fvMeshToFvMesh/fvMeshToFvMesh.C
+++ src/finiteVolume/fvMeshToFvMesh/fvMeshToFvMesh.C
@@ -2,7 +2,7 @@
   =========                 |
   \\      /  F ield         | OpenFOAM: The Open Source CFD Toolbox
    \\    /   O peration     | Website:  https://openfoam.org
-    \\  /    A nd           | Copyright (C) 2022-2025 OpenFOAM Foundation
+    \\  /    A nd           | Copyright (C) 2022-2026 OpenFOAM Foundation
      \\/     M anipulation  |
 -------------------------------------------------------------------------------
 License
@@ -65,7 +65,7 @@ Foam::fvMeshToFvMesh::fvMeshToFvMesh
                 volScalarField::Internal::New
                 (
                     "0",
-                    srcMesh_,
+                    tgtMesh_,
                     dimensionedScalar(dimless, scalar(0))
                 )()
             )
