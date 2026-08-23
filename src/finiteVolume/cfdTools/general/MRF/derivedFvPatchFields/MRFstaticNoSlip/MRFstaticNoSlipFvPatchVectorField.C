c2f0a065f85c2cd124de0f17659f17469720529a
Henry Weller <http://cfd.direct>
2026-08-23 14:10:44 +0100

    MRFstaticNoSlipFvPatchVectorField: New wall BC for MRF cases
    Description
        Wall-velocity condition to be used on walls that are stationary within the
        moving frame in an MRF (multi-reference frame) or SRF (single reference
        frame) case.
    
        SRF cases are simply MRF cases with a single MRF zone which covers the
        entire domain.
    
    Usage
        Example of the boundary condition specification for an SRF case or MRF
        case with a single zone:
        \verbatim
        <patchName>
        {
            type            MRFstaticNoSlip;
        }
        \endverbatim
        or if the case has several MRF zones the particular zone this patch is in
        must be named explicitly, e.g.:
        \verbatim
        <patchName>
        {
            type            MRFstaticNoSlip;
            MRFZoneName     rotor;
        }
        \endverbatim
    
    See also
        Foam::MRFnoSlip
        Foam::MRFZone
        Foam::noSlip
    
    This boundary condition avoids spurious fluxes through stationary walls for
    which the faces are not perfectly tangential to the rotational velocity.
    
    The tutorials/incompressibleFluid/mixerSRF case has been updated to demonstrate
    this new boundary condition.

---
+++ src/finiteVolume/cfdTools/general/MRF/derivedFvPatchFields/MRFstaticNoSlip/MRFstaticNoSlipFvPatchVectorField.C
@@ -0,0 +1,122 @@
+/*---------------------------------------------------------------------------*\
+  =========                 |
+  \\      /  F ield         | OpenFOAM: The Open Source CFD Toolbox
+   \\    /   O peration     | Website:  https://openfoam.org
+    \\  /    A nd           | Copyright (C) 2026 OpenFOAM Foundation
+     \\/     M anipulation  |
+-------------------------------------------------------------------------------
+License
+    This file is part of OpenFOAM.
+
+    OpenFOAM is free software: you can redistribute it and/or modify it
+    under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    OpenFOAM is distributed in the hope that it will be useful, but WITHOUT
+    ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+    FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License
+    for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with OpenFOAM.  If not, see <http://www.gnu.org/licenses/>.
+
+\*---------------------------------------------------------------------------*/
+
+#include "MRFstaticNoSlipFvPatchVectorField.H"
+#include "volFields.H"
+#include "addToRunTimeSelectionTable.H"
+
+// * * * * * * * * * * * * * * * * Constructors  * * * * * * * * * * * * * * //
+
+Foam::MRFstaticNoSlipFvPatchVectorField::MRFstaticNoSlipFvPatchVectorField
+(
+    const fvPatch& p,
+    const DimensionedField<vector, fvMesh>& iF,
+    const dictionary& dict
+)
+:
+    fixedValueFvPatchVectorField(p, iF, dict, false),
+    MRFPatchField(dict)
+{
+    if (dict.found("value"))
+    {
+        fvPatchField<vector>::operator=
+        (
+            vectorField("value", iF.dimensions(), dict, p.size())
+        );
+    }
+    else
+    {
+        fvPatchField<vector>::operator=(Zero);
+    }
+}
+
+
+Foam::MRFstaticNoSlipFvPatchVectorField::MRFstaticNoSlipFvPatchVectorField
+(
+    const MRFstaticNoSlipFvPatchVectorField& pvf,
+    const fvPatch& p,
+    const DimensionedField<vector, fvMesh>& iF,
+    const fieldMapper& mapper
+)
+:
+    fixedValueFvPatchVectorField(pvf, p, iF, mapper),
+    MRFPatchField(pvf)
+{}
+
+
+Foam::MRFstaticNoSlipFvPatchVectorField::MRFstaticNoSlipFvPatchVectorField
+(
+    const MRFstaticNoSlipFvPatchVectorField& pvf,
+    const DimensionedField<vector, fvMesh>& iF
+)
+:
+    fixedValueFvPatchVectorField(pvf, iF),
+    MRFPatchField(pvf)
+{}
+
+
+// * * * * * * * * * * * * * * * Member Functions  * * * * * * * * * * * * * //
+
+void Foam::MRFstaticNoSlipFvPatchVectorField::updateCoeffs()
+{
+    if (updated())
+    {
+        return;
+    }
+
+    fvPatchField<vector>::operator=(Zero);
+    makeRelative(*this);
+
+    // Remove the component of Up normal to the wall
+    // just in case it is not exactly circular
+    const vectorField n(patch().nf());
+    vectorField::operator=(field() - n*(n & field()));
+
+    makeAbsolute(*this);
+
+    fixedValueFvPatchVectorField::updateCoeffs();
+}
+
+
+void Foam::MRFstaticNoSlipFvPatchVectorField::write(Ostream& os) const
+{
+    fvPatchVectorField::write(os);
+    MRFPatchField::write(os);
+    writeEntry(os, "value", *this);
+}
+
+
+// * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * //
+
+namespace Foam
+{
+    makePatchTypeField
+    (
+        fvPatchVectorField,
+        MRFstaticNoSlipFvPatchVectorField
+    );
+}
+
+// ************************************************************************* //
